a327a4e113eeacd1ff4891d219b733454777206e
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-30 20:51:52 +0900

    chore: eslint 초기 세팅

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
+};
