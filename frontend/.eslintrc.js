133237b878b800137a68225ec533a922cca87b0f
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-30 21:01:53 +0900

    chore: storybook 설치

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,20 +1,15 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jsx-a11y/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:jsx-a11y/recommended', 'plugin:storybook/recommended'],
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['react', '@typescript-eslint'],
-  rules: {},
+  rules: {}
 };
