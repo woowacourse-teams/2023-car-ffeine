44135f480a2cb261c9121ce019023c9a2be3a3d8
Dain Lee <feb.dain@gmail.com>
2023-07-01 14:05:25 +0900

    chore: strict 모드 활성화

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,15 +1,23 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:jsx-a11y/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:storybook/recommended',
+  ],
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['react', '@typescript-eslint'],
-  rules: {}
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
 };
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import App from './App';
 import { createRoot } from 'react-dom/client';
 import { worker } from './mocks/browser';
+import { StrictMode } from 'react';
 
 const main = async () => {
   if (process.env.NODE_ENV === 'development') {
@@ -14,7 +15,11 @@ const main = async () => {
   const domNode = document.getElementById('root');
   const root = createRoot(domNode);
 
-  root.render(<App />);
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
 };
 
 main();
