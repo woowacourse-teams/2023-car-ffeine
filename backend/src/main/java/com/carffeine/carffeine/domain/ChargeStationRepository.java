a7e06ca939b1d5a59bb079aefb277cb9b743cc06
drunkenhw <drunkenhw@gmail.com>
2023-07-06 22:10:28 +0900

    feat: 위도, 경도를 기반으로 충전소를 조회하는 기능 추가
    [#28]

--- backend/src/main/java/com/carffeine/carffeine/domain/ChargeStationRepository.java
+++ backend/src/main/java/com/carffeine/carffeine/domain/ChargeStationRepository.java
@@ -1,8 +1,15 @@
 package com.carffeine.carffeine.domain;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface ChargeStationRepository extends Repository<ChargeStation, Long> {
 
     ChargeStation save(ChargeStation chargeStation);
+
+    @EntityGraph(attributePaths = "chargers")
+    List<ChargeStation> findAllByLatitudeBetweenAndLongitudeBetween(BigDecimal minLatitude, BigDecimal maxLatitude, BigDecimal minLongitude, BigDecimal maxLongitude);
 }
