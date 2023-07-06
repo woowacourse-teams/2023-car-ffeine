2583940b35be519be082213f1505f0206621365a
kiarakim <kiarakim413@gmail.com>
2023-07-06 21:27:46 +0900

    feat: 데이터 암호화 기능 추가
    Co-authored-by: sosow0212 <sosow0212@naver.com>
    Co-authored-by: be-student <presentsong@naver.com>
    Co-authored-by: drunkenhw <drunkenhw@gmail.com>

---
+++ backend/src/main/java/com/carffeine/carffeine/config/JasyptConfig.java
@@ -0,0 +1,46 @@
+package com.carffeine.carffeine.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+@Configuration
+public class JasyptConfig {
+
+    List<String> lines;
+    String encryptKey;
+
+    public JasyptConfig() {
+        try {
+            String path = getClass().getClassLoader().getResource("encryptKey.txt").getPath();
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+            lines = bufferedReader.lines().toList();
+            encryptKey = lines.get(0);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Bean(name = "jasyptEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        config.setPassword(encryptKey);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations(1000);
+        config.setPoolSize(1);
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
