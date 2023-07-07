600d83cbdbc7a23338c0633f13f6c5ee2f27c335
Gabriel Ju Hyun, Yoon <gabrielyoon7@gmail.com>
2023-07-07 13:31:20 +0900

    feat: 전역 상태 관리 유틸 구현 (#51)
    - external-state로 사용 가능
    - recoil과 동일한 사용법
    
    Co-authored-by: Dain Lee <feb-dain@users.noreply.github.com>
    Co-authored-by: YoungWoo Kim <kyw0716@users.noreply.github.com>
    
    [#46]
    
    Co-authored-by: Dain Lee <feb-dain@users.noreply.github.com>
    Co-authored-by: YoungWoo Kim <kyw0716@users.noreply.github.com>

---
+++ frontend/src/utils/external-state/StateManager.ts
@@ -0,0 +1,41 @@
+export interface DataObserver<T> {
+  setState: (newState: T) => void,
+  getState: () => T,
+  subscribe: (listener: () => void) => () => void;
+  emitChange: () => void;
+}
+
+class StateManager<T> implements DataObserver<T> {
+  public state: T;
+  private listeners: Array<() => void> = [];
+
+  constructor(initialState: T) {
+    this.state = initialState;
+  }
+
+  setState = (newState: T) => {
+    this.state = newState;
+
+    this.emitChange();
+  }
+
+  getState = () => {
+    return this.state;
+  }
+
+  subscribe = (listener: () => void) => {
+    this.listeners = [...this.listeners, listener];
+
+    return () => {
+      this.listeners = this.listeners.filter(l => l !== listener);
+    };
+  }
+
+  emitChange = () => {
+    for (const listener of this.listeners) {
+      listener();
+    }
+  }
+}
+
+export default StateManager;
---
+++ frontend/src/utils/external-state/index.ts
@@ -0,0 +1,6 @@
+export {
+  store,
+  useExternalState,
+  useSetExternalState,
+  useExternalValue
+} from './tools';
