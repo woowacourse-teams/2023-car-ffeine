8ba22def28c8f197eddcc34586c79f4a90420890
drunkenhw <drunkenhw@gmail.com>
2023-07-09 15:36:25 +0900

    refactor: 예외 메세지 생성자에서 초기화
    [#28]

--- backend/src/main/java/com/carffeine/carffeine/common/BaseException.java
+++ backend/src/main/java/com/carffeine/carffeine/common/BaseException.java
@@ -5,15 +5,11 @@ public class BaseException extends RuntimeException {
     private final ExceptionType exceptionType;
 
     public BaseException(ExceptionType exceptionType) {
+        super(exceptionType.message());
         this.exceptionType = exceptionType;
     }
 
     public ExceptionType getExceptionType() {
         return exceptionType;
-    }
-
-    @Override
-    public String getMessage() {
-        return exceptionType.message();
     }
 }
