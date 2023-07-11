d0188a6fcf0d91fdd979dda4c9a7b8e29a053a01
be-student <presentsong@naver.com>
2023-07-11 10:56:46 +0900

    feat: logger error 마다 슬랙으로 발생하는 로그를 전송한다
    [#9]

---
+++ backend/src/main/java/com/carffeine/carffeine/log/SlackAppender.java
@@ -0,0 +1,42 @@
+package com.carffeine.carffeine.log;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.AppenderBase;
+import org.springframework.web.client.RestTemplate;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Map;
+
+public class SlackAppender extends AppenderBase<ILoggingEvent> {
+    private static final String WBHOOK_URL = "https://hooks.slack.com/services/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";
+
+    @Override
+    protected void append(ILoggingEvent eventObject) {
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String, Object> body = createSlackErrorBody(eventObject);
+        restTemplate.postForEntity(WBHOOK_URL, body, String.class);
+    }
+
+    private Map<String, Object> createSlackErrorBody(ILoggingEvent eventObject) {
+        String message = createMessage(eventObject);
+        return Map.of("attachments",
+                List.of(
+                        Map.of("fallback", "요청을 실패했어요 :cry:",
+                                "color", "#2eb886",
+                                "pretext", "에러가 발생했어요 확인해주세요 :cry:",
+                                "author_name", "car-ffeine",
+                                "text", message,
+                                "fields",
+                                List.of(
+                                        Map.of("title", "로그 레벨", "value", eventObject.getLevel().levelStr, "short", false),
+                                        "ts", eventObject.getTimeStamp()))));
+    }
+
+    private String createMessage(ILoggingEvent eventObject) {
+        final String baseMessage = "에러가 발생했습니다.\n";
+        final String pattern = baseMessage + "```%s %s %s [%s] - %s```";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        return String.format(pattern, simpleDateFormat.format(eventObject.getTimeStamp()), eventObject.getLevel(), eventObject.getThreadName(), eventObject.getLoggerName(), eventObject.getFormattedMessage());
+    }
+}
