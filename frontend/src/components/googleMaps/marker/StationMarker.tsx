2e8f7cc89298b3fb819b0758e790fc5325b01559
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-07-12 17:05:57 +0900

    feat: 마커 클릭시 충전소 간단 정보 컴포넌트 뜨는 기능 구현
    Co-authored-by: Dain Lee <feb-dain@users.noreply.github.com>
    Co-authored-by: YoungWoo Kim <kyw0716@users.noreply.github.com>
    
    [#61]

--- frontend/src/components/googleMaps/marker/StationMarker.tsx
+++ frontend/src/components/googleMaps/marker/StationMarker.tsx
@@ -1,27 +1,49 @@
 import { useEffect } from 'react';
-
+import type { Root } from 'react-dom/client';
 import type { Station } from '../../../types';
+import BriefStationInfo from '../../BriefStationInfo';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { fetchStation } from '../../../hooks/useStations';
 
 interface Props {
   map: google.maps.Map;
   station: Station;
-  onClick: () => void;
+  briefStationInfoRoot: Root;
+  infoWindow: google.maps.InfoWindow;
 }
 
-const StationMarker = ({ map, station, onClick }: Props) => {
+const StationMarker = ({ map, station, briefStationInfoRoot, infoWindow }: Props) => {
   const { latitude, longitude, stationName } = station;
 
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation(['stations'], {
+    mutationFn: fetchStation,
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['stations'] });
+    },
+  }); // TODO: 다른 곳에서도 코드 그대로 사용 중이므로 모듈화 필요
+
   useEffect(() => {
-    const newMarker = new google.maps.Marker({
+    const markerInstance = new google.maps.Marker({
       position: { lat: latitude, lng: longitude },
       map: map,
       title: stationName,
     });
 
-    newMarker.addListener('click', onClick);
+    markerInstance.addListener('click', () => {
+      infoWindow.open({
+        anchor: markerInstance,
+        map,
+      });
+
+      briefStationInfoRoot.render(<BriefStationInfo station={station} />);
+      map.panTo(markerInstance.getPosition());
+      mutate(map);
+    });
 
     return () => {
-      newMarker.setMap(null);
+      markerInstance.setMap(null);
     };
   }, []);
 
--- frontend/src/components/googleMaps/marker/StationMarkersContainer.tsx
+++ frontend/src/components/googleMaps/marker/StationMarkersContainer.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from 'react';
+import type { Root } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+
 import StationMarker from './StationMarker';
 import { useStations } from '../../../hooks/useStations';
 
@@ -8,6 +12,20 @@ interface Props {
 const StationMarkersContainer = ({ map }: Props) => {
   const { ...queryInfo } = useStations(map);
   const stations = queryInfo.data;
+
+  const [briefStationInfoRoot, setBriefStationInfoRoot] = useState<Root>();
+  const [infoWindow, setInfoWindow] = useState<google.maps.InfoWindow>();
+
+  useEffect(() => {
+    const container = document.createElement('div');
+    const newRoot = createRoot(container);
+    setBriefStationInfoRoot(newRoot);
+
+    const initialInfoWindow = new google.maps.InfoWindow({
+      content: container,
+    });
+    setInfoWindow(initialInfoWindow);
+  }, []);
 
   if (!stations || !queryInfo.isSuccess) {
     return <></>;
@@ -20,7 +38,8 @@ const StationMarkersContainer = ({ map }: Props) => {
           key={station.stationId}
           map={map}
           station={station}
-          onClick={() => console.log(station)}
+          briefStationInfoRoot={briefStationInfoRoot}
+          infoWindow={infoWindow}
         />
       ))}
     </>
