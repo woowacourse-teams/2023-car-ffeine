d67dd68712f32c6b783d4993a215a9f1b37b1c38
be-student <presentsong@naver.com>
2023-07-13 14:18:08 +0900

    feat: 시작시 실행하도록 하는 기능을 추가한다

---
+++ backend/src/main/java/com/carffeine/carffeine/config/InitialChargeStationLoader.java
@@ -0,0 +1,21 @@
+package com.carffeine.carffeine.config;
+
+import com.carffeine.carffeine.service.chargerStation.ScrapperService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+@ConditionalOnProperty(name = "initialize-charge.enabled", havingValue = "true")
+public class InitialChargeStationLoader implements ApplicationRunner {
+
+    private final ScrapperService scrapperService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        scrapperService.scrap();
+    }
+}
