52d6e5f6c7f1a891ad0dfbe44ffbefd188d7b0d1
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-07-13 12:38:00 +0900

    style: prettier 적용
    [#71]

--- frontend/src/mocks/data.ts
+++ frontend/src/mocks/data.ts
@@ -2,8 +2,9 @@ import type { CapacityType, Charger, ChargerType, Station } from '../types';
 
 const generateRandomData = <T>(array: T[]): T => {
   const randomIndex = Math.floor(Math.random() * array.length);
+
   return array[randomIndex];
-}
+};
 
 const generateRandomChargers = () => {
   const length = Math.floor(Math.random() * 4) + 1;
@@ -14,16 +15,27 @@ const generateRandomChargers = () => {
   }));
 
   return chargers;
-}
+};
 
 export const stations: Station[] = Array.from({ length: 3000 }).map((_, index) => {
   return {
     stationId: index,
     stationName: `충전소 ${index}`,
-    companyName: generateRandomData<string>(['파워큐브', '에버온', '환경부', '한국전력', '티비유', '플러그링크']),
+    companyName: generateRandomData<string>([
+      '파워큐브',
+      '에버온',
+      '환경부',
+      '한국전력',
+      '티비유',
+      '플러그링크',
+    ]),
     chargers: generateRandomChargers(),
     isParkingFree: generateRandomData<boolean>([true, false]),
-    operatingTime: generateRandomData<string>(['24시간', '09:00 ~ 19:00', '평일 09:00~19:00 / 주말 미운영']),
+    operatingTime: generateRandomData<string>([
+      '24시간',
+      '09:00 ~ 19:00',
+      '평일 09:00~19:00 / 주말 미운영',
+    ]),
     detailLocation: generateRandomData<string>(['지상 1층', '지하 1층', '지하 2층', '']),
     latitude: 37 + 9999 * Math.random() * 0.0001,
     longitude: 127 + 9999 * Math.random() * 0.0001,
