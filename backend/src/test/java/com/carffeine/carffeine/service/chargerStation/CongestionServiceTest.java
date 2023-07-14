dd0e6ef64a853ee76029ef03b9cdf096575f386a
kiarakim <kiarakim413@gmail.com>
2023-07-14 19:17:29 +0900

    feat: 날짜별, 시간별 혼잡도 계산 기능 구현

---
+++ backend/src/test/java/com/carffeine/carffeine/service/chargerStation/CongestionServiceTest.java
@@ -0,0 +1,65 @@
+package com.carffeine.carffeine.service.chargerStation;
+
+import com.carffeine.carffeine.controller.chargerStation.dto.CongestionInfoResponse;
+import com.carffeine.carffeine.controller.chargerStation.dto.CongestionResponse;
+import com.carffeine.carffeine.controller.chargerStation.dto.StatisticsResponse;
+import com.carffeine.carffeine.domain.chargerStation.charger.ChargerRepository;
+import com.carffeine.carffeine.domain.chargerStation.congestion.PeriodicCongestionRepository;
+import com.carffeine.carffeine.service.chargerStation.dto.StatisticsRequest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+@DataJpaTest
+class CongestionServiceTest {
+
+    private CongestionService congestionService;
+
+    @Autowired
+    void setCongestionService(PeriodicCongestionRepository periodicCongestionRepository, ChargerRepository chargerRepository) {
+        congestionService = new CongestionService(periodicCongestionRepository, chargerRepository);
+    }
+
+    @Test
+    void 혼잡도_계산() {
+        StatisticsRequest statisticsRequest = new StatisticsRequest("ME174003");
+        StatisticsResponse statisticsResponse = congestionService.calculateCongestion(statisticsRequest);
+
+        CongestionResponse expected = getExpected();
+
+        Assertions.assertThat(expected).usingRecursiveComparison()
+                .isEqualTo(statisticsResponse.congestion());
+    }
+
+    private CongestionResponse getExpected() {
+        List<CongestionInfoResponse> dailyCongestion = new ArrayList<>();
+        for (int i = 0; i < 24; i++) {
+            dailyCongestion.add(new CongestionInfoResponse(i, -1));
+        }
+        return new CongestionResponse(
+                Map.of("MON", dailyCongestion,
+                        "TUE", dailyCongestion,
+                        "WED", dailyCongestion,
+                        "THU", dailyCongestion,
+                        "FRI", dailyCongestion,
+                        "SAT", dailyCongestion,
+                        "SUN", dailyCongestion),
+                Map.of("MON", dailyCongestion,
+                        "TUE", dailyCongestion,
+                        "WED", dailyCongestion,
+                        "THU", dailyCongestion,
+                        "FRI", dailyCongestion,
+                        "SAT", dailyCongestion,
+                        "SUN", dailyCongestion)
+        );
+    }
+}
