c4bada938db9a250f280c3bd28ae2da21cec207f
sosow0212 <sosow0212@naver.com>
2023-07-14 16:59:40 +0900

    refactor: IntegrationTest Extends해서 사용하도록 변경

--- backend/src/test/java/com/carffeine/carffeine/helper/integration/IntegrationTest.java
+++ backend/src/test/java/com/carffeine/carffeine/helper/integration/IntegrationTest.java
@@ -1,13 +1,39 @@
 package com.carffeine.carffeine.helper.integration;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.support.AbstractTestExecutionListener;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Retention(RetentionPolicy.RUNTIME)
-@TestExecutionListeners(value = {IntegrationTestHelper.class}, mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
-public @interface IntegrationTest {
+public class IntegrationTest extends AbstractTestExecutionListener {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void init() {
+        RestAssured.port = this.port;
+
+        List<String> truncateAllTablesQuery = jdbcTemplate.queryForList("SELECT CONCAT('TRUNCATE TABLE ', TABLE_NAME, ';') AS q FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'PUBLIC'", String.class);
+        truncateAllTables(truncateAllTablesQuery);
+    }
+
+    private void truncateAllTables(List<String> truncateAllTablesQuery) {
+        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 0");
+
+        for (String truncateQuery : truncateAllTablesQuery) {
+            jdbcTemplate.execute(truncateQuery);
+        }
+
+        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 1");
+    }
 }
