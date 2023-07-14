0c824375150de0f2ad558d3d834cc96cf112fda1
sosow0212 <sosow0212@naver.com>
2023-07-14 13:08:16 +0900

    refactor: 주석 제거
    Co-authored-by: drunkenhw <drunkenhw@gmail.com>

--- backend/src/test/java/com/carffeine/carffeine/service/ChargerStationUpdateServiceUnitTest.java
+++ backend/src/test/java/com/carffeine/carffeine/service/ChargerStationUpdateServiceUnitTest.java
@@ -40,18 +40,10 @@ class ChargerStationUpdateServiceUnitTest {
     @Mock
     private ChargeUpdateJdbc chargeUpdateJdbc;
 
-    /**
-     * 1. 충전소가 없으면 저장
-     * 2. 충전소가 변경되면 업데이트
-     * 3. 충전기가 없으면 저장
-     * 4. 충전기가 변경되면 업데이트
-     */
-
-
     @Test
     void 기존에_없던_충전소가_생기면_저장한다() {
         // given
-        given(chargeStationRepository.findAll()).willReturn(new ArrayList<ChargeStation>());
+        given(chargeStationRepository.findAll()).willReturn(new ArrayList<>());
         List<ChargeStation> updateStations = List.of(ChargeStationFixture.선릉역_충전소_충전기_2개_사용가능_1개);
 
         // when
@@ -70,7 +62,7 @@ class ChargerStationUpdateServiceUnitTest {
     @Test
     void 기존에_있던_충전소의_데이터가_변경되면_수정한다() {
         // given
-        given(chargeStationRepository.findAll()).willReturn(new ArrayList<ChargeStation>(List.of(ChargeStationFixture.선릉역_충전소_충전기_2개_사용가능_1개)));
+        given(chargeStationRepository.findAll()).willReturn(new ArrayList<>(List.of(ChargeStationFixture.선릉역_충전소_충전기_2개_사용가능_1개)));
         List<ChargeStation> updateStations = List.of(ChargeStationFixture.선릉역_충전소_충전기_2개_사용가능_1개_이름_변경됨);
 
         // when
@@ -89,7 +81,7 @@ class ChargerStationUpdateServiceUnitTest {
     @Test
     void 기존에_없던_충전기가_생기면_저장한다() {
         // given
-        given(chargeStationRepository.findAll()).willReturn(new ArrayList<ChargeStation>(List.of(ChargeStationFixture.선릉역_충전소_충전기_0개_사용가능_0개)));
+        given(chargeStationRepository.findAll()).willReturn(new ArrayList<>(List.of(ChargeStationFixture.선릉역_충전소_충전기_0개_사용가능_0개)));
         List<ChargeStation> updateStations = List.of(ChargeStationFixture.선릉역_충전소_충전기_2개_사용가능_1개);
 
         // when
@@ -108,7 +100,7 @@ class ChargerStationUpdateServiceUnitTest {
     @Test
     void 기존에_있던_충전기의_데이터가_변경되면_수정한다() {
         // given
-        given(chargeStationRepository.findAll()).willReturn(new ArrayList<ChargeStation>(List.of(ChargeStationFixture.선릉역_충전소_충전기_2개_사용가능_1개)));
+        given(chargeStationRepository.findAll()).willReturn(new ArrayList<>(List.of(ChargeStationFixture.선릉역_충전소_충전기_2개_사용가능_1개)));
         List<ChargeStation> updateStations = List.of(ChargeStationFixture.선릉역_충전소_충전기_2개_사용가능_1개_완속);
 
         // when
