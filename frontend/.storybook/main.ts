31e7b83b1d1504debd76f49d07ca9157ca407dfd
Dain Lee <feb.dain@gmail.com>
2023-07-16 11:02:43 +0900

    chore: 스토리북에도 절대 경로 설정
    [#95]

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,4 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import path from 'path';
+import { Configuration } from 'webpack';
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -12,6 +15,22 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag',
+  },
+  webpackFinal: async (config: Configuration) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@map': path.resolve(__dirname, '../src/components/google-maps/map'),
+        '@marker': path.resolve(__dirname, '../src/components/google-maps/marker'),
+        '@components': path.resolve(__dirname, '../src/components/ui'),
+        '@utils': path.resolve(__dirname, '../src/utils'),
+        '@hooks': path.resolve(__dirname, '../src/hooks'),
+        '@stores': path.resolve(__dirname, '../src/stores'),
+        '@constants': path.resolve(__dirname, '../src/constants/index'),
+      };
+    }
+
+    return config;
   },
 };
 export default config;
