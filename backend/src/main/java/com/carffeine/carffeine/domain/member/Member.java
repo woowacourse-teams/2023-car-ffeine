e38c2bb2e35f353a94908832ad16969ef3929163
drunkenhw <drunkenhw@gmail.com>
2023-07-18 12:55:48 +0900

    feat: OAuth2 회원 추가

---
+++ backend/src/main/java/com/carffeine/carffeine/domain/member/Member.java
@@ -0,0 +1,35 @@
+package com.carffeine.carffeine.domain.member;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    public Member(String email) {
+        this.email = email;
+    }
+
+    public Member update(String email) {
+        this.email = email;
+        return this;
+    }
+}
