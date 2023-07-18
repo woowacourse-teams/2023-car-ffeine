18075db889c0c30eea14562ad553f25dff8acf94
kyw0716 <kyw0716@naver.com>
2023-07-18 15:57:35 +0900

    refactor: 구글 지도 최초 로드 방식 수정
    [#110]

--- frontend/src/stores/googleMapStore.ts
+++ frontend/src/stores/googleMapStore.ts
@@ -1,15 +1,38 @@
 import { store } from '@utils/external-state';
 
-export const googleMapStore = store<google.maps.Map>(null);
+import { INITIAL_CENTER, INITIAL_ZOOM_SIZE } from '@constants';
+
+export const googleMapStore = (() => {
+  let googleMap: google.maps.Map;
+
+  const mapDiv = document.createElement('div');
+
+  mapDiv.id = 'map';
+  mapDiv.style.minHeight = '100vh';
+
+  document.body.appendChild(mapDiv);
+
+  return () => {
+    if (!googleMap) {
+      googleMap = new window.google.maps.Map(mapDiv, {
+        center: INITIAL_CENTER,
+        zoom: INITIAL_ZOOM_SIZE,
+        disableDefaultUI: true,
+      });
+    }
+
+    return store<google.maps.Map>(googleMap);
+  };
+})();
 
 export const googleMapActions = {
   zoomUp: () => {
-    const googleMap = googleMapStore.getState();
+    const googleMap = googleMapStore().getState();
     const prevZoom = googleMap.getZoom();
     googleMap.setZoom(prevZoom + 1);
   },
   zoomDown: () => {
-    const googleMap = googleMapStore.getState();
+    const googleMap = googleMapStore().getState();
     const prevZoom = googleMap.getZoom();
     googleMap.setZoom(prevZoom - 1);
   },
