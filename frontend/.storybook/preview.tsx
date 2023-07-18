068318a8b63004d026816ea0afac7775a6c559b6
Dain Lee <108778921+feb-dain@users.noreply.github.com>
2023-07-18 15:46:19 +0900

    feat: 버튼 스타일 컴포넌트를 구현한다 (#109)
    * feat: 버튼 스타일 컴포넌트 구현
    
    [#107]
    
    * test: 설명 및 옵션 추가
    
    [#107]
    
    * feat: borderRadius에 `all` 옵션 추가
    
    [#107]
    
    -
    
    Co-authored-by: Gabriel Ju Hyun, Yoon <gabrielyoon7@gmail.com>

---
+++ frontend/.storybook/preview.tsx
@@ -0,0 +1,66 @@
+import type { Preview } from '@storybook/react';
+
+import React from 'react';
+
+import { GlobalStyle } from '../src/style/GlobalStyle';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    viewport: {
+      defaultViewport: 'Desktop',
+      viewports: {
+        iphone6: {
+          name: 'iPhone SE',
+          styles: {
+            width: '375px',
+            height: '667px',
+          },
+        },
+        ipad: {
+          name: 'IPad',
+          styles: {
+            width: '768px',
+            height: '1024px',
+          },
+        },
+        desktop: {
+          name: 'Desktop',
+          styles: {
+            width: '1440px',
+            height: '900px',
+          },
+        },
+      },
+    },
+    backgrounds: {
+      default: 'white',
+      values: [
+        {
+          name: 'white',
+          value: '#fff',
+        },
+        {
+          name: 'black',
+          value: '#000',
+        },
+      ],
+    },
+  },
+  decorators: [
+    (Story) => (
+      <>
+        <GlobalStyle />
+        <Story />
+      </>
+    ),
+  ],
+};
+
+export default preview;
