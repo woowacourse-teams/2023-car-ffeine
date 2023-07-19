d69d54944f0d14b0ad3dfb2858c584a834635271
kyw0716 <kyw0716@naver.com>
2023-07-19 14:45:14 +0900

    feat: 간단 정보 띄우는 기능 구현
    [#125]

--- frontend/src/components/ui/BriefStationInfo.tsx
+++ frontend/src/components/ui/BriefStationInfo.tsx
@@ -5,11 +5,33 @@ interface Props {
 }
 
 const BriefStationInfo = ({ station }: Props) => {
+  const slowChargerCount = station.chargers.filter((charger) => charger.type === '완속').length;
+  const fastChargerCount = station.chargers.length - slowChargerCount;
+
+  const handleOpenStationDetail = () => {
+    console.log('야미 형님 여기서 상세 정보 불러오기 작업하시면 됩니다');
+  };
+
   return (
-    <>
+    <div style={{ minWidth: '200px' }}>
+      <div>{station.companyName}</div>
       <div>{station.stationName}</div>
-      <button onClick={() => alert(JSON.stringify(station))}>나의 정보는?</button>
-    </>
+      <div>{station.detailLocation}</div>
+
+      <div style={{ display: 'flex', gap: 10 }}>
+        {station.isParkingFree && <div>주차 무료</div>}
+        {station.isPrivate && <div>이용 제한</div>}
+      </div>
+
+      <div style={{ display: 'flex', gap: 10 }}>
+        {slowChargerCount > 0 && <div>완속: {slowChargerCount}개</div>}
+        {fastChargerCount > 0 && <div>급속: {fastChargerCount}개</div>}
+      </div>
+
+      <button style={{ width: '100%' }} onClick={handleOpenStationDetail}>
+        상세보기
+      </button>
+    </div>
   );
 };
 
