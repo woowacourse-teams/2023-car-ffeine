083b0983375222cb87bf613b71cc22cb0f69597f
YoungWoo Kim <77326660+kyw0716@users.noreply.github.com>
2023-07-19 17:12:07 +0900

    fix: 웹팩 빌드 오류를 해결한다 (#138)
    * chore: 웹팩 빌드 오류 수정
    
    - styled-components의 createGlobalStyle 내부에 @import 삭제
    
    [#135]
    
    * refactor: 개발 환경에서만 msw가 동작하도록 분기처리한 부분 삭제
    
    - 백엔드와 연결 성공 시 다시 분기 처리할 것
    
    [#135]

--- frontend/src/style/GlobalStyle.ts
+++ frontend/src/style/GlobalStyle.ts
@@ -3,13 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 import { reset } from './reset';
 
 export const GlobalStyle = createGlobalStyle`
-  @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap");
-
   ${reset}
-
-  * {
-    font-family: "Noto Sans KR", sans-serif !important;
-  }
   
   /********** hidden scroll **********/
   html,
