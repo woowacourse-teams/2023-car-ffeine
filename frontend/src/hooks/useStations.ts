9f9feda8b25df83c81f2e13ab70993911b2c1ff8
kyw0716 <kyw0716@naver.com>
2023-07-19 12:15:42 +0900

    feat: 충전소 정보 필터링 기능 추가
    [#123]

--- frontend/src/hooks/useStations.ts
+++ frontend/src/hooks/useStations.ts
@@ -1,6 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 
+import { useExternalValue } from '@utils/external-state';
 import { getDisplayPosition } from '@utils/google-maps';
+
+import { stationFilterStore } from '@stores/stationFilterStore';
 
 import type { Station } from 'types';
 
@@ -16,5 +19,23 @@ export const fetchStation = async (map: google.maps.Map) => {
 };
 
 export const useStations = (map: google.maps.Map) => {
-  return useQuery({ queryKey: ['stations'], queryFn: () => fetchStation(map) });
+  const { showAvailableStationOnly, showFasterChargeStationOnly, showParkingFreeStationOnly } =
+    useExternalValue(stationFilterStore);
+
+  return useQuery({
+    queryKey: ['stations'],
+    queryFn: () => fetchStation(map),
+    select: (data) => {
+      return data.filter((station) => {
+        const { availableCount, isParkingFree, chargers } = station;
+
+        if (showAvailableStationOnly && availableCount === 0) return false;
+        if (showFasterChargeStationOnly && !chargers.some((charger) => charger.type === '급속'))
+          return false;
+        if (showParkingFreeStationOnly && !isParkingFree) return false;
+
+        return true;
+      });
+    },
+  });
 };
