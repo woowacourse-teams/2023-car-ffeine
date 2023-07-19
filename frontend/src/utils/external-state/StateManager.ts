2c20e565eb0a99623d50ca7bc4a74a69fb8dbe42
kyw0716 <kyw0716@naver.com>
2023-07-19 12:12:28 +0900

    fix: SetStateCallback 타입 추가 반영
    [#123]

--- frontend/src/utils/external-state/StateManager.ts
+++ frontend/src/utils/external-state/StateManager.ts
@@ -1,4 +1,4 @@
-type SetStateCallback<T> = (prevState: T) => T;
+export type SetStateCallback<T> = (prevState: T) => T;
 
 export interface DataObserver<T> {
   setState: (param: SetStateCallback<T> | T) => void;
