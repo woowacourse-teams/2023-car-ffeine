594813b65e8ffac719222f7e2d76205e20465be3
Dain Lee <feb.dain@gmail.com>
2023-07-20 17:18:24 +0900

    refactor: 충전소 상세 정보 더 구체적으로 변경
    [#128]

--- frontend/src/components/ui/DetailedStationInfo.tsx
+++ frontend/src/components/ui/DetailedStationInfo.tsx
@@ -26,27 +26,27 @@ const DetailedStationInfo = () => {
       <h2>{stationName}</h2>
       <p>{companyName}</p>
       <p>{address}</p>
-      <p>{detailLocation}</p>
-      <p>{operatingTime}</p>
-      <p>{stationState}</p>
-      <p>{contact}</p>
-      <p>{privateReason}</p>
-      <ul>
-        <li>{isPrivate}</li>
-        <li>{isParkingFree}</li>
-      </ul>
+      {detailLocation && <p>{detailLocation}</p>}
+      {operatingTime && <p>{operatingTime}</p>}
+      {stationState && <p>{stationState}</p>}
+      {contact && <p>{contact}</p>}
+      {privateReason && <p>{privateReason}</p>}
+      <div>
+        {isPrivate && <p>이용 제한</p>}
+        {isParkingFree && <p>주차무료</p>}
+      </div>
       {chargers.map((data, index) => {
         const { type, price, capacity, latestUpdateTime, state, method } = data;
 
         return (
-          <ul key={index}>
+          <ChargerContainer key={index}>
             <li>{type}</li>
-            <li>{price}</li>
-            <li>{capacity}</li>
-            <li>{String(latestUpdateTime)}</li>
-            <li>{state}</li>
-            <li>{method}</li>
-          </ul>
+            <li>가격: {price}</li>
+            <li>{capacity >= 50 ? '급속' : '완속'}</li>
+            {latestUpdateTime && <li>{String(latestUpdateTime)}</li>}
+            <li>충전기 상태: {state}</li>
+            {method && <li>{method}</li>}
+          </ChargerContainer>
         );
       })}
     </Container>
@@ -61,6 +61,10 @@ const Container = styled.div`
   padding: 10px;
   background-color: white;
   box-shadow: 1px 1px 2px gray;
+`;
+
+const ChargerContainer = styled.ul`
+  border: 1px solid #000;
 `;
 
 export default DetailedStationInfo;
