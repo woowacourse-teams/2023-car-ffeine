861305a2e4e1d0d551a62e691c26d289ae295666
sosow0212 <sosow0212@naver.com>
2023-07-20 16:40:20 +0900

    test: 업데이트 기능 테스트 추가
    Co-authored-by: drunkenhw <drunkenhw@gmail.com>

--- backend/src/test/java/com/carffeine/carffeine/service/chargerstation/ChargerStationUpdateServiceTest.java
+++ backend/src/test/java/com/carffeine/carffeine/service/chargerstation/ChargerStationUpdateServiceTest.java
@@ -2,118 +2,85 @@ package com.carffeine.carffeine.service.chargerStation;
 
 import com.carffeine.carffeine.domain.chargestation.ChargeStation;
 import com.carffeine.carffeine.domain.chargestation.ChargeStationRepository;
-import com.carffeine.carffeine.domain.chargestation.ChargeUpdateJdbc;
 import com.carffeine.carffeine.fixture.chargerstation.ChargeStationFixture;
+import com.carffeine.carffeine.helper.integration.IntegrationTest;
 import com.carffeine.carffeine.service.chargerstation.ChargerStationUpdateService;
-import com.carffeine.carffeine.service.chargerstation.UpdateScrapper;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.mockito.internal.verification.VerificationModeFactory.times;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
-@ExtendWith(MockitoExtension.class)
-class ChargerStationUpdateServiceUnitTest {
+class ChargerStationUpdateServiceTest extends IntegrationTest {
 
-    @InjectMocks
+    @Autowired
     private ChargerStationUpdateService chargerStationUpdateService;
 
-    @Mock
-    private UpdateScrapper updateScrapper;
-
-    @Mock
+    @Autowired
     private ChargeStationRepository chargeStationRepository;
-
-    @Mock
-    private ChargeUpdateJdbc chargeUpdateJdbc;
 
     @Test
     void 기존에_없던_충전소가_생기면_저장한다() {
         // given
-        given(chargeStationRepository.findAllFetch()).willReturn(new ArrayList<>());
         List<ChargeStation> updateStations = List.of(ChargeStationFixture.선릉역_충전소_충전기_2개_사용가능_1개);
 
         // when
-        when(updateScrapper.updateData()).thenReturn(updateStations);
-        chargerStationUpdateService.updateStations();
+        chargerStationUpdateService.updateStations(updateStations);
 
         // then
-        assertAll(
-                () -> verify(chargeUpdateJdbc, times(1)).saveAllStationsBatch(any()),
-                () -> verify(chargeUpdateJdbc, times(0)).updateAllStationsBatch(any()),
-                () -> verify(chargeUpdateJdbc, times(0)).saveAllChargersBatch(any()),
-                () -> verify(chargeUpdateJdbc, times(0)).updateAllChargersBatch(any())
-        );
+        List<ChargeStation> result = chargeStationRepository.findAll();
+        assertThat(result.get(0)).isEqualTo(updateStations.get(0));
     }
 
     @Test
     void 기존에_있던_충전소의_데이터가_변경되면_수정한다() {
         // given
-        given(chargeStationRepository.findAllFetch()).willReturn(new ArrayList<>(List.of(ChargeStationFixture.선릉역_충전소_충전기_2개_사용가능_1개)));
-        List<ChargeStation> updateStations = List.of(ChargeStationFixture.선릉역_충전소_충전기_2개_사용가능_1개_이름_변경됨);
+        ChargeStation originStation = ChargeStationFixture.선릉역_충전소_충전기_2개_사용가능_1개;
+        ChargeStation changeStation = ChargeStationFixture.선릉역_충전소_충전기_2개_사용가능_1개_이름_변경됨;
+
+        chargeStationRepository.save(originStation);
+        List<ChargeStation> updateStations = List.of(changeStation);
 
         // when
-        when(updateScrapper.updateData()).thenReturn(updateStations);
-        chargerStationUpdateService.updateStations();
+        chargerStationUpdateService.updateStations(updateStations);
 
         // then
-        assertAll(
-                () -> verify(chargeUpdateJdbc, times(0)).saveAllStationsBatch(any()),
-                () -> verify(chargeUpdateJdbc, times(1)).updateAllStationsBatch(any()),
-                () -> verify(chargeUpdateJdbc, times(0)).saveAllChargersBatch(any()),
-                () -> verify(chargeUpdateJdbc, times(0)).updateAllChargersBatch(any())
-        );
+        List<ChargeStation> stations = chargeStationRepository.findAll();
+        assertThat(stations.get(0).getStationName()).isEqualTo(changeStation.getStationName());
     }
 
     @Test
     void 기존에_없던_충전기가_생기면_저장한다() {
         // given
-        given(chargeStationRepository.findAllFetch()).willReturn(new ArrayList<>(List.of(ChargeStationFixture.선릉역_충전소_충전기_0개_사용가능_0개)));
-        List<ChargeStation> updateStations = List.of(ChargeStationFixture.선릉역_충전소_충전기_2개_사용가능_1개);
+        ChargeStation originStation = ChargeStationFixture.선릉역_충전소_충전기_0개_사용가능_0개;
+        chargeStationRepository.save(originStation);
+        List<ChargeStation> updateStations = List.of(ChargeStationFixture.잠실역_충전소_충전기_2개_사용가능_1개);
 
         // when
-        when(updateScrapper.updateData()).thenReturn(updateStations);
-        chargerStationUpdateService.updateStations();
+        chargerStationUpdateService.updateStations(updateStations);
 
         // then
-        assertAll(
-                () -> verify(chargeUpdateJdbc, times(0)).saveAllStationsBatch(any()),
-                () -> verify(chargeUpdateJdbc, times(0)).updateAllStationsBatch(any()),
-                () -> verify(chargeUpdateJdbc, times(1)).saveAllChargersBatch(any()),
-                () -> verify(chargeUpdateJdbc, times(0)).updateAllChargersBatch(any())
-        );
+        List<ChargeStation> stations = chargeStationRepository.findAll();
+        assertThat(stations.size()).isEqualTo(2);
     }
 
     @Test
     void 기존에_있던_충전기의_데이터가_변경되면_수정한다() {
         // given
-        given(chargeStationRepository.findAllFetch()).willReturn(new ArrayList<>(List.of(ChargeStationFixture.선릉역_충전소_충전기_2개_사용가능_1개)));
-        List<ChargeStation> updateStations = List.of(ChargeStationFixture.선릉역_충전소_충전기_2개_사용가능_1개_완속);
+        ChargeStation originStation = ChargeStationFixture.선릉역_충전소_충전기_2개_사용가능_1개;
+        chargeStationRepository.save(originStation);
+        List<ChargeStation> updateStations = List.of(ChargeStationFixture.선릉역_충전소_충전기_2개_사용가능_1개_이름_변경됨);
 
         // when
-        when(updateScrapper.updateData()).thenReturn(updateStations);
-        chargerStationUpdateService.updateStations();
+        chargerStationUpdateService.updateStations(updateStations);
 
         // then
-        assertAll(
-                () -> verify(chargeUpdateJdbc, times(0)).saveAllStationsBatch(any()),
-                () -> verify(chargeUpdateJdbc, times(0)).updateAllStationsBatch(any()),
-                () -> verify(chargeUpdateJdbc, times(0)).saveAllChargersBatch(any()),
-                () -> verify(chargeUpdateJdbc, times(1)).updateAllChargersBatch(any())
-        );
+        List<ChargeStation> stations = chargeStationRepository.findAll();
+        assertThat(stations.get(0).getStationName()).isEqualTo(updateStations.get(0).getStationName());
     }
 }
