f02317ece71346e08c683e09810bba1a18f2ade8
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-07-23 01:11:01 +0900

    refactor: 주석 위치 수정
    [#143]

--- frontend/src/style/GlobalStyle.ts
+++ frontend/src/style/GlobalStyle.ts
@@ -4,11 +4,11 @@ import { reset } from './reset';
 
 export const GlobalStyle = createGlobalStyle`
   ${reset}
-    /********** hidden scroll **********/
   html,
   body {
     font-family: 'Noto Sans KR', sans-serif;
     font-size: 62.5%;
+    /********** hidden scroll **********/
     scrollbar-width: none;
   }
 
