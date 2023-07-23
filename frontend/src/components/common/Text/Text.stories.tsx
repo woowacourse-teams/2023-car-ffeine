7ce8a0bbe28e1d981fe406c0cf651fcc46ba7d4d
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-07-23 22:39:09 +0900

    refactor: Text Storybook 개선
    [#143]

--- frontend/src/components/common/Text/Text.stories.tsx
+++ frontend/src/components/common/Text/Text.stories.tsx
@@ -7,6 +7,9 @@ const meta = {
   component: Text,
   tags: ['autodocs'],
   args: {
+    variant: 'body',
+    align: 'left',
+    mb: 0,
     children: 'You forget a thousand things every day. Make sure this is one of them.',
   },
   argTypes: {
@@ -37,19 +40,11 @@ const meta = {
       description: '글자 크기 및 두께를 바꿀 수 있습니다. 기본값은 body입니다.',
     },
     mb: {
-      options: {
-        none: false,
-        1: 1,
-        2: 2,
-        3: 3,
-        4: 4,
-        5: 5,
-      },
       control: {
-        type: 'select',
+        type: 'number',
       },
       description:
-        '숫자를 입력해 글자의 하단 간격을 조절할 수 있습니다. 입력한 숫자 x 4px 만큼 간격이 늘어납니다.<br> 기본값은 4px 입니다.',
+        '숫자를 입력해 글자의 하단 간격을 조절할 수 있습니다. `입력한 숫자 x 4px` 만큼 간격이 늘어납니다.<br> 기본값은 4px 입니다.',
     },
     align: {
       options: {
@@ -74,7 +69,12 @@ interface Props {
 }
 
 export const Default = (args: Props) => {
-  return <Text {...args} />;
+  return (
+    <>
+      <Text {...args} />
+      <Text {...args} />
+    </>
+  );
 };
 
 export const Sizes = () => {
@@ -96,72 +96,16 @@ export const Sizes = () => {
   );
 };
 
-export const MarginBottoms = () => {
+export const MarginBottom = () => {
   return (
     <>
       <Text variant="h1" mb={5}>
         Heading 1
       </Text>
-      <Text variant="h2" mb={4}>
-        Heading 2
-      </Text>
-      <Text variant="h3" mb={3}>
-        Heading 3
-      </Text>
-      <Text variant="h4" mb={2}>
-        Heading 4
-      </Text>
-      <Text variant="h5" mb={1}>
-        Heading 5
-      </Text>
-      <Text variant="h6" mb={5}>
-        Heading 6
-      </Text>
-      <Text variant="title" mb={4}>
-        Title
-      </Text>
-      <Text variant="subtitle" mb={3}>
-        Subtitle
-      </Text>
-      <Text variant="label" mb={2}>
-        Label
-      </Text>
-      <Text variant="body" mb={1}>
-        Body Text
-      </Text>
-      <Text variant="caption">Caption Text</Text>
-      <Text>You forget a thousand things every day. Make sure this is one of them.</Text>
-    </>
-  );
-};
-
-export const Aligns = () => {
-  return (
-    <>
-      <Text variant="h1" align="center">
+      <Text variant="h1" mb={10}>
         Heading 1
       </Text>
-      <Text variant="h2" align="left">
-        Heading 2
-      </Text>
-      <Text variant="h3" align="right">
-        Heading 3
-      </Text>
-      <Text variant="h4" align="center">
-        Heading 4
-      </Text>
-      <Text variant="h5" align="left">
-        Heading 5
-      </Text>
-      <Text variant="h6" align="right">
-        Heading 6
-      </Text>
-      <Text variant="title">Title</Text>
-      <Text variant="subtitle">Subtitle</Text>
-      <Text variant="label">Label</Text>
-      <Text variant="body">Body Text</Text>
-      <Text variant="caption">Caption Text</Text>
-      <Text>You forget a thousand things every day. Make sure this is one of them.</Text>
+      <Text variant="h1">Heading 1</Text>
     </>
   );
 };
