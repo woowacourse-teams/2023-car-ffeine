85374bfdbad07cd6eae784d518c76c20258bbd8f
Dain Lee <feb.dain@gmail.com>
2023-07-25 00:04:47 +0900

    design: GlobalStyle에서 `font` 적용하는 선택자 변경
    이유
    - font가 적용 안 되는 상황 방지
    - 스토리북 font도 Noto Sans KR로 변경
    
    [#114]

--- frontend/src/style/GlobalStyle.ts
+++ frontend/src/style/GlobalStyle.ts
@@ -4,9 +4,13 @@ import { reset } from './reset';
 
 export const GlobalStyle = createGlobalStyle`
   ${reset}
+
+  * {
+    font-family: 'Noto Sans KR', sans-serif !important;
+  }
+
   html,
   body {
-    font-family: 'Noto Sans KR', sans-serif;
     font-size: 62.5%;
     /********** hidden scroll **********/
     scrollbar-width: none;
