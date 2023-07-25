e3e67b1a2d50541ea86170d01568a25974aba989
drunkenhw <drunkenhw@gmail.com>
2023-07-25 20:49:01 +0900

    feat: 고장 신고 클래스 추가
    [#180]

---
+++ backend/src/main/java/com/carffeine/carffeine/station/domain/report/FaultReport.java
@@ -0,0 +1,33 @@
+package com.carffeine.carffeine.station.domain.report;
+
+import com.carffeine.carffeine.station.domain.station.Station;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "fault_report")
+@Entity
+public class FaultReport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long memberId;
+    
+    @ManyToOne
+    private Station station;
+}
