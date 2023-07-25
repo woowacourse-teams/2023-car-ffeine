12c14f8c0e63810f0daced716c3699ed6666d71b
Dain Lee <108778921+feb-dain@users.noreply.github.com>
2023-07-25 14:14:44 +0900

    feat: 플렉스 박스 스타일 컴포넌트를 구현한다 (#173)
    * test: 필요없는 파일 및 코드 삭제
    
    [#114]
    
    * test: 타입 중복 제거
    
    - 재사용되는 타입은 전역으로 분리
    - 컴포넌트의 Props export하여 스토리북에서 재사용
    
    [#114]
    
    * feat: FlexBox 컴포넌트 구현
    
    [#114]
    
    * design: GlobalStyle에서 `font` 적용하는 선택자 변경
    
    이유
    - font가 적용 안 되는 상황 방지
    - 스토리북 font도 Noto Sans KR로 변경
    
    [#114]
    
    * chore: storybook 접근성 검사 addon 설치
    
    [#114]
    
    * refactor: 공통으로 사용되는 스타일 전역으로 분리
    
    [#114]
    
    * fix: 공통 style 전역으로 분리하면서 생기는 에러 개선
    
    절대 경로 설정
    
    [#114]
    
    * refactor: 타입 이름 명확하게 변경
    
    [#114]
    
    * refactor: `justify content`와 `align items` 속성 따로 적용할 수 있도록 변경
    
    [#114]
    
    * test: row gap과 column gap 설명 보강
    
    [#114]
    
    * refactor: px → rem 단위로 변경
    
    [#114]

---
+++ frontend/src/components/common/FlexBox/FlexBox.stories.tsx
@@ -0,0 +1,225 @@
+import type { Meta } from '@storybook/react';
+import type { CSSProp } from 'styled-components';
+import styled, { css } from 'styled-components';
+
+import type { FlexBoxProps } from './FlexBox';
+import FlexBox, { FLEX_BOX_ITEM_POSITION } from './FlexBox';
+
+const Box = styled.div`
+  width: 30%;
+  padding: 1rem;
+  border: 0.15rem solid #000;
+  border-radius: 0.8rem;
+`;
+
+const Boxes = (tag?: 'li') => {
+  return (
+    <>
+      <Box as={tag}>1 박스</Box>
+      <Box as={tag}>2 박스</Box>
+      <Box as={tag}>3 박스</Box>
+      <Box as={tag}>1 박스</Box>
+      <Box as={tag}>2 박스</Box>
+      <Box as={tag}>3 박스</Box>
+    </>
+  );
+};
+
+const meta = {
+  title: 'Components/FlexBox',
+  component: FlexBox,
+  tags: ['autodocs'],
+  args: {
+    tag: 'ul',
+    justifyContent: 'center',
+    alignItems: 'center',
+    outlined: true,
+    direction: 'row',
+    nowrap: false,
+    children: Boxes('li'),
+    css: css`
+      height: 24rem;
+    `,
+  },
+  argTypes: {
+    tag: {
+      description: '태그명(ex. ul, section)을 입력해 플렉스 컨테이너의 태그를 바꿀 수 있습니다.',
+    },
+    justifyContent: {
+      options: Object.keys({ ...FLEX_BOX_ITEM_POSITION, none: undefined }),
+      control: {
+        type: 'select',
+      },
+      description:
+        '플렉스 컨테이너 안에 있는 아이템의 위치를 조절할 수 있습니다.<br>- direction이 row일 경우: 수평을 기준으로 아이템이 이동합니다.<br>- direction이 column일 경우: 수직을 기준으로 아이템이 이동합니다.',
+    },
+    alignItems: {
+      options: Object.keys({ ...FLEX_BOX_ITEM_POSITION, none: undefined }),
+      control: {
+        type: 'select',
+      },
+      description:
+        '플렉스 컨테이너 안에 있는 아이템의 위치를 조절할 수 있습니다.<br>- direction이 row일 경우: 수직을 기준으로 아이템이 이동합니다.<br>- direction이 column일 경우: 수평을 기준으로 아이템이 이동합니다.',
+    },
+    noRadius: {
+      options: { none: false, all: 'all', top: 'top', bottom: 'bottom' },
+      control: {
+        type: 'select',
+      },
+      description: '특정 방향의 radius 속성을 제거할 수 있습니다.',
+    },
+    outlined: {
+      control: {
+        type: 'boolean',
+      },
+      description: 'true: 플렉스 컨테이너에 검은색 테두리가 생깁니다.',
+    },
+    background: {
+      control: {
+        type: 'color',
+      },
+      description: '선택한 색상에 따라 플렉스 컨테이너의 배경색이 변합니다.',
+    },
+    direction: {
+      description: `row: 플렉스 컨테이너 안 아이템이 수직 방향으로 정렬됩니다. 기본값입니다.<br>column: 플렉스 컨테이너 안 아이템이 수직 방향으로 정렬됩니다.`,
+    },
+    nowrap: {
+      description:
+        'true: 플렉스 컨테이너, 아이템 크기에 상관없이 무조건 한 줄로 정렬합니다. 아이템의 width, height가 정해져 있지 않다면 플렉스 컨테이너의 크기만큼 늘어납니다.',
+    },
+    gap: {
+      description:
+        '숫자를 입력해 플렉스 컨테이너 안 아이템 간의 간격을 조절할 수 있습니다. `입력한 숫자 x 4px` 만큼 간격이 늘어납니다.',
+    },
+    rowGap: {
+      description:
+        '숫자를 입력해 플렉스 컨테이너 안 아이템 간의 행 높이를 조절할 수 있습니다. `입력한 숫자 x 4px` 만큼 간격이 늘어납니다.<br>❗gap이 적용되어 있으면(undefined가 아니면) row gap은 적용되지 않습니다.',
+    },
+    columnGap: {
+      description:
+        '숫자를 입력해 플렉스 컨테이너 안 아이템 간의 열 너비를 조절할 수 있습니다. `입력한 숫자 x 4px` 만큼 간격이 늘어납니다.<br>❗gap이 적용되어 있으면(undefined가 아니면) column gap은 적용되지 않습니다.',
+    },
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+    css: {
+      description: '원하는 css를 적용할 수 있습니다.',
+    },
+  },
+} satisfies Meta<typeof FlexBox>;
+
+export default meta;
+
+export const Default = (args: FlexBoxProps) => {
+  return <FlexBox {...args} />;
+};
+
+interface SizeProps {
+  width?: number;
+  height?: number;
+}
+
+const size = ({ width, height }: SizeProps): CSSProp => css`
+  width: ${width}rem;
+  height: ${height}rem;
+`;
+
+export const JustifyContent = () => {
+  return (
+    <FlexBox nowrap columnGap={4} justifyContent="between">
+      <FlexBox direction="column" rowGap={5}>
+        <FlexBox outlined justifyContent="start" css={size({ height: 10 })}>
+          {Boxes()}
+        </FlexBox>
+        <FlexBox outlined justifyContent="center" css={size({ height: 10 })}>
+          {Boxes()}
+        </FlexBox>
+        <FlexBox outlined justifyContent="end" css={size({ height: 10 })}>
+          {Boxes()}
+        </FlexBox>
+        <FlexBox outlined justifyContent="between" css={size({ height: 10 })}>
+          {Boxes()}
+        </FlexBox>
+      </FlexBox>
+      <FlexBox outlined direction="column" justifyContent="start" css={size({ width: 12 })}>
+        {Boxes()}
+      </FlexBox>
+      <FlexBox outlined direction="column" justifyContent="center" css={size({ width: 12 })}>
+        {Boxes()}
+      </FlexBox>
+      <FlexBox outlined direction="column" justifyContent="end" css={size({ width: 12 })}>
+        {Boxes()}
+      </FlexBox>
+      <FlexBox outlined direction="column" justifyContent="between" css={size({ width: 12 })}>
+        {Boxes()}
+      </FlexBox>
+    </FlexBox>
+  );
+};
+
+export const AlignItems = () => {
+  return (
+    <FlexBox nowrap columnGap={4} justifyContent="between">
+      <FlexBox direction="column" rowGap={5}>
+        <FlexBox outlined alignItems="start" css={size({ height: 10 })}>
+          {Boxes()}
+        </FlexBox>
+        <FlexBox outlined alignItems="center" css={size({ height: 10 })}>
+          {Boxes()}
+        </FlexBox>
+        <FlexBox outlined alignItems="end" css={size({ height: 10 })}>
+          {Boxes()}
+        </FlexBox>
+        <FlexBox outlined alignItems="between" css={size({ height: 10 })}>
+          {Boxes()}
+        </FlexBox>
+      </FlexBox>
+      <FlexBox outlined direction="column" alignItems="start" css={size({ width: 12, height: 26 })}>
+        {Boxes()}
+      </FlexBox>
+      <FlexBox
+        outlined
+        direction="column"
+        alignItems="center"
+        css={size({ width: 12, height: 26 })}
+      >
+        {Boxes()}
+      </FlexBox>
+      <FlexBox outlined direction="column" alignItems="end" css={size({ width: 12, height: 26 })}>
+        {Boxes()}
+      </FlexBox>
+      <FlexBox
+        outlined
+        direction="column"
+        alignItems="between"
+        css={size({ width: 12, height: 26 })}
+      >
+        {Boxes()}
+      </FlexBox>
+    </FlexBox>
+  );
+};
+
+export const Gap = () => {
+  return (
+    <Container>
+      <FlexBox justifyContent="center" rowGap={4} columnGap={8}>
+        {Boxes()}
+      </FlexBox>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  margin-top: 20px;
+
+  &:first-child {
+    margin-top: 0;
+  }
+
+  & > button {
+    margin-right: 20px;
+  }
+`;
---
+++ frontend/src/types/style.ts
@@ -0,0 +1,2 @@
+export type BorderRadiusDirectionType = 'all' | 'top' | 'bottom';
+export type AxisType = 'row' | 'column';
