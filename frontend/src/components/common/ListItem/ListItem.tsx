597edbfb81bf5c60f31cf0a4ec894c90f80dda2e
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-07-26 13:06:15 +0900

    feat: ListItem에 clickable과 divider 추가
    [#172]

--- frontend/src/components/common/ListItem/ListItem.tsx
+++ frontend/src/components/common/ListItem/ListItem.tsx
@@ -4,15 +4,26 @@ import type { ReactNode } from 'react';
 
 interface ListItemProps {
   children: ReactNode;
+  divider?: boolean;
+  clickable?: boolean;
 }
 
-const ListItemWrapper = styled.li`
-  padding: 8px 16px;
-  border-bottom: 1px solid #ccc;
+const ListItemWrapper = styled.li<ListItemProps>`
+  padding: 1rem 2rem;
+  ${({ divider }) => divider && `border-bottom: 0.0625rem solid #ccc;`}
+
+  ${({ clickable }) =>
+    clickable &&
+    `
+    cursor: pointer;
+    &:hover {
+      background-color: #f0f0f0;
+    }
+  `}
 `;
 
-const ListItem = ({ children }: ListItemProps) => {
-  return <ListItemWrapper>{children}</ListItemWrapper>;
+const ListItem = ({ children, ...props }: ListItemProps) => {
+  return <ListItemWrapper {...props}>{children}</ListItemWrapper>;
 };
 
 export default ListItem;
