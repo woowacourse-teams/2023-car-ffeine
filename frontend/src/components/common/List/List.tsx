70d6dfe77f67199e2123ae4395d03e878ae34ad0
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-07-26 13:05:48 +0900

    feat: List에 디자인 옵션 추가
    [#172]

--- frontend/src/components/common/List/List.tsx
+++ frontend/src/components/common/List/List.tsx
@@ -4,15 +4,18 @@ import type { ReactNode } from 'react';
 
 export interface ListProps {
   children: ReactNode;
+  p?: number;
+  border?: boolean;
 }
 
-const ListWrapper = styled.ul`
+const ListWrapper = styled.ul<ListProps>`
   list-style-type: none;
-  padding: 0;
+  ${({ p }) => p && `padding: ${p * 0.4}rem`};
+  ${({ border }) => border && `border: 0.01rem solid #66666666; border-radius:0.4rem;`}
 `;
 
-const List = ({ children }: ListProps) => {
-  return <ListWrapper>{children}</ListWrapper>;
+const List = ({ children, ...props }: ListProps) => {
+  return <ListWrapper {...props}>{children}</ListWrapper>;
 };
 
 export default List;
