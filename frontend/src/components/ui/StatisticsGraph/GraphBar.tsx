e7041924077d3e70a7aaf7be5f09eb4124c542a2
kyw0716 <kyw0716@naver.com>
2023-07-28 01:51:40 +0900

    feat: 누적 혼잡도 ratio가 -1일 경우 회색으로 표시하는 기능 구현

--- frontend/src/components/ui/StatisticsGraph/GraphBar.tsx
+++ frontend/src/components/ui/StatisticsGraph/GraphBar.tsx
@@ -10,21 +10,26 @@ interface GraphBarProps {
 
 const GraphBar = ({ congestionRatio, hour }: GraphBarProps) => {
   return (
-    <FlexBox>
+    <FlexBox tag="li" width="28.4rem" nowrap={true}>
       <div style={{ width: '2rem' }}>
         <Text variant="caption">{hour}</Text>
       </div>
-      <Bar congestionRatio={congestionRatio} />
+      <Bar congestionRatio={congestionRatio}>
+        {congestionRatio === -1 && <Text variant="caption">X</Text>}
+      </Bar>
     </FlexBox>
   );
 };
 
-const Bar = styled.li<Omit<GraphBarProps, 'hour'>>`
-  width: ${(props) => `calc(26rem * ${props.congestionRatio} / 100)`};
-  height: 1rem;
+const Bar = styled.div<Omit<GraphBarProps, 'hour'>>`
+  width: ${(props) =>
+    props.congestionRatio === -1 ? '100%' : `calc(100% * ${props.congestionRatio} / 100)`};
+  height: 1.2rem;
+
+  text-align: center;
 
   border-radius: 1rem;
-  background-color: #0064ff;
+  background-color: ${({ congestionRatio }) => (congestionRatio === -1 ? '#b4b4b4' : '#0064ff')};
 `;
 
 export default GraphBar;
