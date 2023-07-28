b900b18052671818c8986d63bc974abf12ecac4a
drunkenhw <drunkenhw@gmail.com>
2023-07-28 16:10:12 +0900

    feat: 충전소의 잘못된 정보 도메인 추가
    [#182]

---
+++ backend/src/main/java/com/carffeine/carffeine/station/domain/report/MisinformationDetailReport.java
@@ -0,0 +1,27 @@
+package com.carffeine.carffeine.station.domain.report;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "misinformation_detail_report")
+@Entity
+public class MisinformationDetailReport {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String category;
+    private String content;
+}
