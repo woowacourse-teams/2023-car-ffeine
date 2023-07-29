da21bcfbcbc60bc06fea46a2c4f62364e865f4d8
kyw0716 <kyw0716@naver.com>
2023-07-29 18:01:49 +0900

    refactor: Graph.BarContainer 컴포넌트 너비, 높이 고정

--- frontend/src/components/ui/Graph/BarContainer.tsx
+++ frontend/src/components/ui/Graph/BarContainer.tsx
@@ -14,7 +14,13 @@ const BarContainer = ({ align, statistics, renderBar }: BarContainerProps) => {
   const { selectedDay } = useContext(GraphContext);
 
   return (
-    <FlexBox direction={align} nowrap={true} alignItems={align === 'row' ? 'end' : 'start'}>
+    <FlexBox
+      direction={align}
+      nowrap={true}
+      alignItems={align === 'row' ? 'end' : 'start'}
+      width={align === 'column' && '28.4rem'}
+      height={align === 'row' && '28.4rem'}
+    >
       {statistics[selectedDay].map(({ hour, ratio }) => renderBar(hour, ratio))}
     </FlexBox>
   );
