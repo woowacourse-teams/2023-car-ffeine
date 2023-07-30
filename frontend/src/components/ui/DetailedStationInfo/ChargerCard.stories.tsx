44c6ef72224ac458d771ef3b0e9d87d48422e594
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-07-30 13:19:49 +0900

    test: ChargerCard 추가

---
+++ frontend/src/components/ui/DetailedStationInfo/ChargerCard.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta } from '@storybook/react';
+
+import List from '@common/List';
+
+import ChargerCard from '@ui/DetailedStationInfo/ChargerCard';
+
+const meta = {
+  title: 'UI/ChargerCard',
+  component: ChargerCard,
+} satisfies Meta<typeof ChargerCard>;
+
+export default meta;
+
+export const Default = () => {
+  return (
+    <List>
+      <ChargerCard
+        charger={{
+          type: 'DC_AC_3PHASE',
+          price: 200,
+          capacity: 3,
+          latestUpdateTime: new Date('2023-07-18T15:11:40.000Z'),
+          state: 'STANDBY',
+          method: '단독',
+        }}
+      />
+    </List>
+  );
+};
