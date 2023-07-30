ef2751521275eb89365e30eebfcf654611674be9
drunkenhw <drunkenhw@gmail.com>
2023-07-30 20:57:09 +0900

    feat: id 생성 전략 추가

--- backend/src/main/java/com/carffeine/carffeine/station/domain/report/MisinformationDetailReport.java
+++ backend/src/main/java/com/carffeine/carffeine/station/domain/report/MisinformationDetailReport.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -20,7 +21,7 @@ import javax.persistence.Table;
 public class MisinformationDetailReport {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String category;
     private String content;
