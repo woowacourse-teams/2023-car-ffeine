9388dcbbae6b86e383cf9e6792770bca6df40627
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-07-30 22:35:18 +0900

    feat: 마지막 접속 시각을 기록하는 기능 구현

--- frontend/src/components/google-maps/map/ApiKeyChecker.tsx
+++ frontend/src/components/google-maps/map/ApiKeyChecker.tsx
@@ -1,36 +1,47 @@
 import type { ReactNode } from 'react';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { getLocalStorage, setLocalStorage } from '@utils/storage';
 
-import { LOCAL_KEY_GOOGLE_MAPS_API, LOCAL_KEY_GOOGLE_MAPS_API_SAVE } from '@constants';
+import Box from '@common/Box';
+import Button from '@common/Button';
+import FlexBox from '@common/FlexBox';
+import Text from '@common/Text';
+import TextField from '@common/TextField';
+
+import {
+  LOCAL_KEY_GOOGLE_MAPS_API,
+  LOCAL_KEY_GOOGLE_MAPS_API_LAST_LOGIN,
+  LOCAL_KEY_GOOGLE_MAPS_API_SAVE,
+} from '@constants';
 
 interface ApiKeyCheckerProps {
   render: (apiKey: string) => ReactNode;
 }
 
 function ApiKeyChecker({ render }: ApiKeyCheckerProps) {
-  const inputRef = useRef<HTMLInputElement>(null);
   const [apiKey, setApiKey] = useState('');
+  const [value, setValue] = useState('');
   const [isSaveMode, setSaveMode] = useState(true);
+
   useEffect(() => {
     const isSaving = getLocalStorage<boolean>(LOCAL_KEY_GOOGLE_MAPS_API_SAVE, true);
     setSaveMode(isSaving);
     if (isSaving) {
-      const backUpKey = getLocalStorage<string>(LOCAL_KEY_GOOGLE_MAPS_API, '');
-      (inputRef.current as HTMLInputElement).value = backUpKey;
+      const backupKey = getLocalStorage<string>(LOCAL_KEY_GOOGLE_MAPS_API, '');
+      setValue(backupKey);
     }
   }, []);
 
   const onClick = () => {
-    const inputValue = inputRef.current?.value;
-    if (inputValue && inputValue.length > 0) {
+    if (value.length > 0) {
       if (isSaveMode) {
-        setLocalStorage(LOCAL_KEY_GOOGLE_MAPS_API, inputValue);
+        setLocalStorage(LOCAL_KEY_GOOGLE_MAPS_API, value);
       } else {
         setLocalStorage(LOCAL_KEY_GOOGLE_MAPS_API, '');
       }
-      setApiKey(inputValue);
+      setLocalStorage(LOCAL_KEY_GOOGLE_MAPS_API_LAST_LOGIN, new Date().getTime());
+      setApiKey(value);
     } else {
       alert('제대로 된 키를 입력하셔야 합니다.');
     }
@@ -41,24 +52,39 @@ function ApiKeyChecker({ render }: ApiKeyCheckerProps) {
       {apiKey.length > 0 ? (
         render(apiKey)
       ) : (
-        <div>
-          <div>API키를 입력하세요</div>
-          <input type="text" ref={inputRef} />
-          <button type="submit" onClick={onClick}>
-            전송
-          </button>
-          <div>
-            <input
-              type="checkbox"
-              checked={isSaveMode}
+        <FlexBox
+          className={'modal-open'}
+          justifyContent={'center'}
+          alignItems={'center'}
+          height={'100vh'}
+        >
+          <Box border p={10}>
+            <TextField
+              width={100}
+              label="google maps API키를 입력하세요"
+              value={value}
               onChange={(e) => {
-                setLocalStorage(LOCAL_KEY_GOOGLE_MAPS_API_SAVE, e.target.checked);
-                setSaveMode(e.target.checked);
+                setValue(e.target.value);
               }}
             />
-            키를 브라우저에 저장하기
-          </div>
-        </div>
+            <FlexBox justifyContent={'between'}>
+              <FlexBox alignItems={'center'}>
+                <input
+                  type="checkbox"
+                  checked={isSaveMode}
+                  onChange={(e) => {
+                    setLocalStorage(LOCAL_KEY_GOOGLE_MAPS_API_SAVE, e.target.checked);
+                    setSaveMode(e.target.checked);
+                  }}
+                />
+                <Text>키를 브라우저에 저장하기</Text>
+              </FlexBox>
+              <Button size={'md'} outlined onClick={onClick}>
+                전송하기
+              </Button>
+            </FlexBox>
+          </Box>
+        </FlexBox>
       )}
     </div>
   );
