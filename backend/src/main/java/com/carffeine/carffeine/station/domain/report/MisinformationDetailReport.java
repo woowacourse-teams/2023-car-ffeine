2d7daecbc15fab0646a6041ddba4231c944af79b
drunkenhw <drunkenhw@gmail.com>
2023-07-31 16:56:42 +0900

    feat: 제약 조건 추가

--- backend/src/main/java/com/carffeine/carffeine/station/domain/report/MisinformationDetailReport.java
+++ backend/src/main/java/com/carffeine/carffeine/station/domain/report/MisinformationDetailReport.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -23,6 +24,10 @@ public class MisinformationDetailReport {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String category;
+
+    @Column(nullable = false)
     private String content;
 }
