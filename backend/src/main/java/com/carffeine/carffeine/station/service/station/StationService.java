b367b00769a329ef55984058171202666deee00b
be-student <presentsong@naver.com>
2023-07-31 14:20:48 +0900

    feat: station 을 검색하는 기능을 추가한다

--- backend/src/main/java/com/carffeine/carffeine/station/service/station/StationService.java
+++ backend/src/main/java/com/carffeine/carffeine/station/service/station/StationService.java
@@ -15,6 +15,8 @@ import com.carffeine.carffeine.station.domain.station.Stations;
 import com.carffeine.carffeine.station.exception.StationException;
 import com.carffeine.carffeine.station.exception.StationExceptionType;
 import com.carffeine.carffeine.station.service.station.dto.CoordinateRequest;
+import com.carffeine.carffeine.station.service.station.dto.StationSearchResponse;
+import com.carffeine.carffeine.station.service.station.dto.StationsSearchResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -26,10 +28,13 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
 public class StationService {
+
+    private static final int PAGE_SIZE = 6;
 
     private final StationRepository stationRepository;
     private final PeriodicCongestionRepository periodicCongestionRepository;
@@ -86,5 +91,40 @@ public class StationService {
             return status + 1;
         }
         return status;
+    }
+
+    public StationsSearchResponse searchStations(String query, Set<String> scope, int page) {
+        List<Station> stations = stationRepository.findAllByStationNameContainingOrAddressContainingOrderByStationId(query, query);
+        List<StationSearchResponse> stationByScope = stationsToScope(stations, scope, page);
+        return new StationsSearchResponse(
+                stations.size(),
+                stationByScope
+        );
+    }
+
+    private List<StationSearchResponse> stationsToScope(List<Station> stations, Set<String> scope, int page) {
+        return stations.stream()
+                .skip((page - 1) * PAGE_SIZE)
+                .limit(PAGE_SIZE)
+                .map(station -> stationToScope(station, scope))
+                .toList();
+    }
+
+    private StationSearchResponse stationToScope(Station station, Set<String> scope) {
+        StationSearchResponse.StationSearchResponseBuilder builder = StationSearchResponse.builder();
+        if (scope.contains("stationName")) {
+            builder.stationName(station.getStationName());
+        }
+        if (scope.contains("address")) {
+            builder.address(station.getAddress());
+        }
+        if (scope.contains("latitude")) {
+            builder.latitude(station.getLatitude().getValue());
+        }
+        if (scope.contains("longitude")) {
+            builder.longitude(station.getLongitude().getValue());
+        }
+        builder.stationId(station.getStationId());
+        return builder.build();
     }
 }
