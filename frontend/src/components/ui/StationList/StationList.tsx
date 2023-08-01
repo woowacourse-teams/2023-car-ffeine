4a2ade622ebef764f5c0f88e4e0f72dc859ea23f
kyw0716 <kyw0716@naver.com>
2023-08-01 14:46:40 +0900

    refactor: 기존 스타일 재사용

--- frontend/src/components/ui/StationList/StationList.tsx
+++ frontend/src/components/ui/StationList/StationList.tsx
@@ -13,7 +13,8 @@ import { useUpdateStations } from '@hooks/useUpdateStations';
 import FlexBox from '@common/FlexBox';
 import List from '@common/List';
 import ListItem from '@common/ListItem';
-import Text from '@common/Text';
+
+import { windowPositionTriggeredByLnb } from '@style';
 
 import BriefStationInfo from '../BriefStationInfo';
 import StationCard from './StationCard';
@@ -23,7 +24,7 @@ import type { StationSummary } from 'types';
 const StationList = () => {
   const googleMap = useExternalValue(getGoogleMapStore());
 
-  const { data: stations, isSuccess, isFetching } = useStations();
+  const { data: stations, isSuccess } = useStations();
   const stationMarkers = useExternalValue(markerInstanceStore);
 
   const { infoWindowInstance, briefStationInfoRoot } = useExternalValue(
@@ -54,7 +55,7 @@ const StationList = () => {
   };
 
   return (
-    <FlexBox width="34rem" height="100vh" css={containerCss}>
+    <FlexBox width={34} height="100vh" css={[windowPositionTriggeredByLnb, containerCss]}>
       {isSuccess && (
         <List css={listContainer}>
           {stations.map((station) => {
@@ -79,14 +80,12 @@ const StationList = () => {
 };
 
 const containerCss = css`
-  position: fixed;
-  top: 0;
-  left: 7rem;
-  z-index: 999;
-
   padding: 2rem;
+
   background-color: white;
+
   border-right: 1px solid #ddd;
+  border-radius: 0;
 `;
 
 const noPadding = css`
