bfdacca18c50c51fc11e5e68dd4b6137b8c07c3b
Gabriel Ju Hyun, Yoon <gabrielyoon7@gmail.com>
2023-08-02 10:47:56 +0900

    fix: 지도 이동 시 두번 씩 fetching을 시도하려는 문제 수정 (#286)

--- frontend/src/hooks/useUpdateStations.ts
+++ frontend/src/hooks/useUpdateStations.ts
@@ -6,7 +6,6 @@ export const useUpdateStations = () => {
   const queryClient = useQueryClient();
 
   const { mutate } = useMutation(['stations'], {
-    mutationFn: fetchStation,
     onSettled: () => {
       queryClient.invalidateQueries({ queryKey: ['stations'] });
     },
