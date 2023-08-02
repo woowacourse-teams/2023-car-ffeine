6e2f879c98c4a201e5309b89484dafbfd3c9281a
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-08-02 23:18:38 +0900

    feat: Button variant 추가

--- frontend/src/components/common/ButtonNext/ButtonNext.stories.tsx
+++ frontend/src/components/common/ButtonNext/ButtonNext.stories.tsx
@@ -13,3 +13,28 @@ export default meta;
 export const Default = (args: ButtonNextProps) => {
   return <ButtonNext {...args}>Button</ButtonNext>;
 };
+
+export const Variant = () => {
+  return (
+    <>
+      <ButtonNext variant="text">Text</ButtonNext>
+      <ButtonNext variant="outlined">Outlined</ButtonNext>
+      <ButtonNext variant="contained">Contained</ButtonNext>
+    </>
+  );
+};
+export const Colors = () => {
+  return (
+    <>
+      <ButtonNext variant="contained" color="primary">
+        Contained
+      </ButtonNext>
+      <ButtonNext variant="contained" color="warning">
+        Contained
+      </ButtonNext>
+      <ButtonNext variant="contained" color="secondary">
+        Contained
+      </ButtonNext>
+    </>
+  );
+};
--- frontend/src/components/common/ButtonNext/ButtonNext.tsx
+++ frontend/src/components/common/ButtonNext/ButtonNext.tsx
@@ -1,17 +1,86 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-import type { ButtonProps } from '@common/Button/Button';
+export type Color =
+  | 'primary'
+  | 'secondary'
+  | 'success'
+  | 'info'
+  | 'warning'
+  | 'error'
+  | 'light'
+  | 'dark';
 
 export interface ButtonNextProps {
-  variant: 'text' | 'outlined' | 'contained';
+  variant?: 'text' | 'outlined' | 'contained';
+  children?: string;
+  color?: Color;
 }
 
-const ButtonNext = ({ variant }: ButtonNextProps) => {
-  return <S.Button />;
+const ButtonNext = ({ children, ...props }: ButtonNextProps) => {
+  return <S.Button {...props}>{children}</S.Button>;
 };
 
 const S = {
-  Button: styled.button<ButtonProps>``,
+  Button: styled.button<ButtonNextProps>`
+    ${({ color }) => {
+      switch (color) {
+        case 'primary':
+          return css`
+            color: #052c65;
+            background: #cfe2ff;
+            border: 1px solid #9ec5fe;
+          `;
+        case 'secondary':
+          return css`
+            color: #2b2f32;
+            background: #e9ecef;
+            border: 1px solid #c4c8cb;
+          `;
+        case 'success':
+          return css`
+            color: #0a3622;
+            background: #d1e7dd;
+            border: 1px solid #a3cfbb;
+          `;
+        case 'error':
+          return css`
+            color: #58151c;
+            background: #f8d7da;
+            border: 1px solid #f1aeb5;
+          `;
+        case 'warning':
+          return css`
+            color: #664d03;
+            background: #fff3cd;
+            border: 1px solid #ffe69c;
+          `;
+        case 'info':
+          return css`
+            color: #055160;
+            background: #cff4fc;
+            border: 1px solid #9eeaf9;
+          `;
+        case 'light':
+          return css`
+            color: #495057;
+            background: #fcfcfd;
+            border: 1px solid #e9ecef;
+          `;
+        case 'dark':
+          return css`
+            color: #495057;
+            background: #ced4da;
+            border: 1px solid #adb5bd;
+          `;
+        default:
+          return css`
+            color: #495057;
+            background: #fcfcfd;
+            border: 1px solid #e9ecef;
+          `;
+      }
+    }}
+  `,
 };
 
 export default ButtonNext;
