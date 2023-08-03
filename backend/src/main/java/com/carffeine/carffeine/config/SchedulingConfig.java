0255cf637349531fbd461dd4e4a12a887ff95e13
drunkenhw <drunkenhw@gmail.com>
2023-08-03 13:49:26 +0900

    feat: 스케줄링을 production 서버에만 사용하도록 변경
    [#333]

--- backend/src/main/java/com/carffeine/carffeine/config/SchedulingConfig.java
+++ backend/src/main/java/com/carffeine/carffeine/config/SchedulingConfig.java
@@ -1,8 +1,10 @@
 package com.carffeine.carffeine.config;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
 
+@Profile("prod")
 @EnableScheduling
 @Component
 public class SchedulingConfig {
