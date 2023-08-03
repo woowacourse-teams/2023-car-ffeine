2d851791b4f447abf11fc9dde749d11aa80a49ff
한우석 <106640954+drunkenhw@users.noreply.github.com>
2023-08-03 13:51:25 +0900

    fix: cors origin 설정 변경 (#331)
    [#318]

--- backend/src/main/java/com/carffeine/carffeine/web/CorsFilter.java
+++ backend/src/main/java/com/carffeine/carffeine/web/CorsFilter.java
@@ -13,7 +13,7 @@ import java.util.Objects;
 public class CorsFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
