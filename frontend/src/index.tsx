157a4c84b6621fcfa3de857efa220be4b974b492
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-08-03 20:20:44 +0900

    refactor: api key 검사기 제거
    [#345]

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,3 +1,5 @@
+import * as process from 'process';
+
 import { createRoot } from 'react-dom/client';
 
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
@@ -26,7 +28,14 @@ const main = async () => {
   root.render(
     <QueryClientProvider client={queryClient}>
       <GlobalStyle />
-      <ApiKeyChecker render={(apiKey) => <App apiKey={apiKey} />} />
+      {/*<ApiKeyChecker render={(apiKey) => <App apiKey={apiKey} />} />*/}
+      <App
+        apiKey={
+          process.env.NODE_ENV === 'development'
+            ? process.env.GOOGLE_MAPS_API_KEY_DEV
+            : process.env.GOOGLE_MAPS_API_KEY_PROD
+        }
+      />
       <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
