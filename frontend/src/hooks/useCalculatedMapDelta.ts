b99877ff6e6f3571972a4ea58fdb49323f729a68
kyw0716 <kyw0716@naver.com>
2023-08-03 14:33:13 +0900

    feat: 구한 비율 만큼의 델타 값 구하기
    [#332]

--- frontend/src/hooks/useCalculatedMapDelta.ts
+++ frontend/src/hooks/useCalculatedMapDelta.ts
@@ -1,16 +1,21 @@
 import { useExternalValue } from '@utils/external-state';
+import { getDisplayPosition } from '@utils/google-maps';
 
 import { browserWidthStore, navigatorAccordionWidthStore } from '@stores/componentWidthStore';
+import { getGoogleMapStore } from '@stores/googleMapStore';
 
 export const useCalculatedMapDelta = () => {
   const navigatorAccordionWidth = useExternalValue(navigatorAccordionWidthStore);
   const browserWidth = useExternalValue(browserWidthStore);
+  const map = useExternalValue(getGoogleMapStore());
 
   if (browserWidth === null) {
     return 0;
   }
 
-  const navigatorAccordionWidthRatio = (navigatorAccordionWidth / browserWidth) * 100;
+  const navigatorAccordionWidthRatio = navigatorAccordionWidth / browserWidth;
+
+  console.log(getDisplayPosition(map).longitudeDelta * 2 * navigatorAccordionWidthRatio);
 
   return navigatorAccordionWidthRatio;
 };
