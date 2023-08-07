35c1bc5f989247e4c92dcf576d0135d958e60fd5
kiarakim <kiarakim413@gmail.com>
2023-08-07 16:51:52 +0900

    refactor: @EnableJpaAuditing 제거

--- backend/src/main/java/com/carffeine/carffeine/CarffeineApplication.java
+++ backend/src/main/java/com/carffeine/carffeine/CarffeineApplication.java
@@ -2,11 +2,9 @@ package com.carffeine.carffeine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.TimeZone;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class CarffeineApplication {
 
