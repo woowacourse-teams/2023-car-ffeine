566972b7b87b3dd9990d0cb1e71f56693c9f2809
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-08-08 01:35:34 +0900

    feat: 충전소 혼잡도 조회 로딩화면 스켈레톤 적용
    [#388]

---
+++ frontend/src/components/ui/StationDetailsWindow/congestion/CongestionStatisticsSkeleton.tsx
@@ -0,0 +1,44 @@
+import { css } from 'styled-components';
+
+import Box from '@common/Box';
+import FlexBox from '@common/FlexBox';
+import Skeleton from '@common/Skeleton';
+import Text from '@common/Text';
+
+const CongestionStatisticsSkeleton = () => {
+  return (
+    <FlexBox direction="column" gap={4}>
+      <FlexBox nowrap>
+        <Skeleton width="100%" height="2.8rem" />
+        <Skeleton width="100%" height="2.8rem" />
+      </FlexBox>
+      <Box mb={4}>
+        <FlexBox justifyContent="center">
+          {Array(7)
+            .fill(undefined)
+            .map((v, i) => (
+              <Skeleton key={i} width="4.2rem" height="4.2rem" borderRadius="50%" />
+            ))}
+        </FlexBox>
+      </Box>
+      <FlexBox direction="column">
+        {Array(24)
+          .fill(undefined)
+          .map((v, i) => (
+            <Box key={i} css={graphCss}>
+              <Text variant="caption">{i}</Text>
+              <Skeleton width="90%" />
+            </Box>
+          ))}
+      </FlexBox>
+    </FlexBox>
+  );
+};
+
+const graphCss = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export default CongestionStatisticsSkeleton;
