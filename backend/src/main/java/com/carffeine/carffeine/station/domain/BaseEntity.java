2717aaea14c4b4be6c027e5387062843c11470e9
kiarakim <kiarakim413@gmail.com>
2023-08-07 17:14:01 +0900

    refactor: Entity 생성 및 수정 시 시간 자동 저장하는 어노테이션 추가

--- backend/src/main/java/com/carffeine/carffeine/station/domain/BaseEntity.java
+++ backend/src/main/java/com/carffeine/carffeine/station/domain/BaseEntity.java
@@ -1,30 +1,18 @@
 package com.carffeine.carffeine.station.domain;
 
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
-import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.PreUpdate;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
 public abstract class BaseEntity {
 
-    @NotNull
-    @Column(updatable = false)
+    @CreatedDate
     private LocalDateTime createdAt;
-    @NotNull
+    @LastModifiedDate
     private LocalDateTime updatedAt;
-
-    public BaseEntity() {
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    public void update() {
-        this.updatedAt = LocalDateTime.now();
-    }
 }
