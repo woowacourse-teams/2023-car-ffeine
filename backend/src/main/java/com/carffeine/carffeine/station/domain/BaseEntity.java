8892723e40b95d01271f1c88621bdc940271639a
kiarakim <kiarakim413@gmail.com>
2023-08-07 16:52:24 +0900

    refactor: 개행 추가

--- backend/src/main/java/com/carffeine/carffeine/station/domain/BaseEntity.java
+++ backend/src/main/java/com/carffeine/carffeine/station/domain/BaseEntity.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 public abstract class BaseEntity {
+
     @NotNull
     @Column(updatable = false)
     private LocalDateTime createdAt;
