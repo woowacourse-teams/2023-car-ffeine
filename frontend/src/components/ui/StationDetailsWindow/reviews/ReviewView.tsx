e7cdf59086909bb747f3e7ccfef7e4e19f60b47d
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-08-08 22:32:36 +0900

    feat: UserRatings 컴포넌트 추가
    [#412]

---
+++ frontend/src/components/ui/StationDetailsWindow/reviews/ReviewView.tsx
@@ -0,0 +1,13 @@
+import Box from '@common/Box';
+
+import UserRatings from '@ui/StationDetailsWindow/reviews/UserRatings';
+
+const ReviewView = () => {
+  return (
+    <Box my={5}>
+      <UserRatings />
+    </Box>
+  );
+};
+
+export default ReviewView;
---
+++ frontend/src/components/ui/StationDetailsWindow/reviews/UserRatings.tsx
@@ -0,0 +1,7 @@
+import Alert from '@common/Alert';
+
+const UserRatings = () => {
+  return <Alert color="light" text="n명의 사용자들에게 평균 n점을 받은 충전소입니다." />;
+};
+
+export default UserRatings;
