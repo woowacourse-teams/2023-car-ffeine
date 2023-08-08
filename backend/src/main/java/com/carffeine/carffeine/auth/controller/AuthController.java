c0d88757c9558e905ee91bc7458e11eee5f3cea3
drunkenhw <drunkenhw@gmail.com>
2023-08-08 13:07:13 +0900

    feat: method get으로 변경
    [#87]

--- backend/src/main/java/com/carffeine/carffeine/auth/controller/AuthController.java
+++ backend/src/main/java/com/carffeine/carffeine/auth/controller/AuthController.java
@@ -4,29 +4,28 @@ import com.carffeine.carffeine.auth.controller.dto.LoginUriResponse;
 import com.carffeine.carffeine.auth.controller.dto.TokenResponse;
 import com.carffeine.carffeine.auth.service.AuthService;
 import com.carffeine.carffeine.auth.service.dto.OAuthLoginRequest;
-import com.carffeine.carffeine.auth.service.dto.OAuthUriRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping("/oauth/{provider}/login-uri")
+    @GetMapping("/oauth/{provider}/login-uri")
     public ResponseEntity<LoginUriResponse> getRedirectUri(
             @PathVariable String provider,
-            @RequestBody OAuthUriRequest request
+            @RequestParam String redirectUri
     ) {
-        String redirectUri = authService.loginUri(request, provider);
-        return ResponseEntity.ok(new LoginUriResponse(redirectUri));
+        String loginUri = authService.loginUri(redirectUri, provider);
+        return ResponseEntity.ok(new LoginUriResponse(loginUri));
     }
 
     @PostMapping("/oauth/{provider}/login")
