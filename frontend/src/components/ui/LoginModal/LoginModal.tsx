648de0bacba61dd4297ff3c6c54a298a0d080292
YoungWoo Kim <77326660+kyw0716@users.noreply.github.com>
2023-08-09 14:03:25 +0900

    feat: 간편 로그인 기능을 구현한다 (#420)
    * feat: 로그인 기능 구현(미완)
    
    [#397]
    
    * feat: 로그인 기능 구현
    
    [#397]
    
    * refactor: 로그인 관련 로직 함수 분리
    
    [#397]
    
    * feat: 로그인 모달 추가
    
    [#397]
    
    * design: 로그인 모달 디자인 수정
    
    [#397]
    
    * feat: 로그인 redirect 발생시 msw 중지 시키는 기능 구현
    
    [#397]
    
    * refactor: 로그인 에러 처리
    
    - loginUri response를 받아올 때 발생하는 에러에 대한 처리 추가
    
    [#397]
    
    * chore: 웹팩 설정 수정
    
    [#397]

---
+++ frontend/src/components/ui/LoginModal/LoginModal.tsx
@@ -0,0 +1,65 @@
+import { UserCircleIcon } from '@heroicons/react/24/outline';
+import { css } from 'styled-components';
+
+import { redirectToLoginPage } from '@utils/login';
+
+import ButtonNext from '@common/ButtonNext';
+import FlexBox from '@common/FlexBox';
+import Modal from '@common/Modal';
+import type { ModalProps } from '@common/Modal/Modal';
+import Text from '@common/Text';
+
+import GoogleLoginButton from '@assets/google-login-button.svg';
+
+type Props = Omit<ModalProps, 'children'>;
+
+const LoginModal = ({ isOpen, onClose }: Props) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <FlexBox width="100%" justifyContent="center" alignItems="center" css={containerCss}>
+        <FlexBox justifyContent="center" alignItems="center" css={loginButtonContainerCss}>
+          <FlexBox width={7} height={7} justifyContent="center" alignItems="center" css={iconCss}>
+            <UserCircleIcon width="7rem" stroke="lightgrey" />
+          </FlexBox>
+          <FlexBox direction="column" alignItems="center" gap={5}>
+            <Text variant="h4" color="#333">
+              간편 로그인
+            </Text>
+            <ButtonNext noTheme onClick={() => redirectToLoginPage('google')}>
+              <img width="80%" src={GoogleLoginButton} alt="구글 로그인 버튼 이미지" />
+            </ButtonNext>
+          </FlexBox>
+        </FlexBox>
+      </FlexBox>
+    </Modal>
+  );
+};
+
+const containerCss = css`
+  padding: 5rem 0;
+`;
+
+const loginButtonContainerCss = css`
+  width: 80%;
+  height: 25rem;
+
+  border: 1px solid lightgrey;
+  border-radius: 1rem;
+
+  position: relative;
+`;
+
+const iconCss = css`
+  border-radius: 50%;
+
+  position: absolute;
+  top: -3rem;
+  left: 50%;
+  transform: translate(-50%);
+
+  padding: 0;
+
+  background-color: #fff;
+`;
+
+export default LoginModal;
