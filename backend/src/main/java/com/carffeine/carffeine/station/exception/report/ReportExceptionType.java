42df77fbe07fc438b74471775278908b2c7f5690
drunkenhw <drunkenhw@gmail.com>
2023-08-10 11:51:56 +0900

    refactor: 예외 메세지 수정
    [#407]

--- backend/src/main/java/com/carffeine/carffeine/station/exception/report/ReportExceptionType.java
+++ backend/src/main/java/com/carffeine/carffeine/station/exception/report/ReportExceptionType.java
@@ -6,7 +6,7 @@ import com.carffeine.carffeine.common.exception.Status;
 public enum ReportExceptionType implements ExceptionType {
 
     DUPLICATE_REPORT(Status.INVALID, 2001, "이미 신고한 충전소는 신고가 불가합니다"),
-    NOT_FOUND(Status.NOT_FOUND, 2002, "이미 신고한 충전소는 신고가 불가합니다"),
+    NOT_FOUND(Status.NOT_FOUND, 2002, "요청하신 id의 신고 정보를 찾을 수 없습니다."),
     ;
 
     private final Status status;
