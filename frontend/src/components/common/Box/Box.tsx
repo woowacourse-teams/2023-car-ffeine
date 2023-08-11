26cf3c9321722c8a5b154be313ea24f707ef2af2
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-08-11 17:58:54 +0900

    refactor: spacing 관련 코드 분리
    [#435]

--- frontend/src/components/common/Box/Box.tsx
+++ frontend/src/components/common/Box/Box.tsx
@@ -3,23 +3,12 @@ import styled from 'styled-components';
 
 import type { HTMLAttributes, ReactNode } from 'react';
 
-export interface BoxProps extends HTMLAttributes<HTMLDivElement> {
+import type { SpacingProps } from '@common/systems';
+import { spacing } from '@common/systems';
+
+export interface BoxProps extends HTMLAttributes<HTMLDivElement>, SpacingProps {
   children?: ReactNode;
   border?: boolean;
-  p?: number;
-  px?: number;
-  py?: number;
-  pl?: number;
-  pr?: number;
-  pt?: number;
-  pb?: number;
-  m?: number;
-  mx?: number;
-  my?: number;
-  ml?: number;
-  mr?: number;
-  mt?: number;
-  mb?: number;
   height?: number;
   minHeight?: number;
   maxHeight?: number;
@@ -37,47 +26,9 @@ export interface BoxProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 const BoxWrapper = styled.div<BoxProps>`
+  ${spacing}
+
   ${({ border }) => border && `border: 0.01rem solid #66666666; border-radius:0.4rem;`}
-
-  ${({ p }) => p && `padding: ${p * 0.4}rem`};
-
-  ${({ px }) =>
-    px &&
-    `
-      padding-left: ${px * 0.4}rem;
-      padding-right: ${px * 0.4}rem;
-    `}
-  ${({ py }) =>
-    py &&
-    `
-      padding-top: ${py * 0.4}rem;
-      padding-bottom: ${py * 0.4}rem;
-    `}
-
-  ${({ pl }) => pl && `padding-left: ${pl * 0.4}rem`};
-  ${({ pr }) => pr && `padding-right: ${pr * 0.4}rem`};
-  ${({ pt }) => pt && `padding-top: ${pt * 0.4}rem`};
-  ${({ pb }) => pb && `padding-bottom: ${pb * 0.4}rem`};
-
-  ${({ m }) => m && `margin: ${m * 0.4}rem`};
-
-  ${({ mx }) =>
-    mx &&
-    `
-      margin-left: ${mx * 0.4}rem;
-      margin-right: ${mx * 0.4}rem;
-    `}
-  ${({ my }) =>
-    my &&
-    `
-      margin-top: ${my * 0.4}rem;
-      margin-bottom: ${my * 0.4}rem;
-    `}
-
-  ${({ ml }) => ml && `margin-left: ${ml * 0.4}rem`};
-  ${({ mr }) => mr && `margin-right: ${mr * 0.4}rem`};
-  ${({ mt }) => mt && `margin-top: ${mt * 0.4}rem`};
-  ${({ mb }) => mb && `margin-bottom: ${mb * 0.4}rem`};
 
   ${({ height }) => height && `height: ${height * 0.4}rem`};
   ${({ minHeight }) => minHeight && `min-height: ${minHeight * 0.4}rem`};
---
+++ frontend/src/components/common/systems.ts
@@ -0,0 +1,60 @@
+import { css } from 'styled-components';
+
+export interface SpacingProps {
+  p?: number;
+  px?: number;
+  py?: number;
+  pl?: number;
+  pr?: number;
+  pt?: number;
+  pb?: number;
+  m?: number;
+  mx?: number;
+  my?: number;
+  ml?: number;
+  mr?: number;
+  mt?: number;
+  mb?: number;
+}
+
+export const spacing = css<SpacingProps>`
+  ${({ p }) => p && `padding: ${p * 0.4}rem`};
+
+  ${({ px }) =>
+    px &&
+    `
+      padding-left: ${px * 0.4}rem;
+      padding-right: ${px * 0.4}rem;
+    `}
+  ${({ py }) =>
+    py &&
+    `
+      padding-top: ${py * 0.4}rem;
+      padding-bottom: ${py * 0.4}rem;
+    `}
+
+  ${({ pl }) => pl && `padding-left: ${pl * 0.4}rem`};
+  ${({ pr }) => pr && `padding-right: ${pr * 0.4}rem`};
+  ${({ pt }) => pt && `padding-top: ${pt * 0.4}rem`};
+  ${({ pb }) => pb && `padding-bottom: ${pb * 0.4}rem`};
+
+  ${({ m }) => m && `margin: ${m * 0.4}rem`};
+
+  ${({ mx }) =>
+    mx &&
+    `
+      margin-left: ${mx * 0.4}rem;
+      margin-right: ${mx * 0.4}rem;
+    `}
+  ${({ my }) =>
+    my &&
+    `
+      margin-top: ${my * 0.4}rem;
+      margin-bottom: ${my * 0.4}rem;
+    `}
+
+  ${({ ml }) => ml && `margin-left: ${ml * 0.4}rem`};
+  ${({ mr }) => mr && `margin-right: ${mr * 0.4}rem`};
+  ${({ mt }) => mt && `margin-top: ${mt * 0.4}rem`};
+  ${({ mb }) => mb && `margin-bottom: ${mb * 0.4}rem`};
+`;
