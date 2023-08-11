f371ffbb09dace17a873a2752bbec4a7aa5d8443
YoungWoo Kim <77326660+kyw0716@users.noreply.github.com>
2023-08-11 17:55:04 +0900

    fix: 스토리북 빌드 오류 수정 (#449)
    [#448]

--- frontend/src/mocks/browser.ts
+++ frontend/src/mocks/browser.ts
@@ -1,23 +1,5 @@
 import { setupWorker } from 'msw';
 
-import { memberHandlers } from './handlers/memberHandlers';
-import { stationReportHandlers } from './handlers/station-details/reports/stationReportHandlers';
-import { stationReviewHandlers } from './handlers/station-details/reviews/stationReviewHandlers';
-import { stationDetailHandlers } from './handlers/station-details/stationDetailHandlers';
-import { statisticsHandlers } from './handlers/station-details/statisticsHandlers';
-import { memberFilterHandlers } from './handlers/station-filters/memberFilterHandlers';
-import { serverFilterHandlers } from './handlers/station-filters/serverFilterHandlers';
-import { stationHandlers } from './handlers/station-markers/stationHandlers';
-import { stationSearchHandlers } from './handlers/stationSearchHandlers';
+import { handlers } from './handlers';
 
-export const worker = setupWorker(
-  ...memberHandlers,
-  ...stationSearchHandlers,
-  ...stationHandlers,
-  ...stationDetailHandlers,
-  ...statisticsHandlers,
-  ...memberFilterHandlers,
-  ...serverFilterHandlers,
-  ...stationReportHandlers,
-  ...stationReviewHandlers
-);
+export const worker = setupWorker(...handlers);
