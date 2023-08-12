51dbf309f5e920d71f8f20b10a50c0935d39f117
kiarakim <kiarakim413@gmail.com>
2023-08-12 15:58:06 +0900

    refactor: 토큰 검증을 JwtProvider가 하도록 변경

--- backend/src/main/java/com/carffeine/carffeine/auth/exception/AuthExceptionType.java
+++ backend/src/main/java/com/carffeine/carffeine/auth/exception/AuthExceptionType.java
@@ -6,6 +6,11 @@ import com.carffeine.carffeine.common.exception.Status;
 public enum AuthExceptionType implements ExceptionType {
 
     INVALID_AUTH_PROVIDER(Status.UNAUTHORIZED, 2001, "지원하지 않는 로그인 플랫폼입니다"),
+    SIGNITURE_NOT_FOUND(Status.INVALID, 2002, "서명을 확인하지 못했습니다"),
+    MALFORMED_TOKEN(Status.INVALID, 2003, "토큰의 길이 및 형식이 올바르지 않습니다"),
+    EXPIRED_TOKEN(Status.INVALID, 2004, "이미 만료된 토큰입니다"),
+    UNSUPPORTED_TOKEN(Status.INVALID, 2005, "지원되지 않는 토큰입니다"),
+    INVALID_TOKEN(Status.INVALID, 2006, "토큰이 유효하지 않습니다")
     ;
 
     private final Status status;
