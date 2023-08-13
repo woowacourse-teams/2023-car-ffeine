a8a4567c08a043f65623642b64802d4fde20e778
drunkenhw <drunkenhw@gmail.com>
2023-08-13 15:38:20 +0900

    feat: flyway 적용
    [#463]

---
+++ backend/src/main/resources/db/migration/V3__add_review.sql
@@ -0,0 +1,23 @@
+create table review
+(
+    id         bigint       not null auto_increment,
+    created_at datetime,
+    updated_at datetime,
+    content    varchar(255) not null,
+    is_deleted bit          not null,
+    ratings    integer      not null,
+    member_id  bigint       not null,
+    station_id varchar(255) not null,
+    primary key (id)
+) engine=InnoDB;
+
+alter table review
+    add constraint FKk0ccx5i4ci2wd70vegug074w1
+        foreign key (member_id)
+            references member (id);
+
+alter table review
+    add constraint FKhid46v3nj14ymk050qbpyrb5c
+        foreign key (station_id)
+            references charge_station (station_id);
+
