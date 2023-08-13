f6e6e8a03324195f6c7bc4d1276eea671202a770
Gabriel Ju Hyun, Yoon <gabrielyoon7@gmail.com>
2023-08-13 16:49:13 +0900

    refactor: 평균 별점 API 수정을 대응한다
    [#466]

--- frontend/src/components/ui/StationDetailsWindow/reviews/previews/ReviewPreviewSkeleton.tsx
+++ frontend/src/components/ui/StationDetailsWindow/reviews/previews/ReviewPreviewSkeleton.tsx
@@ -1,21 +1,7 @@
-import Box from '@common/Box';
-import FlexBox from '@common/FlexBox';
-import Skeleton from '@common/Skeleton';
-
 import ReviewCardsLoading from '@ui/StationDetailsWindow/reviews/ReviewCardsLoading';
 
 const ReviewPreviewSkeleton = () => {
-  return (
-    <Box my={5}>
-      <Box px={4}>
-        <FlexBox justifyContent="between">
-          <Skeleton width="7rem" height="2rem" />
-          <Skeleton width="5rem" height="2rem" />
-        </FlexBox>
-      </Box>
-      <ReviewCardsLoading count={3} />
-    </Box>
-  );
+  return <ReviewCardsLoading count={3} />;
 };
 
 export default ReviewPreviewSkeleton;
--- frontend/src/components/ui/StationDetailsWindow/reviews/previews/UserRatings.tsx
+++ frontend/src/components/ui/StationDetailsWindow/reviews/previews/UserRatings.tsx
@@ -1,22 +1,46 @@
 import { StarIcon } from '@heroicons/react/24/solid';
 
+import { useReviewRatings } from '@hooks/tanstack-query/station-details/reviews/useReviewRatings';
+
 import Box from '@common/Box';
 import FlexBox from '@common/FlexBox';
 import Text from '@common/Text';
 
+import UserRatingsSkeleton from '@ui/StationDetailsWindow/reviews/previews/UserRatingsSkeleton';
+
 export interface UserRatingsProps {
-  ratings: number;
-  counts: number;
+  stationId: string;
 }
 
-const UserRatings = ({ ratings, counts }: UserRatingsProps) => {
+const UserRatings = ({ stationId }: UserRatingsProps) => {
+  const {
+    data: totalRatings,
+    isLoading: isReviewRatingsLoading,
+    isError: isReviewRatingsError,
+    error: reviewRatingsError,
+  } = useReviewRatings(stationId);
+
+  if (isReviewRatingsLoading) {
+    return <UserRatingsSkeleton />;
+  }
+
+  if (isReviewRatingsError) {
+    return (
+      <>
+        <Text variant="title">ReviewPreview Error!</Text>
+        <Text variant="subtitle">reviewRatingsError</Text>
+        <Text>{JSON.stringify(reviewRatingsError)}</Text>
+      </>
+    );
+  }
+
   return (
     <Box mb={5} px={4}>
       <FlexBox justifyContent="between" alignItems="center">
         <Text variant="title">충전소 후기 </Text>
         <Text variant="subtitle">
           <StarIcon width={14} display="inline-block" />
-          {ratings} ({counts}명)
+          {totalRatings.totalRatings} ({totalRatings.totalCount}명)
         </Text>
       </FlexBox>
     </Box>
