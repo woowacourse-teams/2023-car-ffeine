379da09d700551120a337d2b7a6551cead0d56a0
kyw0716 <kyw0716@naver.com>
2023-08-14 20:00:59 +0900

    fix: 충전소 정보 조회 오류 수정
    [#485]

--- frontend/src/utils/request-query-params/index.ts
+++ frontend/src/utils/request-query-params/index.ts
@@ -1,6 +1,11 @@
 export const getQueryFormattedUrl = (queryObject: { [key: string]: string }) => {
   const queryFormattedUrl = Object.entries(queryObject).map(([key, value]) => {
     if (value === '') return '';
+
+    if (key === 'companies') {
+      return `companyNames=${value}`;
+    }
+
     return `${key}=${value}`;
   });
 
