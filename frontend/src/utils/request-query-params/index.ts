c352fea89f8100f64e45e10c000f2940a48f529a
kyw0716 <kyw0716@naver.com>
2023-08-14 21:01:10 +0900

    feat: 유저 차량 필터링 오류 수정
    [#485]

--- frontend/src/utils/request-query-params/index.ts
+++ frontend/src/utils/request-query-params/index.ts
@@ -2,10 +2,6 @@ export const getQueryFormattedUrl = (queryObject: { [key: string]: string }) => 
   const queryFormattedUrl = Object.entries(queryObject).map(([key, value]) => {
     if (value === '') return '';
 
-    if (key === 'companies') {
-      return `companyNames=${value}`;
-    }
-
     return `${key}=${value}`;
   });
 
