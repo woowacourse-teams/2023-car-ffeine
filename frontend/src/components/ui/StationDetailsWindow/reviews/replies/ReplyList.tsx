1dc174478ebe21d72e60a0cfc5a5857f7706bdae
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-08-15 04:14:34 +0900

    design: 답글 리스트 디자인 개선
    [#490]

--- frontend/src/components/ui/StationDetailsWindow/reviews/replies/ReplyList.tsx
+++ frontend/src/components/ui/StationDetailsWindow/reviews/replies/ReplyList.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { useInfiniteReplies } from '@hooks/tanstack-query/station-details/reviews/useInfiniteReplies';
 
+import Box from '@common/Box';
 import ButtonNext from '@common/ButtonNext';
 import Text from '@common/Text';
 
@@ -20,7 +21,7 @@ const ReplyList = ({ stationId, reviewId }: ReplyListProps) => {
   return (
     <>
       {status === 'loading' ? (
-        <ReplyListLoading count={10} />
+        <ReplyListLoading count={5} />
       ) : status === 'error' ? (
         <Text variant="caption" align="center">
           Error: {JSON.stringify(error)}
@@ -40,19 +41,22 @@ const ReplyList = ({ stationId, reviewId }: ReplyListProps) => {
             </div>
           ))}
           {isFetchingNextPage && <ReplyListLoading count={5} />}
-          <ButtonNext
-            size="xs"
-            variant="contained"
-            onClick={() => fetchNextPage()}
-            disabled={!hasNextPage || isFetchingNextPage}
-            fullWidth
-          >
-            {isFetchingNextPage
-              ? '로딩중...'
-              : hasNextPage
-              ? '답글 더 보기'
-              : '더 이상 답글이 없습니다.'}
-          </ButtonNext>
+          <Box pl={8} pr={4}>
+            <ButtonNext
+              size="xs"
+              color="secondary"
+              variant="contained"
+              onClick={() => fetchNextPage()}
+              disabled={!hasNextPage || isFetchingNextPage}
+              fullWidth
+            >
+              {isFetchingNextPage
+                ? '로딩중...'
+                : hasNextPage
+                ? '답글 더 보기'
+                : '더 이상 답글이 없습니다.'}
+            </ButtonNext>
+          </Box>
         </>
       )}
     </>
