30acbaa1181a58e29369ca0e5b2f8b61e0e21e54
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-08-14 17:01:29 +0900

    refactor: 불필요한 import 제거
    [#480]

--- frontend/src/hooks/tanstack-query/station-details/reviews/useRemoveReview.ts
+++ frontend/src/hooks/tanstack-query/station-details/reviews/useRemoveReview.ts
@@ -1,14 +1,11 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-import { getLocalStorage } from '@utils/storage';
-
 import { serverStore } from '@stores/config/serverStore';
 import { toastActions } from '@stores/layout/toastStore';
 import { memberTokenStore } from '@stores/login/memberTokenStore';
 
-import { DEFAULT_TOKEN, SERVERS } from '@constants';
+import { SERVERS } from '@constants';
 import { QUERY_KEY_STATION_PREVIEWS, QUERY_KEY_STATION_REVIEWS } from '@constants/queryKeys';
-import { LOCAL_KEY_TOKEN } from '@constants/storageKeys';
 
 export interface FetchRemoveReviewRequest {
   reviewId: number;
