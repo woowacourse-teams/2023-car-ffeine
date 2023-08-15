2bb5f9e12b982a996d706af9f52bea3859c2baac
drunkenhw <drunkenhw@gmail.com>
2023-08-15 14:47:50 +0900

    feat: form data 형식으로 받게끔 수정
    [#498]

--- backend/src/main/java/com/carffeine/carffeine/admin/controller/AdminStationController.java
+++ backend/src/main/java/com/carffeine/carffeine/admin/controller/AdminStationController.java
@@ -14,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,7 +54,7 @@ public class AdminStationController {
     public ResponseEntity<Void> updateStation(
             @AuthMember Long memberId,
             @PathVariable String stationId,
-            @RequestBody StationUpdateRequest request
+            StationUpdateRequest request
     ) {
         adminStationService.updateStation(memberId, stationId, request);
         return ResponseEntity.noContent().build();
