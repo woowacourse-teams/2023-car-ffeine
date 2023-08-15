4aae74a8483d97c4ed0084413e2ff6685b401eaa
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-08-16 02:24:25 +0900

    test: calculateLatestUpdateTime 테스트
    [#512]

---
+++ frontend/src/components/ui/StationDetailsWindow/chargers/ChargerCard.test.tsx
@@ -0,0 +1,35 @@
+import { calculateLatestUpdateTime } from '@utils/index';
+
+describe('calculateLatestUpdateTime test', () => {
+  it('1분 미만의 시간 차이일 때 "방금 전"을 표시합니다', () => {
+    const currentDateTime = new Date();
+    const latestUpdatedDateTime = new Date(currentDateTime);
+    latestUpdatedDateTime.setSeconds(currentDateTime.getSeconds() - 30);
+
+    expect(calculateLatestUpdateTime(latestUpdatedDateTime.toISOString())).toBe('방금 전');
+  });
+
+  it('1시간 미만의 시간 차이일 때 "n분 전"을 표시합니다', () => {
+    const currentDateTime = new Date();
+    const latestUpdatedDateTime = new Date(currentDateTime);
+    latestUpdatedDateTime.setMinutes(currentDateTime.getMinutes() - 45);
+
+    expect(calculateLatestUpdateTime(latestUpdatedDateTime.toISOString())).toBe('45분 전');
+  });
+
+  it('1일 미만의 시간 차이일 때 "n시간 전"을 표시합니다', () => {
+    const currentDateTime = new Date();
+    const latestUpdatedDateTime = new Date(currentDateTime);
+    latestUpdatedDateTime.setHours(currentDateTime.getHours() - 12);
+
+    expect(calculateLatestUpdateTime(latestUpdatedDateTime.toISOString())).toBe('12시간 전');
+  });
+
+  it('1일 이상의 시간 차이일 때 "n일 전"을 표시합니다', () => {
+    const currentDateTime = new Date();
+    const latestUpdatedDateTime = new Date(currentDateTime);
+    latestUpdatedDateTime.setDate(currentDateTime.getDate() - 3);
+
+    expect(calculateLatestUpdateTime(latestUpdatedDateTime.toISOString())).toBe('3일 전');
+  });
+});
