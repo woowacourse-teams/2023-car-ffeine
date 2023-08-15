80a948b8a093b9db2c9d554335736f21f1c0b5e5
kyw0716 <kyw0716@naver.com>
2023-08-15 17:38:38 +0900

    refactor: 멤버의 차 아이디가 없는 경우 에러 처리
    [#506]

--- frontend/src/hooks/tanstack-query/station-filters/useCarFilters.ts
+++ frontend/src/hooks/tanstack-query/station-filters/useCarFilters.ts
@@ -6,13 +6,25 @@ import { memberInfoStore } from '@stores/login/memberInfoStore';
 import { SERVERS } from '@constants';
 import { QUERY_KEY_MEMBER_CAR_FILTERS } from '@constants/queryKeys';
 
-const fetchCarFilters = async () => {
+import type { StationFilters } from '@type';
+
+const fetchCarFilters = async (): Promise<StationFilters> => {
   const mode = serverStore.getState();
   const memberInfo = memberInfoStore.getState();
 
-  const carFilters = await fetch(`${SERVERS[mode]}/cars/${memberInfo.car.carId}/filters`).then(
-    (response) => response.json()
-  );
+  if (memberInfo.car === undefined || memberInfo.car === null) {
+    return new Promise((resolve) =>
+      resolve({
+        capacities: [],
+        companies: [],
+        connectorTypes: [],
+      })
+    );
+  }
+
+  const carFilters = await fetch(
+    `${SERVERS[mode]}/cars/${memberInfo.car.carId}/filters`
+  ).then<StationFilters>((response) => response.json());
 
   return carFilters;
 };
