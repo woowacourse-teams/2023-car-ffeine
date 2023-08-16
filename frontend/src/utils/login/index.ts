acca6c30816d5def676e2c617820f805ef1b1575
YoungWoo Kim <77326660+kyw0716@users.noreply.github.com>
2023-08-16 17:42:40 +0900

    fix: 로그인 오류 수정 (#539)

--- frontend/src/utils/login/index.ts
+++ frontend/src/utils/login/index.ts
@@ -61,8 +61,8 @@ export const logout = () => {
 };
 
 export const getRedirectUri = () => {
-  const isProductionServer = process.env.NODE_ENV === 'production';
-  const isDevServer = window.location.href.match(/dev.carffe.in/) !== null;
+  const isProductionServer = window.location.href.match(/https:\/\/carffe.in/) !== null;
+  const isDevServer = window.location.href.match(/https:\/\/dev.carffe.in/) !== null;
 
   if (isProductionServer) {
     return `https://carffe.in`;
@@ -76,8 +76,8 @@ export const getRedirectUri = () => {
 };
 
 export const getAPIEndPoint = () => {
-  const isProductionServer = process.env.NODE_ENV === 'production';
-  const isDevServer = window.location.href.match(/dev.carffe.in/) !== null;
+  const isProductionServer = window.location.href.match(/https:\/\/carffe.in/) !== null;
+  const isDevServer = window.location.href.match(/https:\/\/dev.carffe.in/) !== null;
 
   if (isProductionServer) {
     return SERVERS['production'];
