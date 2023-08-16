81077277d65bb6d7a148ad35be5795c7c5cd86fa
kyw0716 <kyw0716@naver.com>
2023-08-17 02:21:23 +0900

    refactor: fetchMemberFilters 메서드 에러 처리 추가
    [#543]

--- frontend/src/hooks/tanstack-query/station-filters/useMemberFilters.ts
+++ frontend/src/hooks/tanstack-query/station-filters/useMemberFilters.ts
@@ -4,7 +4,6 @@ import { fetchUtils } from '@utils/fetch';
 
 import { serverStore } from '@stores/config/serverStore';
 import { memberInfoStore } from '@stores/login/memberInfoStore';
-import { memberTokenStore } from '@stores/login/memberTokenStore';
 
 import { SERVERS } from '@constants';
 import { QUERY_KEY_MEMBER_SELECTED_FILTERS } from '@constants/queryKeys';
@@ -15,10 +14,14 @@ const fetchMemberFilters = async (): Promise<StationFilters> => {
   const mode = serverStore.getState();
   const memberId = memberInfoStore.getState().memberId;
 
-  return await fetchUtils.get<StationFilters>(
-    `${SERVERS[mode]}/members/${memberId}/filters`,
-    '저장된 필터 정보를 불러오는데 실패했습니다.'
-  );
+  try {
+    return await fetchUtils.get<StationFilters>(
+      `${SERVERS[mode]}/members/${memberId}/filters`,
+      '저장된 필터 정보를 불러오는데 실패했습니다.'
+    );
+  } catch (error) {
+    return new Promise((resolve) => resolve({ capacities: [], companies: [], connectorTypes: [] }));
+  }
 };
 
 export const useMemberFilters = () => {
