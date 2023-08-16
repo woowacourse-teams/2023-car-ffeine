ff8bdd8af74803f43dddc7b0a73f22c05eb652fc
kyw0716 <kyw0716@naver.com>
2023-08-16 16:07:13 +0900

    refactor: 필요 없는 코드 삭제
    [#527]

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -3,8 +3,6 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
 import GoogleLogin from '../components/login/GoogleLogin';
 
-const isProductionServer = window.location.href.match(/https:\/\/carffe.in/) !== null
-
 export const router = createBrowserRouter([
   {
     path: '/',
