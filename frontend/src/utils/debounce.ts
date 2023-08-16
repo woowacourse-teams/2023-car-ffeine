b12a14f42f76fa6872f9ba59e72f5b8fee785660
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-08-16 15:12:23 +0900

    refactor: 개행 수정
    [#528]

--- frontend/src/utils/debounce.ts
+++ frontend/src/utils/debounce.ts
@@ -5,8 +5,10 @@ export type DebounceFunction = <T extends ((...args: T) => void)[]>(
 
 export const debounce: DebounceFunction = (func, delay) => {
   let timerId: NodeJS.Timeout;
+
   return (...args) => {
     clearTimeout(timerId);
+
     timerId = setTimeout(() => {
       func.apply(this, args);
     }, delay);
