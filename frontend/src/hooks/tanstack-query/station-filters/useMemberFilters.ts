9ab968b96320bf10047fea6e467f66752d1f5e22
kyw0716 <kyw0716@naver.com>
2023-08-17 00:47:01 +0900

    refactor: useMemberFilters 훅 내부 fetch함수 fetchUtils로 변경
    [#543]

--- frontend/src/hooks/tanstack-query/station-filters/useMemberFilters.ts
+++ frontend/src/hooks/tanstack-query/station-filters/useMemberFilters.ts
@@ -1,5 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 
+import { fetchUtils } from '@utils/fetch';
+
 import { serverStore } from '@stores/config/serverStore';
 import { memberInfoStore } from '@stores/login/memberInfoStore';
 import { memberTokenStore } from '@stores/login/memberTokenStore';
@@ -11,41 +13,12 @@ import type { StationFilters } from '@type';
 
 const fetchMemberFilters = async (): Promise<StationFilters> => {
   const mode = serverStore.getState();
-  const memberToken = memberTokenStore.getState();
   const memberId = memberInfoStore.getState().memberId;
 
-  if (memberId === undefined || memberToken === '') {
-    return new Promise((resolve) => {
-      resolve({
-        companies: [],
-        capacities: [],
-        connectorTypes: [],
-      });
-    });
-  }
-
-  const memberFilters = await fetch(`${SERVERS[mode]}/members/${memberId}/filters`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${memberToken}`,
-    },
-  })
-    .then<StationFilters>((response) => {
-      if (!response.ok) {
-        throw new Error('유저의 필터 정보를 불러오는데 실패했습니다.');
-      }
-
-      return response.json();
-    })
-    .catch<StationFilters>(() => {
-      return {
-        companies: [],
-        capacities: [],
-        connectorTypes: [],
-      };
-    });
-
-  return memberFilters;
+  return await fetchUtils.get<StationFilters>(
+    `${SERVERS[mode]}/members/${memberId}/filters`,
+    '저장된 필터 정보를 불러오는데 실패했습니다.'
+  );
 };
 
 export const useMemberFilters = () => {
