f2b3df6e69cc754bb24b9b2b1f49fc35f4eb81bf
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-08-16 15:04:56 +0900

    feat: 지도 요청을 디바운스로 처리하는 기능 구현
    [#528]

---
+++ frontend/src/utils/debounce.ts
@@ -0,0 +1,14 @@
+export type DebounceFunction = <T extends ((...args: T) => void)[]>(
+  func: (...args: T) => void,
+  delay: number
+) => (...args: T) => void;
+
+export const debounce: DebounceFunction = (func, delay) => {
+  let timerId: NodeJS.Timeout;
+  return (...args) => {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+};
