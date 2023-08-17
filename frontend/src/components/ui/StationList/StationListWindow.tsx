b6745261775b6f8db264bd9824e0a26660b7bd60
kyw0716 <kyw0716@naver.com>
2023-08-17 23:35:19 +0900

    design: 충전소 리스트 모바일 레이아웃 추가
    [#395]

--- frontend/src/components/ui/StationList/StationListWindow.tsx
+++ frontend/src/components/ui/StationList/StationListWindow.tsx
@@ -1,15 +1,32 @@
+import { XMarkIcon } from '@heroicons/react/24/outline';
 import { css } from 'styled-components';
 
+import { useExternalValue } from '@utils/external-state';
+
+import { selectedStationIdStore } from '@stores/selectedStationStore';
+
+import Button from '@common/Button';
 import FlexBox from '@common/FlexBox';
 import Text from '@common/Text';
 
 import { containerCss } from '@ui/ServerStationFilters/ServerStationFilters';
+import { useNavigationBar } from '@ui/compound/NavigationBar/hooks/useNavigationBar';
 
 import StationList from './StationList';
 
 const StationListWindow = () => {
+  const selectedStationId = useExternalValue(selectedStationIdStore);
+  const { closeBasePanel } = useNavigationBar();
+
+  if (selectedStationId !== null) {
+    closeBasePanel();
+  }
+
   return (
     <FlexBox css={[containerCss]}>
+      <Button css={xIconCss} onClick={closeBasePanel}>
+        <XMarkIcon width={32} />
+      </Button>
       <FlexBox css={headerCss}>
         <Text variant="h5">주변 충전소</Text>
       </FlexBox>
@@ -23,6 +40,12 @@ const headerCss = css`
 
   width: calc(100vw - 6rem);
   margin-bottom: 2rem;
+`;
+
+const xIconCss = css`
+  position: absolute;
+  top: 4rem;
+  right: 3rem;
 `;
 
 export default StationListWindow;
