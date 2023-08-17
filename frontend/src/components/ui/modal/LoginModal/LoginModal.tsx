c93eaf15eef1239b156a1ea7da096f420cb3b7fb
Dain Lee <feb.dain@gmail.com>
2023-08-17 19:41:57 +0900

    design: 로그인 모달 디자인 개선
    [#572]

--- frontend/src/components/ui/modal/LoginModal/LoginModal.tsx
+++ frontend/src/components/ui/modal/LoginModal/LoginModal.tsx
@@ -1,59 +1,68 @@
-import { UserCircleIcon } from '@heroicons/react/24/outline';
-import { css } from 'styled-components';
+import { css, styled } from 'styled-components';
 
 import { redirectToLoginPage } from '@utils/login';
 
-import ButtonNext from '@common/ButtonNext';
 import FlexBox from '@common/FlexBox';
 import Text from '@common/Text';
 
-import GoogleLoginButton from '@assets/google-login-button.svg';
+import LogoIcon from '@ui/Svg/LogoIcon';
+
+import GoogleLogo from '@assets/google-logo.svg';
 
 const LoginModal = () => {
+  const handleLogin = () => {
+    redirectToLoginPage('google');
+  };
+
   return (
-    <FlexBox width="100%" justifyContent="center" alignItems="center" css={containerCss}>
-      <FlexBox justifyContent="center" alignItems="center" css={loginButtonContainerCss}>
-        <FlexBox width={7} height={7} justifyContent="center" alignItems="center" css={iconCss}>
-          <UserCircleIcon width="7rem" stroke="lightgrey" />
-        </FlexBox>
-        <FlexBox direction="column" alignItems="center" gap={5}>
-          <Text variant="h4" color="#333">
-            간편 로그인
-          </Text>
-          <ButtonNext noTheme onClick={() => redirectToLoginPage('google')}>
-            <img width="80%" src={GoogleLoginButton} alt="구글 로그인 버튼 이미지" />
-          </ButtonNext>
-        </FlexBox>
-      </FlexBox>
+    <FlexBox
+      tag="article"
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      css={containerCss}
+    >
+      <LogoIcon width={8} />
+      <Text tag="h2" variant="h4" mt={4} mb={2} color="#333">
+        카페인
+      </Text>
+      <Text tag="h3">편리한 전기차 충전소 지도</Text>
+      <GoogleLogin onClick={handleLogin}>
+        <GoogleLogo width="24" />
+        <p>구글 로그인</p>
+      </GoogleLogin>
     </FlexBox>
   );
 };
 
 const containerCss = css`
-  padding: 5rem 0;
+  padding: 4.8rem 5rem 6.4rem;
 `;
 
-const loginButtonContainerCss = css`
-  width: 80%;
-  height: 25rem;
+const GoogleLogin = styled.button`
+  display: flex;
+  align-items: center;
+  column-gap: 6.6rem;
 
-  border: 1px solid lightgrey;
-  border-radius: 1rem;
+  width: 29.2rem;
+  height: 4rem;
+  margin: 3.6rem auto -1rem;
+  padding: 0 1.8rem;
 
-  position: relative;
-`;
+  font-size: 1.5rem;
+  font-weight: 600;
+  color: #333;
 
-const iconCss = css`
-  border-radius: 50%;
+  border-radius: 24px;
+  box-shadow: 1px 1px 5px 2px #e7e7e7;
 
-  position: absolute;
-  top: -3rem;
-  left: 50%;
-  transform: translate(-50%);
+  &:hover {
+    box-shadow: 0.8px 1px 5px 2px #e2e2e2;
+  }
 
-  padding: 0;
-
-  background-color: #fff;
+  & > p {
+    margin: 0.9rem 2.4rem 1.2rem 0;
+  }
 `;
 
 export default LoginModal;
