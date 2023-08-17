523a94aeece3678a1fc21598c7362b9ac4f358a7
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-08-17 10:11:45 +0900

    design: css 개선
    [#544]

--- frontend/src/components/ui/StationDetailsWindow/reports/station/StationReportButton.tsx
+++ frontend/src/components/ui/StationDetailsWindow/reports/station/StationReportButton.tsx
@@ -1,4 +1,5 @@
 import { MegaphoneIcon } from '@heroicons/react/24/outline';
+import { css } from 'styled-components';
 
 import StationReportPreConfirmation from '@ui/StationDetailsWindow/reports/station/StationReportPreConfirmation';
 
@@ -12,11 +13,18 @@ interface StationReportButtonProps {
 
 const StationReportButton = ({ station }: StationReportButtonProps) => {
   return (
-    <ReportButton modalContent={<StationReportPreConfirmation station={station} />}>
+    <ReportButton
+      css={calculatedButtonWidthCss}
+      modalContent={<StationReportPreConfirmation station={station} />}
+    >
       <MegaphoneIcon width={20} stroke="#666" aria-hidden />
       잘못된 충전소 정보 제보
     </ReportButton>
   );
 };
+
+const calculatedButtonWidthCss = css`
+  width: calc(100% - 1.6px * 2);
+`;
 
 export default StationReportButton;
