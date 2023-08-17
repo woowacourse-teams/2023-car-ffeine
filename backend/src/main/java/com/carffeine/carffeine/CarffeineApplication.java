95d087b60a1cac5a5fd8abcfbac233b4358d293c
한우석 <106640954+drunkenhw@users.noreply.github.com>
2023-08-17 11:03:39 +0900

    fix: time zone 설정 변경 (#561)
    [#552]

--- backend/src/main/java/com/carffeine/carffeine/CarffeineApplication.java
+++ backend/src/main/java/com/carffeine/carffeine/CarffeineApplication.java
@@ -11,7 +11,7 @@ import java.util.TimeZone;
 public class CarffeineApplication {
 
     public static void main(String[] args) {
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(CarffeineApplication.class, args);
     }
 }
