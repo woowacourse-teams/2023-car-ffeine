f9c602f46e61d0d5f5411729f6dd810bcb437d9c
kiarakim <kiarakim413@gmail.com>
2023-08-17 16:24:05 +0900

    refactor: 테스트 리팩터링

--- backend/src/test/java/com/carffeine/carffeine/station/domain/review/ReviewRepositoryTest.java
+++ backend/src/test/java/com/carffeine/carffeine/station/domain/review/ReviewRepositoryTest.java
@@ -18,7 +18,6 @@ import org.springframework.data.domain.Sort;
 import static com.carffeine.carffeine.member.fixture.MemberFixture.일반_회원;
 import static com.carffeine.carffeine.station.fixture.review.ReviewFixture.리뷰_13개;
 import static com.carffeine.carffeine.station.fixture.review.ReviewFixture.저장_전_리뷰;
-import static com.carffeine.carffeine.station.fixture.review.ReviewFixture.저장_전_리뷰2;
 import static com.carffeine.carffeine.station.fixture.station.StationFixture.선릉역_충전소_충전기_2개_사용가능_1개;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
@@ -40,13 +39,19 @@ class ReviewRepositoryTest {
     private Member member;
     private Station station;
     private Pageable pageable;
+    private Review review14;
 
     @BeforeEach
     void setUp() {
         member = memberRepository.save(일반_회원);
         pageable = PageRequest.of(0, 10, Sort.by("id").descending());
         station = stationRepository.save(선릉역_충전소_충전기_2개_사용가능_1개);
+        for (Review review : 리뷰_13개(member)) {
+            reviewRepository.save(review);
+        }
+        review14 = reviewRepository.save(저장_전_리뷰(member));
     }
+
     @Test
     void 리뷰를_등록한다() {
         // given
@@ -62,68 +67,59 @@ class ReviewRepositoryTest {
 
         // when
         Review foundReview = reviewRepository.findById(reviewId).get();
+        Page<Review> reviews = reviewRepository.findAllByStation(station, pageable);
 
         // then
-        assertThat(foundReview).usingRecursiveComparison()
-                .isEqualTo(review);
+        assertSoftly(softly -> {
+            softly.assertThat(reviews.getTotalElements()).isEqualTo(15);
+            softly.assertThat(foundReview).usingRecursiveComparison()
+                    .isEqualTo(review);
+        });
+
     }
 
     @Test
     void 전체_리뷰를_조회한다() {
-        // given
-        for (Review review : 리뷰_13개(member)) {
-            reviewRepository.save(review);
-        }
-
         // when
         Page<Review> foundReviews = reviewRepository.findAllByStation(station, pageable);
 
         // then
         assertSoftly(softly -> {
             softly.assertThat(foundReviews.getSize()).isEqualTo(10);
-            softly.assertThat(foundReviews.getTotalElements()).isEqualTo(13);
+            softly.assertThat(foundReviews.getTotalElements()).isEqualTo(14);
         });
     }
 
     @Test
     void 리뷰를_삭제한다() {
-        // given
-        Review review = reviewRepository.save(저장_전_리뷰(member));
-        reviewRepository.save(저장_전_리뷰2(member));
-
         // when
-        reviewRepository.delete(review);
-        Page<Review> 삭제_전_리뷰들 = reviewRepository.findAllByStation(station, pageable);
+        reviewRepository.delete(review14);
+        Page<Review> reviews = reviewRepository.findAllByStation(station, pageable);
 
         // then
-        assertThat(삭제_전_리뷰들.getContent().get(1).isDeleted()).isTrue();
+        assertSoftly(softly -> {
+            softly.assertThat(reviews.getTotalElements()).isEqualTo(14);
+            softly.assertThat(reviews.getContent().get(0).isDeleted()).isTrue();
+        });
+
     }
 
     @Test
     void 평균_별점을_구한다() {
-        // given
-        for (Review review : 리뷰_13개(member)) {
-            reviewRepository.save(review);
-        }
-
         // when
         Double averageRatings = reviewRepository.findAverageRatingsByStation(station).get();
+        double roundAverageRatings = Math.round(averageRatings * 10.0) / 10.0;
 
         // then
-        assertThat(averageRatings).isEqualTo(2.0);
+        assertThat(roundAverageRatings).isEqualTo(2.1);
     }
 
     @Test
     void 리뷰의_총_개수를_구한다() {
-        // given
-        for (Review review : 리뷰_13개(member)) {
-            reviewRepository.save(review);
-        }
-
         // when
         long count = reviewRepository.countByStation(station);
 
         // then
-        assertThat(count).isEqualTo(13);
+        assertThat(count).isEqualTo(14);
     }
 }
