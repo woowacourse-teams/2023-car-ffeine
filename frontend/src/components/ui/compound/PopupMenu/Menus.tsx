d28a34a11a4f8828a4d1b8cf3282d6aafa90c9f2
kyw0716 <kyw0716@naver.com>
2023-08-17 20:26:19 +0900

    refactor: 프로필 메뉴 웹, 모바일에서 위치 다르게 보이도록 수정
    [#395]

--- frontend/src/components/ui/compound/PopupMenu/Menus.tsx
+++ frontend/src/components/ui/compound/PopupMenu/Menus.tsx
@@ -5,15 +5,18 @@ import type { PropsWithChildren } from 'react';
 import ButtonNext from '@common/ButtonNext';
 import FlexBox from '@common/FlexBox';
 
+import { MOBILE_BREAKPOINT } from '@constants';
+
 interface Props {
   menus: PropsWithChildren<{ onClick: () => void }>[];
   closeMenu: () => void;
+  containerWidth: number;
 }
 
-const Menus = ({ menus, closeMenu }: Props) => {
+const Menus = ({ menus, containerWidth, closeMenu }: Props) => {
   return (
     <FlexBox tag="ul" width="max-content" direction="column" alignItems="center" css={containerCss}>
-      <StartingPointBox />
+      <StartingPointBox containerWidth={containerWidth} />
       {menus.map(({ children, onClick }, i) => (
         <FlexBox key={i} tag="li">
           <ButtonNext
@@ -33,21 +36,33 @@ const Menus = ({ menus, closeMenu }: Props) => {
   );
 };
 
-const StartingPointBox = styled.div`
+const StartingPointBox = styled.div<{ containerWidth: number }>`
   width: 2rem;
   height: 2rem;
 
   position: absolute;
-  top: 2rem;
-  left: -1rem;
 
   border: 1px solid #d6d6d6;
-  border-top: none;
-  border-right: none;
 
   background-color: #fff;
 
   transform: rotate(45deg);
+
+  @media screen and (min-width: ${MOBILE_BREAKPOINT}px) {
+    top: 2rem;
+    left: -1rem;
+
+    border-top: none;
+    border-right: none;
+  }
+
+  @media screen and (max-width: ${MOBILE_BREAKPOINT}px) {
+    bottom: -1rem;
+    left: ${(props) => props.containerWidth / 2 - 10}px;
+
+    border-top: none;
+    border-left: none;
+  }
 `;
 
 const containerCss = css`
--- frontend/src/components/ui/compound/PopupMenu/index.tsx
+++ frontend/src/components/ui/compound/PopupMenu/index.tsx
@@ -1,22 +1,30 @@
 import { css } from 'styled-components';
 
-import type { PropsWithChildren, ReactNode } from 'react';
+import type { PropsWithChildren, ReactElement } from 'react';
 import { useEffect, useRef, useState } from 'react';
 
 import Box from '@common/Box';
 import FlexBox from '@common/FlexBox';
 
+import { MOBILE_BREAKPOINT } from '@constants';
+
 import Menus from './Menus';
 
 interface Props {
-  trigger: ReactNode;
+  trigger: ReactElement;
   menus: PropsWithChildren<{ onClick: () => void }>[];
 }
 
 const PopupMenu = ({ trigger, menus }: Props) => {
+  const popupRef = useRef<HTMLDivElement>(null);
   const triggerRef = useRef<HTMLButtonElement>(null);
+
   const [isOpen, setIsOpen] = useState(false);
-  const [triggerWidth, setTriggerWidth] = useState(0);
+  const [triggerSize, setTriggerSize] = useState({
+    width: 0,
+    height: 0,
+  });
+  const [popupSize, setPopupSize] = useState({ popupWidth: 0, popupHeight: 0 });
 
   const handleToggleMenu = () => {
     setIsOpen((prev) => !prev);
@@ -27,16 +35,36 @@ const PopupMenu = ({ trigger, menus }: Props) => {
   };
 
   useEffect(() => {
-    if (triggerRef.current) setTriggerWidth(triggerRef.current.offsetWidth);
-  }, []);
+    if (triggerRef.current) {
+      const width = triggerRef.current.offsetWidth;
+      const height = triggerRef.current.offsetHeight;
+      const popupWidth = popupRef.current.offsetWidth;
+      const popupHeight = popupRef.current.offsetHeight;
+
+      setTriggerSize({ width, height });
+      setPopupSize({ popupWidth, popupHeight });
+
+      console.log(popupHeight, triggerSize);
+    }
+  }, [isOpen]);
 
   return (
     <FlexBox css={container}>
       <button ref={triggerRef} onClick={handleToggleMenu}>
         {trigger}
       </button>
-      <Box css={getMenuContainerCss(triggerWidth)}>
-        {isOpen && <Menus menus={menus} closeMenu={handleCloseMenu} />}
+      <Box
+        ref={popupRef}
+        css={getMenuContainerCss(
+          triggerSize.width,
+          triggerSize.height,
+          popupSize.popupWidth,
+          popupSize.popupHeight
+        )}
+      >
+        {isOpen && (
+          <Menus menus={menus} closeMenu={handleCloseMenu} containerWidth={popupSize.popupWidth} />
+        )}
       </Box>
     </FlexBox>
   );
@@ -46,12 +74,24 @@ const container = css`
   position: relative;
 `;
 
-const getMenuContainerCss = (triggerWidth: number) => {
+const getMenuContainerCss = (
+  triggerWidth: number,
+  triggerHeight: number,
+  popupWidth: number,
+  popupHeight: number
+) => {
   return css`
     position: absolute;
 
     top: -2rem;
     left: calc(${triggerWidth}px + 2rem);
+
+    @media screen and (max-width: ${MOBILE_BREAKPOINT}px) {
+      top: 0;
+      left: 0;
+      top: calc(-${popupHeight - triggerHeight}px - 7rem);
+      left: calc(-${popupWidth / 2}px + ${triggerWidth / 2}px);
+    }
   `;
 };
 
