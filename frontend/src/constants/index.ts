e269fa61dd6e9bb7ad311a70ac7990fbc1907f54
kyw0716 <kyw0716@naver.com>
2023-08-23 11:41:53 +0900

    refactor: 사용하지 않는 상수 삭제
    [#610]

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -2,13 +2,6 @@ export const DEFAULT_TOKEN = -1;
 
 export const INVALID_VALUE_LIST = ['null', '.', '..', '1', '#'];
 
-export const SERVERS = {
-  dain: 'https://dain.carffe.in/api',
-  production: 'https://api.carffe.in/api',
-} as const;
-
-export const MSW_OFF = 'mswOff';
-
 export const FORM_ADDRESS_LENGTH_LIMIT = 150;
 export const FORM_DETAIL_LOCATION_LENGTH_LIMIT = 200;
 export const FORM_OPERATING_TIME_LENGTH_LIMIT = 50;
