2d038ae4a8b3b50b85a24088aad931df37550800
Dain Lee <feb.dain@gmail.com>
2023-08-31 15:44:44 +0900

    refactor: `업데이트(latestUpdateTime)` 대신 더 정확한 문구 사용
    [#605]

--- frontend/src/components/ui/StationDetailsWindow/chargers/ChargerCard.tsx
+++ frontend/src/components/ui/StationDetailsWindow/chargers/ChargerCard.tsx
@@ -36,6 +36,15 @@ const statusLightColor = (state: ChargerStateType) => {
 
 const ChargerCard = ({ charger }: ChargerCardProps) => {
   const { type, price, capacity, latestUpdateTime, state, method } = charger;
+  const STATE_MESSAGE = {
+    COMMUNICATION_ERROR: '마지막 통신',
+    STANDBY: '마지막 충전',
+    CHARGING_IN_PROGRESS: '충전 시작',
+    OPERATION_SUSPENDED: '마지막 충전',
+    UNDER_INSPECTION: '마지막 충전',
+    STATUS_UNKNOWN: '마지막 통신',
+  };
+
   return (
     <FlexBox tag="article" outlined nowrap direction="column" width="49%" p={2} css={borderCss}>
       <SquareBox heavyColor={statusLightColor(state)} lightColor={statusHeavyColor(state)}>
@@ -56,7 +65,7 @@ const ChargerCard = ({ charger }: ChargerCardProps) => {
       </Text>
       {latestUpdateTime && (
         <Text variant="caption" align="right" css={bottomTextCss}>
-          업데이트 : {calculateLatestUpdateTime(latestUpdateTime)}
+          {STATE_MESSAGE[state]} : {calculateLatestUpdateTime(latestUpdateTime)}
         </Text>
       )}
     </FlexBox>
