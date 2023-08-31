ed17707567ed9f6ff61bcec67480ddbe9d9c94a8
drunkenhw <drunkenhw@gmail.com>
2023-09-01 01:06:45 +0900

    feat: empty list 반환하도록 변경
    [#631]

--- backend/src/main/java/com/carffeine/carffeine/station/service/station/StationQueryService.java
+++ backend/src/main/java/com/carffeine/carffeine/station/service/station/StationQueryService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -39,6 +40,9 @@ public class StationQueryService {
                 chargerTypes,
                 capacities
         );
+        if (stationIds.isEmpty()) {
+            return Collections.emptyList();
+        }
         return stationQueryRepository.findStationByStationIds(stationIds);
     }
 }
