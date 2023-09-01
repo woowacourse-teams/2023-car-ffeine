c6d5c25552353bd91070e6ffd94f21c6b87c7fd8
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-01 10:46:25 +0900

    refactor: 피드백 반영
    [#638]

--- frontend/src/components/ui/compound/Graph/Bar.stories.tsx
+++ frontend/src/components/ui/compound/Graph/Bar.stories.tsx
@@ -16,11 +16,11 @@ export default meta;
 export const Default = () => {
   return Array.from({ length: 25 }, (_, index) => (
     <Box key={index} my={1}>
-      <Bar align={'column'} ratio={(index / 24) * 100} hour={index} />
+      <Bar align="column" ratio={(index / 24) * 100} hour={index} />
     </Box>
   ));
 };
 
 export const NoRatio = () => {
-  return <Bar align={'column'} ratio={NO_RATIO} hour={1} />;
+  return <Bar align="column" ratio={NO_RATIO} hour={1} />;
 };
