693d32a7146e26843757470c412905d07b1fdbf2
kiarakim <kiarakim413@gmail.com>
2023-09-01 13:49:54 +0900

    refactor: 사용하지 않는 메서드 제거

--- backend/src/main/java/com/carffeine/carffeine/station/infrastructure/repository/review/dto/TotalRatingsResponse.java
+++ backend/src/main/java/com/carffeine/carffeine/station/infrastructure/repository/review/dto/TotalRatingsResponse.java
@@ -4,8 +4,4 @@ public record TotalRatingsResponse(
         Double totalRatings,
         Long totalCount
 ) {
-
-    public static TotalRatingsResponse of(Double totalRatings, Long totalCount) {
-        return new TotalRatingsResponse(totalRatings, totalCount);
-    }
 }
