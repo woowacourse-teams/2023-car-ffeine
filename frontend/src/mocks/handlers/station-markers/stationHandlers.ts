72affa3a4d381edd39a6cc110ed7a42bfd124d57
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-04 20:13:56 +0900

    feat: 충전소 간단 정보 리스트 조회 api 모킹
    - 새로운 명세 대응
    
    [#650]

--- frontend/src/mocks/handlers/station-markers/stationHandlers.ts
+++ frontend/src/mocks/handlers/station-markers/stationHandlers.ts
@@ -83,4 +83,49 @@ export const stationHandlers = [
       })
     );
   }),
+
+  rest.get(`${DEVELOP_SERVER_URL}/stations/summary`, async (req, res, ctx) => {
+    const { searchParams } = req.url;
+    // ?stationIds=PE123456,PE123457,PE123465 ==> 대략 10개, 검사는 안함
+    const stationIdsParam = searchParams.get('stationIds');
+    if (stationIdsParam === undefined) {
+      return res(ctx.delay(1000), ctx.status(200), ctx.json([]));
+    }
+    const stationIds = stationIdsParam.split(DELIMITER);
+    const foundStations = stations.filter((station) => stationIds.includes(station.stationId));
+    const stationSummaries: StationSummary[] = foundStations.map((station) => {
+      const {
+        address,
+        availableCount,
+        chargers,
+        companyName,
+        detailLocation,
+        isParkingFree,
+        isPrivate,
+        latitude,
+        longitude,
+        operatingTime,
+        stationId,
+        stationName,
+        totalCount,
+      }: StationSummary = station;
+
+      return {
+        address,
+        availableCount,
+        chargers,
+        companyName,
+        detailLocation,
+        isParkingFree,
+        isPrivate,
+        latitude,
+        longitude,
+        operatingTime,
+        stationId,
+        stationName,
+        totalCount,
+      };
+    });
+    return res(ctx.delay(1000), ctx.status(200), ctx.json(stationSummaries));
+  }),
 ];
