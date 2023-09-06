bd613b4056fa62aec2cdd4d0f62ccae0533658b5
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-06 14:49:37 +0900

    refactor: ref 제거
    [#646]

--- frontend/src/components/common/Box/Box.tsx
+++ frontend/src/components/common/Box/Box.tsx
@@ -1,7 +1,6 @@
 import type { CSSProp } from 'styled-components';
 import styled from 'styled-components';
 
-import type { Ref } from 'react';
 import { type HTMLAttributes, type ReactNode } from 'react';
 
 import type { SpacingProps } from '@common/systems';
@@ -51,12 +50,8 @@ const BoxWrapper = styled.div<BoxProps>`
   ${({ css }) => css};
 `;
 
-const Box = ({ children, ...props }: BoxProps, ref: Ref<HTMLDivElement>) => {
-  return (
-    <BoxWrapper ref={ref} {...props}>
-      {children}
-    </BoxWrapper>
-  );
+const Box = ({ children, ...props }: BoxProps) => {
+  return <BoxWrapper {...props}>{children}</BoxWrapper>;
 };
 
 export default Box;
