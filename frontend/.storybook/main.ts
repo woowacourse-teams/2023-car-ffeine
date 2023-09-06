a5dbd107f2b6869ce78920e5f031d942914abb0e
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-06 14:59:40 +0900

    fix: 스토리북에서 전역 경로 접근 허용
    [#646]

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,5 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
-// import path from 'path';
+import path from 'path';
 import { Configuration, RuleSetUseItem } from 'webpack';
 
 interface RuleSetRules {
@@ -39,24 +39,24 @@ const config: StorybookConfig = {
       use: ['@svgr/webpack'],
     });
 
-    // if (resolve) {
-    //   resolve.alias = {
-    //     ...resolve.alias,
-    //     '@mocks': path.resolve(__dirname, '../src/mocks'),
-    //     '@map': path.resolve(__dirname, '../src/components/google-maps/map'),
-    //     '@marker': path.resolve(__dirname, '../src/components/google-maps/marker'),
-    //     '@ui': path.resolve(__dirname, '../src/components/ui'),
-    //     '@common': path.resolve(__dirname, '../src/components/common'),
-    //     '@components': path.resolve(__dirname, '../src/components'),
-    //     '@utils': path.resolve(__dirname, '../src/utils'),
-    //     '@hooks': path.resolve(__dirname, '../src/hooks'),
-    //     '@stores': path.resolve(__dirname, '../src/stores'),
-    //     '@style': path.resolve(__dirname, '../src/style'),
-    //     '@constants': path.resolve(__dirname, '../src/constants'),
-    //     '@assets': path.resolve(__dirname, '../src/assets'),
-    //     '@type': path.resolve(__dirname, '../src/types'),
-    //   };
-    // }
+    if (resolve) {
+      resolve.alias = {
+        ...resolve.alias,
+        '@mocks': path.resolve(__dirname, '../src/mocks'),
+        '@map': path.resolve(__dirname, '../src/components/google-maps/map'),
+        '@marker': path.resolve(__dirname, '../src/components/google-maps/marker'),
+        '@ui': path.resolve(__dirname, '../src/components/ui'),
+        '@common': path.resolve(__dirname, '../src/components/common'),
+        '@components': path.resolve(__dirname, '../src/components'),
+        '@utils': path.resolve(__dirname, '../src/utils'),
+        '@hooks': path.resolve(__dirname, '../src/hooks'),
+        '@stores': path.resolve(__dirname, '../src/stores'),
+        '@style': path.resolve(__dirname, '../src/style'),
+        '@constants': path.resolve(__dirname, '../src/constants'),
+        '@assets': path.resolve(__dirname, '../src/assets'),
+        '@type': path.resolve(__dirname, '../src/types'),
+      };
+    }
 
     return config;
   },
