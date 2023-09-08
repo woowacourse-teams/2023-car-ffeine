96b83f853cf31910001592b96820d4b794b0f609
Dain Lee <feb.dain@gmail.com>
2023-09-09 01:53:01 +0900

    refactor: 필요없는 파일 삭제 및 service worker 설정
    [#634]

--- frontend/public/pwabuilder-sw.js
+++ frontend/public/pwabuilder-sw.js
@@ -3,10 +3,10 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = 'pwabuilder-page';
+const CACHE = 'car-ffeine';
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = 'ToDo-replace-this-name.html';
+const offlineFallbackPage = 'index.html';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
