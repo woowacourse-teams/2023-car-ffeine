3687417a51bf9e3692e033ec40497723c44d04da
sosow0212 <sosow0212@naver.com>
2023-09-09 17:20:08 +0900

    refactor: 요일별로 혼잡도 통계를 조회하도록 리팩토링

--- backend/src/main/java/com/carffeine/carffeine/station/controller/congestion/CongestionController.java
+++ backend/src/main/java/com/carffeine/carffeine/station/controller/congestion/CongestionController.java
@@ -2,22 +2,25 @@ package com.carffeine.carffeine.station.controller.congestion;
 
 import com.carffeine.carffeine.station.controller.congestion.dto.StatisticsResponse;
 import com.carffeine.carffeine.station.service.congestion.CongestionService;
-import com.carffeine.carffeine.station.service.congestion.dto.StatisticsRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class CongestionController {
 
+    private static final String MONDAY = "1";
+
     private final CongestionService congestionService;
 
     @GetMapping("/stations/{stationId}/statistics")
-    public ResponseEntity<StatisticsResponse> showCongestionStatistics(@PathVariable String stationId) {
-        StatisticsResponse statisticsResponse = congestionService.showCongestionStatistics(new StatisticsRequest(stationId));
+    public ResponseEntity<StatisticsResponse> showCongestionStatistics(@PathVariable String stationId,
+                                                                       @RequestParam(required = false, defaultValue = MONDAY) int dayOfWeek) {
+        StatisticsResponse statisticsResponse = congestionService.showCongestionStatistics(stationId, dayOfWeek);
         return ResponseEntity.ok(statisticsResponse);
     }
 }
