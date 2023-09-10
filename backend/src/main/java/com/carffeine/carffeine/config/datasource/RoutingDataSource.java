da1fe478c48bd771e57d27b0fb76415e33616b85
drunkenhw <drunkenhw@gmail.com>
2023-09-10 16:58:31 +0900

    feat: 로그 추가
    [#680]

--- backend/src/main/java/com/carffeine/carffeine/config/datasource/RoutingDataSource.java
+++ backend/src/main/java/com/carffeine/carffeine/config/datasource/RoutingDataSource.java
@@ -1,10 +1,12 @@
 package com.carffeine.carffeine.config.datasource;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import java.util.Map;
 
+@Slf4j
 public class RoutingDataSource extends AbstractRoutingDataSource {
 
     public static RoutingDataSource from(Map<Object, Object> dataSources) {
@@ -18,8 +20,10 @@ public class RoutingDataSource extends AbstractRoutingDataSource {
     protected Object determineCurrentLookupKey() {
         boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
         if (readOnly) {
+            log.info("Routing to REPLICA");
             return DataSourceType.REPLICA;
         }
+        log.info("Routing to SOURCE");
         return DataSourceType.SOURCE;
     }
 }
