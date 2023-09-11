eb60a9aea0d6bb72965180ba799989af56ff64c2
sosow0212 <sosow0212@naver.com>
2023-09-11 17:00:07 +0900

    refactor: 불필요한 필드 제거

--- backend/src/main/java/com/carffeine/carffeine/station/domain/congestion/PeriodicCongestion.java
+++ backend/src/main/java/com/carffeine/carffeine/station/domain/congestion/PeriodicCongestion.java
@@ -2,32 +2,24 @@ package com.carffeine.carffeine.station.domain.congestion;
 
 import com.carffeine.carffeine.common.domain.BaseEntity;
 import com.carffeine.carffeine.station.config.RequestPeriodConverter;
-import com.carffeine.carffeine.station.domain.charger.Charger;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
-import javax.persistence.ConstraintMode;
 import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.ForeignKey;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 import javax.persistence.Index;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinColumns;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import java.time.DayOfWeek;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 @Entity
 @EqualsAndHashCode(of = {"id"}, callSuper = false)
 @IdClass(PeriodicCongestionPK.class)
@@ -57,13 +49,6 @@ public class PeriodicCongestion extends BaseEntity {
 
     @Column(nullable = false)
     private double congestion;
-
-    @ManyToOne
-    @JoinColumns(value = {
-            @JoinColumn(name = "station_id", referencedColumnName = "station_id", insertable = false, updatable = false),
-            @JoinColumn(name = "charger_id", referencedColumnName = "charger_id", insertable = false, updatable = false)
-    }, foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
-    private Charger charger;
 
     @Column(name = "station_id", nullable = false)
     private String stationId;
