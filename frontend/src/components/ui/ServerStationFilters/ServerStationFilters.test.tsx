7ad58aceb8521f6dac1b714ee45e0fbf753eb4fd
kyw0716 <kyw0716@naver.com>
2023-09-12 01:44:41 +0900

    test: 필터 초기화 로직 테스트 코드 추가
    [#618]

--- frontend/src/components/ui/ServerStationFilters/ServerStationFilters.test.tsx
+++ frontend/src/components/ui/ServerStationFilters/ServerStationFilters.test.tsx
@@ -6,6 +6,7 @@ import {
   selectedCapacitiesFilterStore,
   selectedCompaniesFilterStore,
   selectedConnectorTypesFilterStore,
+  serverStationFilterAction,
 } from '@stores/station-filters/serverStationFiltersStore';
 
 import { useServerStationFilters } from '@hooks/tanstack-query/station-filters/useServerStationFilters';
@@ -20,6 +21,10 @@ const queryClient = new QueryClient();
 jest.mock('@hooks/tanstack-query/station-filters/useServerStationFilters');
 
 describe('ServerStationFilters 컴포넌트 테스트', () => {
+  beforeEach(() => {
+    serverStationFilterAction.deleteAllServerStationFilters();
+  });
+
   it('ServerStationFilters 컴포넌트가 열리면 useServerStationFilters 훅에서 받아온 정보를 화면에 렌더링 한다.', () => {
     (useServerStationFilters as jest.Mock).mockReturnValue({
       data: {
@@ -83,5 +88,41 @@ describe('ServerStationFilters 컴포넌트 테스트', () => {
     expect(selectedCapacities.has('3.00')).toBeTruthy();
 
     expect(selectedConnectorTypes.has('AC_3PHASE')).toBeTruthy();
+  });
+
+  it('필터 초기화 버튼을 클릭하면 차량 필터를 제외한 모든 필터 옵션들이 해제된다.', () => {
+    (useServerStationFilters as jest.Mock).mockReturnValue({
+      data: {
+        capacities: ['100.00', '200.00', '3.00'],
+        companies: ['AM', 'BA', 'BG', 'BK'],
+        connectorTypes: ['AC_3PHASE', 'AC_SLOW', 'DC_COMBO'],
+      } as StationFilters,
+      isLoading: false,
+    });
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <ServerStationFilters />
+      </QueryClientProvider>
+    );
+
+    fireEvent.click(screen.getByText(CAPACITIES[0]));
+
+    fireEvent.click(screen.getByText(COMPANIES['AM']));
+    fireEvent.click(screen.getByText(COMPANIES['BA']));
+
+    fireEvent.click(screen.getByText(CONNECTOR_TYPES['AC_3PHASE']));
+
+    // 유저가 클릭한 만큼 전역 상태에 추가되었는지 먼저 확인
+    expect(selectedCapacitiesFilterStore.getState().size === 1).toBeTruthy();
+    expect(selectedCompaniesFilterStore.getState().size === 2).toBeTruthy();
+    expect(selectedConnectorTypesFilterStore.getState().size === 1).toBeTruthy();
+
+    fireEvent.click(screen.getByLabelText('모든 필터 지우기 버튼'));
+
+    // 모든 필터 지우기 버튼을 클릭했을시 전역 상태의 모든 필터 옵션이 지워졌는지 확인
+    expect(selectedCapacitiesFilterStore.getState().size === 0).toBeTruthy();
+    expect(selectedCompaniesFilterStore.getState().size === 0).toBeTruthy();
+    expect(selectedConnectorTypesFilterStore.getState().size === 0).toBeTruthy();
   });
 });
