b8cbbca57a909cf8e469f107289c364d90f763ad
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-11 21:12:57 +0900

    refactor: 델타 제한 해제
    [#695]

--- frontend/src/utils/google-maps/index.ts
+++ frontend/src/utils/google-maps/index.ts
@@ -15,8 +15,8 @@ export const getDisplayPosition = (map: google.maps.Map) => {
   return {
     longitude,
     latitude,
-    longitudeDelta: longitudeDelta < 0.008 ? longitudeDelta : 0.008,
-    latitudeDelta: latitudeDelta < 0.004 ? latitudeDelta : 0.004,
+    longitudeDelta: longitudeDelta,
+    latitudeDelta: latitudeDelta,
     zoom,
   };
 };
