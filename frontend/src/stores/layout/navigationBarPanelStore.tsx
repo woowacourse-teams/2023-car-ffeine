24557efe60e1bb7da2c4f1bdac4d75e897be5a7d
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-11 14:07:45 +0900

    refactor: 초기에 basePanel을 닫히도록 수정
    [#686]

--- frontend/src/stores/layout/navigationBarPanelStore.tsx
+++ frontend/src/stores/layout/navigationBarPanelStore.tsx
@@ -2,14 +2,12 @@ import type { ReactElement } from 'react';
 
 import { store } from '@utils/external-state';
 
-import StationSearchWindow from '@ui/StationSearchWindow';
-
 export interface Panels {
   basePanel: ReactElement | null;
   lastPanel: ReactElement | null;
 }
 
 export const navigationBarPanelStore = store<Panels>({
-  basePanel: <StationSearchWindow />,
+  basePanel: null,
   lastPanel: null,
 });
