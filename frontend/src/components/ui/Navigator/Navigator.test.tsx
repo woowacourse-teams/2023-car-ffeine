eba36d3e110bcc60dc81e2a103b0fe0d4ff08738
kyw0716 <kyw0716@naver.com>
2023-09-12 23:58:03 +0900

    test: 프로필 메뉴 렌더링 기능 테스트 추가
    [#618]

--- frontend/src/components/ui/Navigator/Navigator.test.tsx
+++ frontend/src/components/ui/Navigator/Navigator.test.tsx
@@ -2,6 +2,8 @@ import { fireEvent, render, screen } from '@testing-library/react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+import { memberTokenStore } from '@stores/login/memberTokenStore';
+
 import ModalContainer from '@ui/ModalContainer';
 import NavigationBar from '@ui/compound/NavigationBar';
 
@@ -19,5 +21,21 @@ describe('Navigator 컴포넌트 테스트', () => {
     fireEvent.click(screen.getByLabelText('로그인 하기'));
 
     expect(screen.getByText('구글 로그인')).toBeInTheDocument();
+  });
+
+  it('멤버 토큰이 있을 때 프로필 버튼을 누르면 프로필 메뉴가 렌더링 된다.', () => {
+    memberTokenStore.setState('test-member-token');
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <ModalContainer />
+        <NavigationBar.Menu />
+      </QueryClientProvider>
+    );
+
+    fireEvent.click(screen.getByLabelText('프로필 메뉴 열기'));
+
+    expect(screen.getByText('차량등록')).toBeInTheDocument();
+    expect(screen.getByText('로그아웃')).toBeInTheDocument();
   });
 });
