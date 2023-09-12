ce06e8c762188ef7ec711afc3bc047dc5dbcef5e
kyw0716 <kyw0716@naver.com>
2023-09-13 00:11:57 +0900

    test: 테스트 코드 위치 수정
    [#618]

--- frontend/src/components/ui/Navigator/Navigator.test.tsx
+++ frontend/src/components/ui/Navigator/Navigator.test.tsx
@@ -25,6 +25,22 @@ describe('Navigator 컴포넌트 테스트', () => {
     expect(screen.getByText('구글 로그인')).toBeInTheDocument();
   });
 
+  it('로그아웃 버튼을 클릭하면 저장해두었던 멤버 토큰이 지워진다.', () => {
+    memberTokenStore.setState('test-member-token');
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <ModalContainer />
+        <NavigationBar.Menu />
+      </QueryClientProvider>
+    );
+
+    fireEvent.click(screen.getByLabelText('프로필 메뉴 열기'));
+    fireEvent.click(screen.getByText('로그아웃'));
+
+    expect(memberTokenStore.getState()).toBe(EMPTY_MEMBER_TOKEN);
+  });
+
   it('멤버 토큰이 있을 때 프로필 버튼을 누르면 프로필 메뉴가 렌더링 된다.', () => {
     memberTokenStore.setState('test-member-token');
 
@@ -41,7 +57,7 @@ describe('Navigator 컴포넌트 테스트', () => {
     expect(screen.getByText('로그아웃')).toBeInTheDocument();
   });
 
-  it('로그아웃 버튼을 클릭하면 저장해두었던 멤버 토큰이 지워진다.', () => {
+  it('차량 등록 버튼을 클릭하면 차량 선택 모달이 렌더링 된다.', () => {
     memberTokenStore.setState('test-member-token');
 
     render(
@@ -50,10 +66,5 @@ describe('Navigator 컴포넌트 테스트', () => {
         <NavigationBar.Menu />
       </QueryClientProvider>
     );
-
-    fireEvent.click(screen.getByLabelText('프로필 메뉴 열기'));
-    fireEvent.click(screen.getByText('로그아웃'));
-
-    expect(memberTokenStore.getState()).toBe(EMPTY_MEMBER_TOKEN);
   });
 });
