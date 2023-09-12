50d72ca644b702b2e3bb4219c87993d8db41e667
Dain Lee <feb.dain@gmail.com>
2023-09-13 01:03:50 +0900

    feat: 인포 아이콘 버튼 누르면 `Help` 컴포넌트가 나오는 기능 구현
    [#636]

--- frontend/src/components/ui/StationDetailsWindow/congestion/Title.tsx
+++ frontend/src/components/ui/StationDetailsWindow/congestion/Title.tsx
@@ -1,17 +1,26 @@
 import { InformationCircleIcon } from '@heroicons/react/24/outline';
 
+import { modalActions } from '@stores/layout/modalStore';
+
+import Button from '@common/Button';
 import FlexBox from '@common/FlexBox';
 import Text from '@common/Text';
 
+import Help from './Help';
+
 const Title = () => {
+  const handleOpenStatisticsHelp = () => {
+    modalActions.openModal(<Help />);
+  };
+
   return (
     <FlexBox justifyContent="between" alignItems="center" mb={3}>
       <Text fontSize={1.8} weight="bold">
         충전소 시간별 혼잡도
       </Text>
-      <Text>
-        <InformationCircleIcon width={24} stroke="#747474" />
-      </Text>
+      <Button>
+        <InformationCircleIcon width={24} stroke="#747474" onClick={handleOpenStatisticsHelp} />
+      </Button>
     </FlexBox>
   );
 };
