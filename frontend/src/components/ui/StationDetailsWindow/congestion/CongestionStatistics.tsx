af27470855f3d205834fc2b4cdf0f3ec08062166
Dain Lee <108778921+feb-dain@users.noreply.github.com>
2023-09-12 14:31:22 +0900

    design: 임시로 만들어져 있는 혼잡도 통계 버튼을 개선한다 (#703)
    * design: style primary, default 색상 변경
    
    [#682]
    
    * feat: 버튼 위치 변경 및 닫기 기능 구현
    
    [#682]
    
    * feat: 충전기 닫기 버튼 구현
    
    - 더보기 버튼을 눌러 모든 충전기를 볼 수 있도록 펼쳤을 때, 다시 처음처럼
      되돌릴 수 있는 닫기 버튼 구현
    
    [#682]
    
    * refactor: 통계 확인 버튼을 눌러야 요청을 보내도록 개선
    
    [#682]

--- frontend/src/components/ui/StationDetailsWindow/congestion/CongestionStatistics.tsx
+++ frontend/src/components/ui/StationDetailsWindow/congestion/CongestionStatistics.tsx
@@ -1,63 +1,35 @@
-import { useState } from 'react';
+import { ChartBarSquareIcon, ChevronDownIcon } from '@heroicons/react/24/outline';
 
-import { useStationCongestionStatistics } from '@hooks/tanstack-query/station-details/useStationCongestionStatistics';
+import { useState } from 'react';
 
 import Box from '@common/Box';
 import ButtonNext from '@common/ButtonNext';
 import FlexBox from '@common/FlexBox';
-import Text from '@common/Text';
 
-import CongestionStatisticsSkeleton from '@ui/StationDetailsWindow/congestion/CongestionStatisticsSkeleton';
-import StatisticsGraph from '@ui/StatisticsGraph';
-
-import type { CHARGING_SPEED } from '@constants/chargers';
-import { ENGLISH_DAYS } from '@constants/congestion';
+import Statistics from './Statistics';
+import Title from './Title';
 
 interface CongestionStatisticsProps {
   stationId: string;
 }
 
 const CongestionStatistics = ({ stationId }: CongestionStatisticsProps) => {
-  const { data: congestionStatistics, isLoading } = useStationCongestionStatistics(stationId);
-  const [chargingSpeed, setChargingSpeed] = useState<keyof typeof CHARGING_SPEED>('standard');
-
-  if (isLoading) {
-    return <CongestionStatisticsSkeleton />;
-  }
+  const [isStatisticsOpen, setIsStatisticsOpen] = useState(false);
 
   return (
     <Box my={5}>
-      <FlexBox justifyContent="between" alignItems="center" mt={12.5} mb={3}>
-        <Text fontSize={1.8} weight="bold">
-          충전소 시간별 혼잡도
-        </Text>
-        <Text>BETA</Text>
-      </FlexBox>
-      <FlexBox direction="column" gap={4}>
-        <StatisticsGraph
-          statistics={congestionStatistics.congestion[chargingSpeed]}
-          menus={[...ENGLISH_DAYS]}
-          align="column"
-        />
-        <FlexBox nowrap>
-          <ButtonNext
-            variant={chargingSpeed === 'standard' ? 'contained' : 'outlined'}
-            size="xs"
-            onClick={() => setChargingSpeed('standard')}
-            fullWidth
-          >
-            완속 충전기 그룹
-          </ButtonNext>
-          <ButtonNext
-            variant={chargingSpeed === 'quick' ? 'contained' : 'outlined'}
-            size="xs"
-            onClick={() => setChargingSpeed('quick')}
-            fullWidth
-          >
-            급속 충전기 그룹
-          </ButtonNext>
-        </FlexBox>
-      </FlexBox>
+      <Title />
+      {isStatisticsOpen ? (
+        <Statistics stationId={stationId} setIsStatisticsOpen={setIsStatisticsOpen} />
+      ) : (
+        <ButtonNext onClick={() => setIsStatisticsOpen(true)} fullWidth size="sm" p={2}>
+          <FlexBox justifyContent="center" alignItems="center" columnGap={2}>
+            <ChartBarSquareIcon width={24} />
+            <p>시간별 혼잡도 확인 (BETA)</p>
+            <ChevronDownIcon width={20} />
+          </FlexBox>
+        </ButtonNext>
+      )}
     </Box>
   );
 };
---
+++ frontend/src/components/ui/StationDetailsWindow/congestion/Statistics.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+
+import { useStationCongestionStatistics } from '@hooks/tanstack-query/station-details/useStationCongestionStatistics';
+
+import ButtonNext from '@common/ButtonNext';
+import FlexBox from '@common/FlexBox';
+
+import ShowHideButton from '@ui/ShowHideButton';
+import StatisticsGraph from '@ui/StatisticsGraph';
+
+import type { CHARGING_SPEED } from '@constants/chargers';
+import { ENGLISH_DAYS } from '@constants/congestion';
+
+import CongestionStatisticsSkeleton from './CongestionStatisticsSkeleton';
+
+interface Props {
+  stationId: string;
+  setIsStatisticsOpen: (pram: boolean) => void;
+}
+
+const Statistics = ({ stationId, setIsStatisticsOpen }: Props) => {
+  const { data: congestionStatistics, isLoading } = useStationCongestionStatistics(stationId);
+  const [chargingSpeed, setChargingSpeed] = useState<keyof typeof CHARGING_SPEED>('standard');
+
+  if (isLoading) {
+    return <CongestionStatisticsSkeleton />;
+  }
+
+  return (
+    <>
+      <FlexBox direction="column" gap={4} mb={3.5}>
+        <StatisticsGraph
+          statistics={congestionStatistics.congestion[chargingSpeed]}
+          menus={[...ENGLISH_DAYS]}
+          align="column"
+        />
+        <FlexBox nowrap>
+          <ButtonNext
+            variant={chargingSpeed === 'standard' ? 'contained' : 'outlined'}
+            size="xs"
+            onClick={() => setChargingSpeed('standard')}
+            fullWidth
+          >
+            완속 충전기 그룹
+          </ButtonNext>
+          <ButtonNext
+            variant={chargingSpeed === 'quick' ? 'contained' : 'outlined'}
+            size="xs"
+            onClick={() => setChargingSpeed('quick')}
+            fullWidth
+          >
+            급속 충전기 그룹
+          </ButtonNext>
+        </FlexBox>
+      </FlexBox>
+      <ShowHideButton name="닫기" onClick={() => setIsStatisticsOpen(false)} />
+    </>
+  );
+};
+
+export default Statistics;
