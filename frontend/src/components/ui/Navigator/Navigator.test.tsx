5c0c716ef86edff1a63dc0ae4532f82d890c42b4
kyw0716 <kyw0716@naver.com>
2023-09-13 00:24:58 +0900

    test: 차량 선택 모달 렌더링 테스트 추가
    [#618]

--- frontend/src/components/ui/Navigator/Navigator.test.tsx
+++ frontend/src/components/ui/Navigator/Navigator.test.tsx
@@ -4,10 +4,14 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { memberTokenStore } from '@stores/login/memberTokenStore';
 
+import { useCars } from '@hooks/tanstack-query/car/useCars';
+
 import ModalContainer from '@ui/ModalContainer';
 import NavigationBar from '@ui/compound/NavigationBar';
 
 import { EMPTY_MEMBER_TOKEN } from '@constants';
+
+jest.mock('@hooks/tanstack-query/car/useCars');
 
 const queryClient = new QueryClient();
 
@@ -41,6 +45,42 @@ describe('Navigator 컴포넌트 테스트', () => {
     expect(memberTokenStore.getState()).toBe(EMPTY_MEMBER_TOKEN);
   });
 
+  it('차량 등록 버튼을 클릭하면 차량 선택 모달이 렌더링 된다.', async () => {
+    (useCars as jest.Mock).mockReturnValue({
+      data: [
+        {
+          carId: 1,
+          name: 'dummy car1',
+          vintage: 'dummy vintage1',
+        },
+        {
+          carId: 2,
+          name: 'dummy car2',
+          vintage: 'dummy vintage2',
+        },
+        {
+          carId: 3,
+          name: 'dummy car3',
+          vintage: 'dummy vintage3',
+        },
+      ],
+      isLoading: false,
+    });
+    memberTokenStore.setState('test-member-token');
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <ModalContainer />
+        <NavigationBar.Menu />
+      </QueryClientProvider>
+    );
+
+    fireEvent.click(screen.getByLabelText('프로필 메뉴 열기'));
+    fireEvent.click(screen.getByText('차량등록'));
+
+    expect(screen.getByText('차량 선택')).toBeInTheDocument();
+  });
+
   it('멤버 토큰이 있을 때 프로필 버튼을 누르면 프로필 메뉴가 렌더링 된다.', () => {
     memberTokenStore.setState('test-member-token');
 
@@ -55,16 +95,5 @@ describe('Navigator 컴포넌트 테스트', () => {
 
     expect(screen.getByText('차량등록')).toBeInTheDocument();
     expect(screen.getByText('로그아웃')).toBeInTheDocument();
-  });
-
-  it('차량 등록 버튼을 클릭하면 차량 선택 모달이 렌더링 된다.', () => {
-    memberTokenStore.setState('test-member-token');
-
-    render(
-      <QueryClientProvider client={queryClient}>
-        <ModalContainer />
-        <NavigationBar.Menu />
-      </QueryClientProvider>
-    );
   });
 });
