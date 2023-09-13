863be6f57224f052b8c2ef1e5b88048631b084e5
Dain Lee <feb.dain@gmail.com>
2023-09-13 15:40:16 +0900

    refactor: 확장성을 위해 page도 함께 요청
    [#714]

--- frontend/src/constants/stationSearch.ts
+++ frontend/src/constants/stationSearch.ts
@@ -1,3 +1,3 @@
 export const SEARCH_SCOPE =
-  '&scope=stationId&scope=stationName&scope=address&scope=latitude&scope=longitude&limit=12';
+  '&scope=stationId&scope=stationName&scope=address&scope=latitude&scope=longitude&page=1&limit=12';
 export const MAX_SEARCH_RESULTS = 10;
