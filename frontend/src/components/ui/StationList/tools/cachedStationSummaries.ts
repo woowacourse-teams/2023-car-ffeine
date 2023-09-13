6a1de7bb9e55ae26d6f1484c896909b080702b4d
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-13 17:36:30 +0900

    feat: 좌측 리스트에는 스크린 영역 내의 유효한 충전소만 출력되도록 하는 기능 구현
    [#713]

--- frontend/src/components/ui/StationList/tools/cachedStationSummaries.ts
+++ frontend/src/components/ui/StationList/tools/cachedStationSummaries.ts
@@ -1,6 +1,13 @@
+import { getDisplayPosition } from '@utils/google-maps';
+
+import { getGoogleMapStore } from '@stores/google-maps/googleMapStore';
+
+import { DELTA_FACTOR } from '@constants/googleMaps';
+
 import type { StationSummary } from '@type';
 
 const cachedStationSummaries = new Map<string, StationSummary>();
+
 export const cachedStationSummariesActions = {
   add: (stationSummaries: StationSummary[]) => {
     console.log(`before: ${cachedStationSummaries.size}`);
@@ -12,7 +19,40 @@ export const cachedStationSummariesActions = {
     console.log(`after: ${cachedStationSummaries.size}`);
   },
   get: () => {
-    return Array.from(cachedStationSummaries.values());
+    const googleMap = getGoogleMapStore().getState();
+    const { latitude, latitudeDelta, longitude, longitudeDelta } = getDisplayPosition(googleMap);
+
+    const northEastBoundary = {
+      latitude: latitude + latitudeDelta * DELTA_FACTOR,
+      longitude: longitude + longitudeDelta * DELTA_FACTOR,
+    };
+
+    const southWestBoundary = {
+      latitude: latitude - latitudeDelta * DELTA_FACTOR,
+      longitude: longitude - longitudeDelta * DELTA_FACTOR,
+    };
+
+    const isStationLatitudeWithinBounds = (station: StationSummary) => {
+      return (
+        station.latitude > southWestBoundary.latitude &&
+        station.latitude < northEastBoundary.latitude
+      );
+    };
+
+    const isStationLongitudeWithinBounds = (station: StationSummary) => {
+      return (
+        station.longitude > southWestBoundary.longitude &&
+        station.longitude < northEastBoundary.longitude
+      );
+    };
+
+    const filteredStationSummaries = Array.from(cachedStationSummaries.values()).filter(
+      (station) => isStationLatitudeWithinBounds(station) && isStationLongitudeWithinBounds(station)
+    );
+
+    console.log(filteredStationSummaries.length);
+
+    return filteredStationSummaries;
   },
   clear: (stationIds: string[]) => {
     stationIds.forEach((stationId) => cachedStationSummaries.delete(stationId));
