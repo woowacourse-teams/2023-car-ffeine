c6ea8739b50aa64fcf6031fc6edb0fd0be870202
kyw0716 <kyw0716@naver.com>
2023-09-14 15:30:10 +0900

    refactor: 컨테이너에서 마커를 한번에 출력하도록 수정
    [#732]

--- frontend/src/components/google-maps/marker/StationMarkersContainer.tsx
+++ frontend/src/components/google-maps/marker/StationMarkersContainer.tsx
@@ -1,21 +1,72 @@
+import { createRoot } from 'react-dom/client';
+
+import { getStoreSnapshot, useSetExternalState } from '@utils/external-state/tools';
+
+import { getGoogleMapStore } from '@stores/google-maps/googleMapStore';
+import { markerInstanceStore } from '@stores/google-maps/markerInstanceStore';
+
 import { useStationMarkers } from '@hooks/tanstack-query/station-markers/useStationMarkers';
 
-import Marker from './Marker';
+import CarFfeineMarker from '@ui/CarFfeineMarker';
+
+import type { StationMarker } from '@type';
+
+const createMarkerInstances = (markers: StationMarker[]) => {
+  return markers.map((marker) => {
+    const { latitude, longitude, stationName, stationId } = marker;
+
+    const markerInstance = new google.maps.marker.AdvancedMarkerElement({
+      position: { lat: latitude, lng: longitude },
+      title: stationName,
+    });
+
+    return {
+      stationId,
+      markerInstance,
+    };
+  });
+};
 
 const StationMarkersContainer = () => {
   const { data: stationMarkers, isSuccess } = useStationMarkers();
+  const googleMap = getStoreSnapshot(getGoogleMapStore());
+  const setMarkerInstances = useSetExternalState(markerInstanceStore);
 
   if (!stationMarkers || !isSuccess) {
     return <></>;
   }
 
-  return (
-    <>
-      {stationMarkers.map((stationMarker) => {
-        return <Marker key={stationMarker.stationId} stationMarker={stationMarker} />;
-      })}
-    </>
-  );
+  setMarkerInstances((prevMarkerInstances) => {
+    const markerInstances = createMarkerInstances(stationMarkers);
+
+    // const markerInstancesOutOfBoundary = prevMarkerInstances.filter(
+    //   ({ stationId: prevStationId }) =>
+    //     !stationMarkers.some(({ stationId }) => prevStationId === stationId)
+    // );
+
+    // markerInstancesOutOfBoundary.forEach(({ markerInstance }) => {
+    //   markerInstance.map = null;
+    // });
+
+    return markerInstances;
+  });
+
+  markerInstanceStore.getState().forEach(({ markerInstance }, index) => {
+    const container = document.createElement('div');
+
+    markerInstance.content = container;
+    markerInstance.map = googleMap;
+
+    createRoot(container).render(<CarFfeineMarker {...stationMarkers[index]} />);
+  });
+
+  // return (
+  //   <>
+  //     {stationMarkers.map((stationMarker) => {
+  //       return <Marker key={stationMarker.stationId} stationMarker={stationMarker} />;
+  //     })}
+  //   </>
+  // );
 };
 
 export default StationMarkersContainer;
