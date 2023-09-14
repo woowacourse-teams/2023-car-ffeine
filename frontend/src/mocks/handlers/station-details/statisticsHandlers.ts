39a37488e3ba21813b73654d1f6d247bfc759c47
kyw0716 <kyw0716@naver.com>
2023-09-14 14:03:28 +0900

    refactor: 타입 수정
    [#725]

--- frontend/src/mocks/handlers/station-details/statisticsHandlers.ts
+++ frontend/src/mocks/handlers/station-details/statisticsHandlers.ts
@@ -1,8 +1,10 @@
 import { getCongestionStatistics } from '@mocks/data';
 import { rest } from 'msw';
 
-import { ENGLISH_DAYS_OF_WEEK_LONG_TO_SHORT, SHORT_ENGLISH_DAYS_OF_WEEK } from '@constants/congestion';
+import { ENGLISH_DAYS_OF_WEEK_LONG_TO_SHORT } from '@constants/congestion';
 import { DEVELOP_SERVER_URL } from '@constants/server';
+
+import type { ShortEnglishDaysOfWeek } from '@type';
 
 export const statisticsHandlers = [
   rest.get(`${DEVELOP_SERVER_URL}/stations/:stationId/statistics`, (req, res, ctx) => {
@@ -10,9 +12,7 @@ export const statisticsHandlers = [
       .split('?')[0]
       .replace(/\/api\/stations\//, '')
       .replace(/\/statistics/, '');
-    const dayOfWeek = req.url.searchParams.get(
-      'dayOfWeek'
-    ) as (typeof SHORT_ENGLISH_DAYS_OF_WEEK)[number];
+    const dayOfWeek = req.url.searchParams.get('dayOfWeek') as ShortEnglishDaysOfWeek;
 
     const fullCongestionStatistics = getCongestionStatistics(stationId);
     const congestionStatistics = {
