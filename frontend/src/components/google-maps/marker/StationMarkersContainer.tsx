8f987f1135af6971c2c5659e4edc6ffd7c7a306e
kyw0716 <kyw0716@naver.com>
2023-09-14 21:25:15 +0900

    refactor: 필요한 타입 import
    [#732]

--- frontend/src/components/google-maps/marker/StationMarkersContainer.tsx
+++ frontend/src/components/google-maps/marker/StationMarkersContainer.tsx
@@ -1,8 +1,9 @@
 import { createRoot } from 'react-dom/client';
 
-import { getStoreSnapshot, useSetExternalState } from '@utils/external-state/tools';
+import { getStoreSnapshot } from '@utils/external-state/tools';
 
 import { getGoogleMapStore } from '@stores/google-maps/googleMapStore';
+import type { StationMarkerInstance } from '@stores/google-maps/markerInstanceStore';
 import { markerInstanceStore } from '@stores/google-maps/markerInstanceStore';
 
 import { useStationMarkers } from '@hooks/tanstack-query/station-markers/useStationMarkers';
@@ -11,6 +12,7 @@ import CarFfeineMarker from '@ui/CarFfeineMarker';
 
 import type { StationMarker } from '@type';
 
+// TODO: 새로운 영역과 겹치는 영역에 대해서는 마커 인스턴스를 생성하지 않아야 한다.
 const createMarkerInstances = (markers: StationMarker[]) => {
   return markers.map((marker) => {
     const { latitude: lat, longitude: lng, stationName, stationId } = marker;
@@ -27,10 +29,23 @@ const createMarkerInstances = (markers: StationMarker[]) => {
   });
 };
 
+const removeMarkerOutOfBounds = (
+  prevMarkers: StationMarkerInstance[],
+  currentMarkers: StationMarkerInstance[]
+) => {
+  const markersOutOfBounds = prevMarkers.filter(
+    (prevMarker) =>
+      !currentMarkers.some((currentMarker) => currentMarker.stationId === prevMarker.stationId)
+  );
+
+  markersOutOfBounds.forEach((marker) => {
+    marker.markerInstance.map = null;
+  });
+};
+
 const StationMarkersContainer = () => {
   const { data: stationMarkers, isSuccess } = useStationMarkers();
   const googleMap = getStoreSnapshot(getGoogleMapStore());
-  const setMarkerInstances = useSetExternalState(markerInstanceStore);
 
   if (!stationMarkers || !isSuccess) {
     return <></>;
@@ -38,16 +53,28 @@ const StationMarkersContainer = () => {
 
   const markerInstances = createMarkerInstances(stationMarkers);
 
-  markerInstances.forEach(({ markerInstance }, index) => {
-    const container = document.createElement('div');
+  // 필터링을 여기서 걸지 말고 위에서 걸어야될지도?
+  markerInstances
+    .filter(
+      (marker) =>
+        !markerInstanceStore
+          .getState()
+          .some((prevMarker) => prevMarker.stationId === marker.stationId)
+    )
+    .forEach(({ markerInstance }, index) => {
+      const container = document.createElement('div');
 
-    markerInstance.content = container;
-    markerInstance.map = googleMap;
+      markerInstance.content = container;
+      markerInstance.map = googleMap;
 
-    createRoot(container).render(<CarFfeineMarker {...stationMarkers[index]} />);
-  });
+      createRoot(container).render(<CarFfeineMarker {...stationMarkers[index]} />);
+    });
 
-  setMarkerInstances(markerInstances);
+  removeMarkerOutOfBounds(markerInstanceStore.getState(), markerInstances);
+
+  markerInstanceStore.setState(markerInstances);
+
+  return <></>;
 };
 
 export default StationMarkersContainer;
