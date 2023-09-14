e4e8b691bcf801a6a0138f804f4d3f2697f4aad3
Gabriel Ju Hyun, Yoon <gabrielyoon7@gmail.com>
2023-09-14 10:14:43 +0900

    feat: 충전소 리스트 정보를 캐싱할 클래스를 구현한다 (#719)
    * feat: cachedStationSummaries 추가
    
    Co-authored-by: YoungWoo Kim <kyw0716@users.noreply.github.com>
    
    [#713]
    
    * feat: stationSummaries 수신 시 캐시 저장소에 추가하는 기능 구현
    
    [#713]
    
    * refactor: 캐시된 충전소 요약 정보를 다시 요청하지 않도록 개선
    
    [#713]
    
    * refactor: 더보기 버튼 길이 수정
    
    [#713]
    
    * refactor: 리스트가 중복으로 쌓이는 버그 수정
    
    Co-authored-by: YoungWoo Kim <kyw0716@users.noreply.github.com>
    
    [#713]
    
    * feat: 좌측 리스트에는 스크린 영역 내의 유효한 충전소만 출력되도록 하는 기능 구현
    
    [#713]
    
    * refactor: 주석 제거
    
    [#713]
    
    * refactor: 불필요한 출력문 제거
    
    [#713]
    
    * refactor: 불필요한 출력문 제거
    
    [#713]
    
    * refactor: 변수명 분리
    
    [#713]
    
    * refactor: 변수명 변경
    
    [#713]
    
    * refactor: 변수명 변경
    
    [#713]
    
    * refactor: 변수명 개선
    
    [#713]
    
    Co-authored-by: kyw0716 <kyw0716@naver.com>

--- frontend/src/components/ui/StationList/StationList.tsx
+++ frontend/src/components/ui/StationList/StationList.tsx
@@ -11,7 +11,8 @@ import StationSummaryCardSkeleton from '@ui/StationList/StationSummaryCardSkelet
 import { MOBILE_BREAKPOINT } from '@constants';
 
 import StationSummaryCard from './StationSummaryCard';
-import { useStationSummaries } from './hooks/useStationSummaries';
+import { useFetchStationSummaries } from './hooks/useFetchStationSummaries';
+import { cachedStationSummariesActions } from './tools/cachedStationSummaries';
 
 const StationList = () => {
   const {
@@ -22,10 +23,11 @@ const StationList = () => {
 
   const {
     isLoading: isStationSummariesLoading,
-    stationSummaries,
     loadMore,
     hasNextPage,
-  } = useStationSummaries(filteredMarkers ?? []);
+  } = useFetchStationSummaries(filteredMarkers ?? []);
+
+  const cachedStationSummaries = cachedStationSummariesActions.get();
 
   if (isFilteredMarkersLoading) {
     return (
@@ -39,9 +41,9 @@ const StationList = () => {
 
   // TODO: 초기에 텅 안보이게 하기
   if (
-    stationSummaries.length === 0 &&
     isStationSummariesLoading === false &&
-    isFilteredMarkersSuccess
+    isFilteredMarkersSuccess &&
+    cachedStationSummaries.length === 0
   ) {
     return <EmptyStationsNotice />;
   }
@@ -49,19 +51,9 @@ const StationList = () => {
   return (
     isFilteredMarkersSuccess && (
       <List css={searchResultList}>
-        {/* 
-          캐싱된 값을 먼저 보여주기
-            - 10개 제한 없고, 그냥 알고 있는거 다 보여주기
-            - 화면 영역 내에 있는 것만 보여주기
-            - useSyncExternalStore 사용 금지 (상태로 취급 ㄴㄴ)
-            - 
-        */}
-
-        {/* 새로 수신한 데이터를 보여주기 */}
-        {stationSummaries.length > 0 &&
-          stationSummaries.map((stationSummary) => (
-            <StationSummaryCard key={stationSummary.stationId} station={stationSummary} />
-          ))}
+        {cachedStationSummaries.map((stationSummary) => (
+          <StationSummaryCard key={stationSummary.stationId} station={stationSummary} />
+        ))}
         {isStationSummariesLoading && (
           <>
             {Array.from({ length: 10 }, (_, index) => (
@@ -69,7 +61,11 @@ const StationList = () => {
             ))}
           </>
         )}
-        {hasNextPage && <ButtonNext onClick={loadMore}>더 보 기</ButtonNext>}
+        {hasNextPage && (
+          <ButtonNext onClick={loadMore} fullWidth>
+            더 보 기
+          </ButtonNext>
+        )}
       </List>
     )
   );
