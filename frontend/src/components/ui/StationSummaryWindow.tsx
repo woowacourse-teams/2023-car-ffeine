0c5a16c02de74a029b3907767bd0fe1891650418
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-15 14:53:01 +0900

    refactor: StationSummaryWindow Loader 대체
    [#736]

--- frontend/src/components/ui/StationSummaryWindow.tsx
+++ frontend/src/components/ui/StationSummaryWindow.tsx
@@ -12,13 +12,12 @@ import { useFetchStationSummary } from '@hooks/fetch/useFetchStationSummary';
 import Button from '@common/Button';
 import ButtonNext from '@common/ButtonNext';
 import FlexBox from '@common/FlexBox';
-import List from '@common/List';
 import ListItem from '@common/ListItem';
+import Loader from '@common/Loader';
 import Text from '@common/Text';
 
 import ChargingSpeedIcon from './ChargingSpeedIcon';
 import StationDetailsWindow from './StationDetailsWindow';
-import StationSummaryCardSkeleton from './StationList/StationSummaryCardSkeleton';
 import { useNavigationBar } from './compound/NavigationBar/hooks/useNavigationBar';
 
 interface Props {
@@ -31,24 +30,7 @@ const StationSummaryWindow = ({ stationId }: Props) => {
   const { openLastPanel } = useNavigationBar();
 
   const { isLoading, stationSummary } = useFetchStationSummary(stationId);
-
-  if (isLoading || stationSummary === null) {
-    return (
-      <List>
-        <StationSummaryCardSkeleton />
-      </List>
-    );
-  }
-
-  const {
-    stationName,
-    companyName,
-    address,
-    operatingTime,
-    isPrivate,
-    isParkingFree,
-    quickChargerCount,
-  } = stationSummary;
+  const { stationName, companyName, address, operatingTime, quickChargerCount } = stationSummary;
 
   const handleOpenStationDetail = () => {
     setSelectedStationId(stationId);
@@ -60,13 +42,18 @@ const StationSummaryWindow = ({ stationId }: Props) => {
     infoWindowInstance.infoWindowInstance.close();
   };
 
+  if (isLoading || stationSummary === null) {
+    return (
+      <FlexBox justifyContent="center" alignItems="center" height="184.39px">
+        <Loader size="xxl" />
+      </FlexBox>
+    );
+  }
+
   return (
     <>
       <ListItem tag="div" key={stationId} css={padding}>
         <Button width="100%" shadow css={foundStationButton} onClick={handleOpenStationDetail}>
-          {/*<Button onClick={handleCloseStationSummary} css={closeButtonCss}>*/}
-          {/*  <XMarkIcon fill="#333" width={24} />*/}
-          {/*</Button>*/}
           <FlexBox alignItems="start" justifyContent="between" nowrap columnGap={2.8}>
             <article>
               <Text
@@ -88,14 +75,6 @@ const StationSummaryWindow = ({ stationId }: Props) => {
               <Text variant="caption" align="left" lineClamp={1} mb={1.8} color="#585858">
                 {operatingTime}
               </Text>
-              {/*<FlexBox columnGap={3}>*/}
-              {/*  <Text variant="label" align="left" color="#4b4b4b" css={labelStyle}>*/}
-              {/*    {isPrivate ? '이용 제한' : '외부인 개방'}*/}
-              {/*  </Text>*/}
-              {/*  <Text variant="label" align="left" color="#4b4b4b" css={labelStyle}>*/}
-              {/*    {isParkingFree ? '무료 주차' : '유료 주차'}*/}
-              {/*  </Text>*/}
-              {/*</FlexBox>*/}
             </article>
             {quickChargerCount !== 0 && <ChargingSpeedIcon />}
           </FlexBox>
@@ -113,7 +92,6 @@ const StationSummaryWindow = ({ stationId }: Props) => {
           <ButtonNext
             variant="contained"
             size="xs"
-            // fullWidth
             color="dark"
             css={{ width: '68%' }}
             onClick={handleOpenStationDetail}
@@ -135,22 +113,8 @@ const companyNameText = css`
 `;
 
 const foundStationButton = css`
-  padding: 1.2rem 1.2rem 1.6rem;
+  padding: 1.2rem 1.2rem 1.4rem;
   box-shadow: none;
 `;
 
-const labelStyle = css`
-  padding: 0.2rem 1rem 0.3rem;
-  background: var(--light-color);
-  border-radius: 8px;
-`;
-
-const closeButtonCss = css`
-  position: absolute;
-  top: 0.8rem;
-  right: 0.8rem;
-`;
-
 export default StationSummaryWindow;
-
-//0.4rem 1.2rem 1.6rem 1.2rem
