d78486b95fe23e26184827bea8d6c3df1550167e
Gabriel Ju Hyun, Yoon <gabrielyoon7@gmail.com>
2023-09-18 11:09:13 +0900

    refactor: 설문조사 페이지 이동 시 사용자에게 허가를 받도록 개선한다 (#761)
    * refactor: 설문조사 페이지로 이동하기 전 질문하는 기능 구현
    
    [#759]
    
    * refactor: PersonalMenu 분리
    
    [#759]
    
    * refactor: 로그인 여부를 변수로 분리
    
    [#759]

--- frontend/src/components/ui/compound/NavigationBar/Menu.tsx
+++ frontend/src/components/ui/compound/NavigationBar/Menu.tsx
@@ -1,22 +1,12 @@
 import { AdjustmentsHorizontalIcon, Bars3Icon, UserCircleIcon } from '@heroicons/react/24/outline';
-import { ArrowRightOnRectangleIcon, PencilSquareIcon } from '@heroicons/react/24/solid';
 import { css } from 'styled-components';
 
-import type { PropsWithChildren } from 'react';
 import { HiArrowPath, HiOutlineChatBubbleOvalLeftEllipsis } from 'react-icons/hi2';
 
-import { useQueryClient } from '@tanstack/react-query';
-
 import { useExternalValue } from '@utils/external-state';
-import { logout } from '@utils/login';
 
 import { modalActions } from '@stores/layout/modalStore';
 import { memberTokenStore } from '@stores/login/memberTokenStore';
-import {
-  selectedCapacitiesFilterStore,
-  selectedCompaniesFilterStore,
-  selectedConnectorTypesFilterStore,
-} from '@stores/station-filters/serverStationFiltersStore';
 
 import Button from '@common/Button';
 import FlexBox from '@common/FlexBox';
@@ -25,57 +15,24 @@ import ServerStationFilters from '@ui/ServerStationFilters';
 import StationListWindow from '@ui/StationList/StationListWindow';
 import StationSearchWindow from '@ui/StationSearchWindow';
 import LogoIcon from '@ui/Svg/LogoIcon';
-import CarModal from '@ui/modal/CarModal/CarModal';
+import PersonalMenu from '@ui/compound/NavigationBar/PersonalMenu';
 import LoginModal from '@ui/modal/LoginModal/LoginModal';
 
 import { displayNoneInMobile, displayNoneInWeb } from '@style/mediaQuery';
 
 import { EMPTY_MEMBER_TOKEN, MOBILE_BREAKPOINT } from '@constants';
-import { QUERY_KEY_STATION_MARKERS } from '@constants/queryKeys';
 
-import PopupMenu from '../PopupMenu';
 import { useNavigationBar } from './hooks/useNavigationBar';
 
 const Menu = () => {
   const { openBasePanel } = useNavigationBar();
 
   const memberToken = useExternalValue(memberTokenStore);
-  const queryClient = useQueryClient();
-
-  const { openModal } = modalActions;
+  const isSignIn = memberToken !== EMPTY_MEMBER_TOKEN;
 
   const handleClickLoginIcon = () => {
     modalActions.openModal(<LoginModal />);
   };
-
-  const loginMenus: PropsWithChildren<{ onClick: () => void }>[] = [
-    {
-      children: (
-        <>
-          <PencilSquareIcon width="1.8rem" color="#333" /> 차량등록
-        </>
-      ),
-      onClick: () => {
-        openModal(<CarModal />);
-      },
-    },
-    {
-      children: (
-        <>
-          <ArrowRightOnRectangleIcon width="1.8rem" color="#333" /> 로그아웃
-        </>
-      ),
-      onClick: () => {
-        logout();
-
-        selectedCapacitiesFilterStore.setState(new Set([]));
-        selectedConnectorTypesFilterStore.setState(new Set([]));
-        selectedCompaniesFilterStore.setState(new Set([]));
-
-        queryClient.invalidateQueries({ queryKey: [QUERY_KEY_STATION_MARKERS] });
-      },
-    },
-  ];
 
   return (
     <FlexBox css={[fixedPositionCss, paddingCss, borderCss, flexCss]} noRadius="all" nowrap>
@@ -94,16 +51,19 @@ const Menu = () => {
       >
         <Bars3Icon width="2.8rem" stroke="#333" />
       </Button>
+
       <Button aria-label="필터링 메뉴 열기" onClick={() => openBasePanel(<ServerStationFilters />)}>
         <AdjustmentsHorizontalIcon width="2.8rem" stroke="#333" />
       </Button>
-      {memberToken !== EMPTY_MEMBER_TOKEN ? (
-        <PopupMenu menus={loginMenus} />
+
+      {isSignIn ? (
+        <PersonalMenu />
       ) : (
         <Button aria-label="로그인 하기" onClick={handleClickLoginIcon}>
           <UserCircleIcon width="2.8rem" stroke="#333" />
         </Button>
       )}
+
       <Button
         css={displayNoneInWeb}
         aria-label="충전소 리스트 열기"
@@ -111,9 +71,14 @@ const Menu = () => {
       >
         <Bars3Icon width="2.8rem" stroke="#333" />
       </Button>
+
       <Button
         aria-label="설문조사 하기"
-        onClick={() => window.open('https://forms.gle/YQKx1zchUifjUJ396')}
+        onClick={() => {
+          if (confirm('설문조사에 참여하시겠습니까?')) {
+            window.open('https://forms.gle/YQKx1zchUifjUJ396');
+          }
+        }}
       >
         <HiOutlineChatBubbleOvalLeftEllipsis size="2.8rem" stroke="#333" color="#333" />
       </Button>
---
+++ frontend/src/components/ui/compound/NavigationBar/PersonalMenu.tsx
@@ -0,0 +1,56 @@
+import { ArrowRightOnRectangleIcon, PencilSquareIcon } from '@heroicons/react/24/solid';
+
+import type { PropsWithChildren } from 'react';
+
+import { useQueryClient } from '@tanstack/react-query';
+
+import { logout } from '@utils/login';
+
+import { modalActions } from '@stores/layout/modalStore';
+import {
+  selectedCapacitiesFilterStore,
+  selectedCompaniesFilterStore,
+  selectedConnectorTypesFilterStore,
+} from '@stores/station-filters/serverStationFiltersStore';
+
+import PopupMenu from '@ui/compound/PopupMenu';
+import CarModal from '@ui/modal/CarModal/CarModal';
+
+import { QUERY_KEY_STATION_MARKERS } from '@constants/queryKeys';
+
+const PersonalMenu = () => {
+  const queryClient = useQueryClient();
+  const { openModal } = modalActions;
+
+  const personalMenus: PropsWithChildren<{ onClick: () => void }>[] = [
+    {
+      children: (
+        <>
+          <PencilSquareIcon width="1.8rem" color="#333" /> 차량등록
+        </>
+      ),
+      onClick: () => {
+        openModal(<CarModal />);
+      },
+    },
+    {
+      children: (
+        <>
+          <ArrowRightOnRectangleIcon width="1.8rem" color="#333" /> 로그아웃
+        </>
+      ),
+      onClick: () => {
+        logout();
+
+        selectedCapacitiesFilterStore.setState(new Set([]));
+        selectedConnectorTypesFilterStore.setState(new Set([]));
+        selectedCompaniesFilterStore.setState(new Set([]));
+
+        queryClient.invalidateQueries({ queryKey: [QUERY_KEY_STATION_MARKERS] });
+      },
+    },
+  ];
+
+  return <PopupMenu menus={personalMenus} />;
+};
+export default PersonalMenu;
