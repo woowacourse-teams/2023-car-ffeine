cb3c65ea7a8cbba2a12f645407116f6424d7e86d
Gabriel Ju Hyun, Yoon <gabrielyoon7@gmail.com>
2023-09-19 15:52:43 +0900

    refactor: 충전소 혼잡도가 처음부터 보이도록 개선한다 (#766)
    * refactor: 충전소 혼잡도가 처음부터 보이도록 개선한다
    
    [#764]
    
    * refactor: 충전소 혼잡도가 접히더라도 레이아웃이 유지되도록 한다
    
    [#764]
    
    * refactor: suspense 오류 수정
    
    [#764]
    
    * refactor: 혼잡도 열기 닫기 기능 제거
    
    [#764]

--- frontend/src/components/ui/StationDetailsWindow/congestion/CongestionStatistics.tsx
+++ frontend/src/components/ui/StationDetailsWindow/congestion/CongestionStatistics.tsx
@@ -1,10 +1,6 @@
-import { ChartBarSquareIcon, ChevronDownIcon } from '@heroicons/react/24/outline';
-
 import { useState } from 'react';
 
 import Box from '@common/Box';
-import ButtonNext from '@common/ButtonNext';
-import FlexBox from '@common/FlexBox';
 
 import { ENGLISH_DAYS_OF_WEEK } from '@constants/congestion';
 
@@ -22,27 +18,11 @@ const CongestionStatistics = ({ stationId }: CongestionStatisticsProps) => {
   const [dayOfWeek, setDayOfWeek] = useState<LongEnglishDaysOfWeek>(
     ENGLISH_DAYS_OF_WEEK[todayIndex < 0 ? 6 : todayIndex]
   );
-  const [isStatisticsOpen, setIsStatisticsOpen] = useState(false);
 
   return (
     <Box my={5}>
       <Title />
-      {isStatisticsOpen ? (
-        <Statistics
-          stationId={stationId}
-          setIsStatisticsOpen={setIsStatisticsOpen}
-          dayOfWeek={dayOfWeek}
-          onChangeDayOfWeek={setDayOfWeek}
-        />
-      ) : (
-        <ButtonNext onClick={() => setIsStatisticsOpen(true)} fullWidth size="sm" p={2}>
-          <FlexBox justifyContent="center" alignItems="center" columnGap={2}>
-            <ChartBarSquareIcon width={24} />
-            <p>시간별 혼잡도 확인 (BETA)</p>
-            <ChevronDownIcon width={20} />
-          </FlexBox>
-        </ButtonNext>
-      )}
+      <Statistics stationId={stationId} dayOfWeek={dayOfWeek} onChangeDayOfWeek={setDayOfWeek} />
     </Box>
   );
 };
