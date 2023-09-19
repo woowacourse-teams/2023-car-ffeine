c370bf82ba289b9ccb81bf462c834df663afd216
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-19 15:37:54 +0900

    refactor: 혼잡도 열기 닫기 기능 제거
    [#764]

--- frontend/src/components/ui/StationDetailsWindow/congestion/CongestionStatistics.tsx
+++ frontend/src/components/ui/StationDetailsWindow/congestion/CongestionStatistics.tsx
@@ -1,10 +1,6 @@
-import { ChartBarSquareIcon, ChevronDownIcon } from '@heroicons/react/24/outline';
-
 import { useState } from 'react';
 
 import Box from '@common/Box';
-import ButtonNext from '@common/ButtonNext';
-import FlexBox from '@common/FlexBox';
 
 import { ENGLISH_DAYS_OF_WEEK } from '@constants/congestion';
 
@@ -22,29 +18,11 @@ const CongestionStatistics = ({ stationId }: CongestionStatisticsProps) => {
   const [dayOfWeek, setDayOfWeek] = useState<LongEnglishDaysOfWeek>(
     ENGLISH_DAYS_OF_WEEK[todayIndex < 0 ? 6 : todayIndex]
   );
-  const [isStatisticsOpen, setIsStatisticsOpen] = useState(true);
 
   return (
     <Box my={5}>
       <Title />
-      {isStatisticsOpen ? (
-        <Statistics
-          stationId={stationId}
-          setIsStatisticsOpen={setIsStatisticsOpen}
-          dayOfWeek={dayOfWeek}
-          onChangeDayOfWeek={setDayOfWeek}
-        />
-      ) : (
-        <FlexBox justifyContent="center" alignItems="center" minHeight={`${47.5 + 1.4 + 2.2}rem`}>
-          <ButtonNext onClick={() => setIsStatisticsOpen(true)} fullWidth size="sm" p={2}>
-            <FlexBox justifyContent="center" alignItems="center" columnGap={2}>
-              <ChartBarSquareIcon width={24} />
-              <p>요일/시간별 혼잡도 확인</p>
-              <ChevronDownIcon width={20} />
-            </FlexBox>
-          </ButtonNext>
-        </FlexBox>
-      )}
+      <Statistics stationId={stationId} dayOfWeek={dayOfWeek} onChangeDayOfWeek={setDayOfWeek} />
     </Box>
   );
 };
--- frontend/src/components/ui/StationDetailsWindow/congestion/Statistics.tsx
+++ frontend/src/components/ui/StationDetailsWindow/congestion/Statistics.tsx
@@ -6,7 +6,6 @@ import ButtonNext from '@common/ButtonNext';
 import FlexBox from '@common/FlexBox';
 
 import Error from '@ui/Error';
-import ShowHideButton from '@ui/ShowHideButton';
 import StatisticsGraph from '@ui/StatisticsGraph';
 
 import type { CHARGING_SPEED } from '@constants/chargers';
@@ -16,12 +15,11 @@ import type { LongEnglishDaysOfWeek } from '@type';
 
 interface Props {
   stationId: string;
-  setIsStatisticsOpen: (pram: boolean) => void;
   dayOfWeek: LongEnglishDaysOfWeek;
   onChangeDayOfWeek: (dayOfWeek: LongEnglishDaysOfWeek) => void;
 }
 
-const Statistics = ({ stationId, setIsStatisticsOpen, dayOfWeek, onChangeDayOfWeek }: Props) => {
+const Statistics = ({ stationId, dayOfWeek, onChangeDayOfWeek }: Props) => {
   const {
     data: congestionStatistics,
     isLoading,
@@ -47,37 +45,34 @@ const Statistics = ({ stationId, setIsStatisticsOpen, dayOfWeek, onChangeDayOfWe
   }
 
   return (
-    <>
-      <FlexBox direction="column" gap={4} mb={3.5}>
-        <StatisticsGraph
-          statistics={congestionStatistics?.congestion[chargingSpeed]}
-          menus={[...SHORT_ENGLISH_DAYS_OF_WEEK]}
-          align="column"
-          dayOfWeek={dayOfWeek}
-          onChangeDayOfWeek={onChangeDayOfWeek}
-          isLoading={isLoading}
-        />
-        <FlexBox nowrap>
-          <ButtonNext
-            variant={chargingSpeed === 'standard' ? 'contained' : 'outlined'}
-            size="xs"
-            onClick={() => setChargingSpeed('standard')}
-            fullWidth
-          >
-            완속 충전기 그룹
-          </ButtonNext>
-          <ButtonNext
-            variant={chargingSpeed === 'quick' ? 'contained' : 'outlined'}
-            size="xs"
-            onClick={() => setChargingSpeed('quick')}
-            fullWidth
-          >
-            급속 충전기 그룹
-          </ButtonNext>
-        </FlexBox>
+    <FlexBox direction="column" gap={4} mb={3.5}>
+      <StatisticsGraph
+        statistics={congestionStatistics?.congestion[chargingSpeed]}
+        menus={[...SHORT_ENGLISH_DAYS_OF_WEEK]}
+        align="column"
+        dayOfWeek={dayOfWeek}
+        onChangeDayOfWeek={onChangeDayOfWeek}
+        isLoading={isLoading}
+      />
+      <FlexBox nowrap>
+        <ButtonNext
+          variant={chargingSpeed === 'standard' ? 'contained' : 'outlined'}
+          size="xs"
+          onClick={() => setChargingSpeed('standard')}
+          fullWidth
+        >
+          완속 충전기 그룹
+        </ButtonNext>
+        <ButtonNext
+          variant={chargingSpeed === 'quick' ? 'contained' : 'outlined'}
+          size="xs"
+          onClick={() => setChargingSpeed('quick')}
+          fullWidth
+        >
+          급속 충전기 그룹
+        </ButtonNext>
       </FlexBox>
-      <ShowHideButton name="닫기" onClick={() => setIsStatisticsOpen(false)} />
-    </>
+    </FlexBox>
   );
 };
 
