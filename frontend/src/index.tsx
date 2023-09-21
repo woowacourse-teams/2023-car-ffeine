cf398674916c7edb8de301b6f57329573e2e0dff
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-22 00:52:31 +0900

    fix: 잘못된 파일 수정 복원
    [#779]

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,6 +6,8 @@ import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+import { mswModeActions } from '@stores/config/mswModeStore';
+
 import { GlobalStyle } from 'style/GlobalStyle';
 
 const queryClient = new QueryClient();
@@ -14,7 +16,7 @@ const main = async () => {
   const domNode = document.getElementById('root');
   const root = createRoot(domNode);
 
-  // await mswModeActions.startMsw();
+  await mswModeActions.startMsw();
 
   root.render(
     <QueryClientProvider client={queryClient}>
