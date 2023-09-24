a80b5b23e5ef28f2f257767283aba249fef72314
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-24 22:38:40 +0900

    refactor: 불필요한 mswModeStore 스토어 제거
    [#800]

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,7 +6,7 @@ import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-import { mswModeActions } from '@stores/config/mswModeStore';
+import { mswModeActions } from '@utils/mswModeActions';
 
 import { GlobalStyle } from 'style/GlobalStyle';
 
@@ -16,7 +16,9 @@ const main = async () => {
   const domNode = document.getElementById('root');
   const root = createRoot(domNode);
 
-  await mswModeActions.startMsw();
+  if (process.env.NODE_ENV === 'development') {
+    await mswModeActions.startMsw();
+  }
 
   root.render(
     <QueryClientProvider client={queryClient}>
