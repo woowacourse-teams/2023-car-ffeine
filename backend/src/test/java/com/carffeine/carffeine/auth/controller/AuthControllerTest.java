f44fa29dd489d42654a7b08023fba1698badb2a5
drunkenhw <drunkenhw@gmail.com>
2023-09-29 11:52:52 +0900

    feat: refresh token 발급 및 access token 재발급 api 추가
    [#815]

--- backend/src/test/java/com/carffeine/carffeine/auth/controller/AuthControllerTest.java
+++ backend/src/test/java/com/carffeine/carffeine/auth/controller/AuthControllerTest.java
@@ -1,6 +1,7 @@
 package com.carffeine.carffeine.auth.controller;
 
 import com.carffeine.carffeine.auth.service.dto.OAuthLoginRequest;
+import com.carffeine.carffeine.auth.service.dto.Tokens;
 import com.carffeine.carffeine.helper.MockBeanInjection;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -9,12 +10,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
 import org.springframework.test.web.servlet.MockMvc;
+
+import javax.servlet.http.Cookie;
+import java.time.Duration;
 
 import static com.carffeine.carffeine.helper.RestDocsHelper.customDocument;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
@@ -61,8 +70,15 @@ public class AuthControllerTest extends MockBeanInjection {
         String provider = "google";
 
         // when
-        when(authService.generateToken(any()))
-                .thenReturn("access token");
+        when(authService.generateTokens(any()))
+                .thenReturn(new Tokens("access token", "refreshToken"));
+        when(refreshTokenCookieGenerator.createCookie(anyString()))
+                .thenReturn(ResponseCookie.from("refresh-token", "refreshToken")
+                        .maxAge(Duration.ofMillis(10000))
+                        .path("/")
+                        .secure(true)
+                        .httpOnly(true)
+                        .build());
 
         // then
         mockMvc.perform(post("/oauth/{provider}/login", provider)
@@ -77,8 +93,49 @@ public class AuthControllerTest extends MockBeanInjection {
                         ),
                         responseFields(
                                 fieldWithPath("token").description("Access token")
+                        ),
+                        responseHeaders( //응답 헤더 문서화
+                                headerWithName(HttpHeaders.SET_COOKIE).description("refresh token")
                         )
                 ));
+    }
+
+    @Test
+    void refresh_token으로_access_token을_반환한다() throws Exception {
+        // given
+        String refreshToken = "your-refresh-token";
+        String accessToken = "new-access-token";
+        when(authService.renewAccessToken(refreshToken)).thenReturn(accessToken);
+
+        // when & then
+        mockMvc.perform(post("/renew")
+                        .cookie(new Cookie("refresh-token", refreshToken)))
+                .andExpect(status().isOk())
+                .andDo(customDocument("renew-access-token",
+                        responseFields(
+                                fieldWithPath("token").description("Renewed access token")
+                        )
+                ));
+    }
+
+    @Test
+    public void logout을_한다() throws Exception {
+        // given
+        when(refreshTokenCookieGenerator.createLogoutCookie())
+                .thenReturn(ResponseCookie.from("refreshToke", "")
+                        .maxAge(0)
+                        .build());
+
+        // when & then
+        mockMvc.perform(post("/logout")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer token~~"))
+                .andExpect(status().isOk())
+                .andDo(customDocument("logout",
+                        responseHeaders(
+                                headerWithName("Set-Cookie")
+                                        .description("Logout cookie containing refreshed token")
+                        )));
     }
 }
 
