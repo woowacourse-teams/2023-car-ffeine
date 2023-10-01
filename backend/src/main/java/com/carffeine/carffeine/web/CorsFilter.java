a0815b7273333fce042183632d703c6b653ef7ea
drunkenhw <drunkenhw@gmail.com>
2023-10-01 16:17:18 +0900

    fix: cookie 설정

--- backend/src/main/java/com/carffeine/carffeine/web/CorsFilter.java
+++ backend/src/main/java/com/carffeine/carffeine/web/CorsFilter.java
@@ -20,7 +20,7 @@ public class CorsFilter extends OncePerRequestFilter {
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Headers", "Location");
 
         filterChain.doFilter(request, response);
     }
