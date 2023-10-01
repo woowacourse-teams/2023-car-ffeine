e831a019be425f9c6006a9bc544a59caa6146cd2
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-10-01 13:30:29 +0900

    refactor: StationInfo 컴포넌트 분리
    [#820]

---
+++ frontend/src/components/ui/StationInfoWindow/StationInfo.tsx
@@ -0,0 +1,91 @@
+import { css } from 'styled-components';
+
+import type { MouseEvent } from 'react';
+
+import Button from '@common/Button';
+import FlexBox from '@common/FlexBox';
+import ListItem from '@common/ListItem';
+import Text from '@common/Text';
+
+import SummaryButtons from '@ui/StationInfoWindow/SummaryButtons';
+
+import type { StationDetails } from '@type';
+
+export interface StationInfoProps {
+  stationDetails: StationDetails;
+  handleOpenStationDetail: () => void;
+  handleCloseStationSummary: (event: MouseEvent<HTMLButtonElement>) => void;
+}
+
+const StationInfo = ({
+  stationDetails,
+  handleOpenStationDetail,
+  handleCloseStationSummary,
+}: StationInfoProps) => {
+  const {
+    address,
+    chargers,
+    companyName,
+    contact,
+    detailLocation,
+    isParkingFree,
+    isPrivate,
+    latitude,
+    longitude,
+    operatingTime,
+    privateReason,
+    reportCount,
+    stationId,
+    stationName,
+    stationState,
+  } = stationDetails;
+
+  return (
+    <ListItem tag="div" key={stationId} css={padding}>
+      <Button width="100%" shadow css={foundStationButton} onClick={handleOpenStationDetail}>
+        <FlexBox alignItems="start" justifyContent="between" nowrap columnGap={2.8}>
+          <article>
+            <Text
+              tag="h3"
+              align="left"
+              variant="subtitle"
+              title={stationName}
+              lineClamp={1}
+              css={companyNameText}
+            >
+              {companyName}
+            </Text>
+            <Text tag="h3" align="left" variant="h5" title={stationName} lineClamp={1}>
+              {stationName}
+            </Text>
+            <Text variant="body" align="left" lineClamp={1} mb={1} color="#585858">
+              {address === 'null' || !address ? '주소 미확인' : address}
+            </Text>
+            <Text variant="caption" align="left" lineClamp={1} mb={1.8} color="#585858">
+              {operatingTime}
+            </Text>
+          </article>
+          {/*{quickChargerCount !== 0 && <ChargingSpeedIcon />}*/}
+        </FlexBox>
+      </Button>
+      <SummaryButtons
+        handleCloseStationSummary={handleCloseStationSummary}
+        handleOpenStationDetail={handleOpenStationDetail}
+      />
+    </ListItem>
+  );
+};
+
+const padding = css`
+  padding: 2.4rem 2.4rem 1.8rem;
+`;
+
+const companyNameText = css`
+  font-size: 1.3rem;
+`;
+
+const foundStationButton = css`
+  box-shadow: none;
+`;
+
+export default StationInfo;
--- frontend/src/components/ui/StationInfoWindow/StationInfoWindow.tsx
+++ frontend/src/components/ui/StationInfoWindow/StationInfoWindow.tsx
@@ -1,22 +1,17 @@
-import { css } from 'styled-components';
-
 import type { MouseEvent } from 'react';
 
 import { useExternalValue } from '@utils/external-state';
 
 import { getStationInfoWindowStore } from '@stores/google-maps/stationInfoWindowStore';
 
-import Button from '@common/Button';
 import FlexBox from '@common/FlexBox';
-import ListItem from '@common/ListItem';
 import Loader from '@common/Loader';
-import Text from '@common/Text';
 
 import { useNavigationBar } from '@ui/Navigator/NavigationBar/hooks/useNavigationBar';
+import StationInfo from '@ui/StationInfoWindow/StationInfo';
 import { useFetchStationDetatils } from '@ui/StationInfoWindow/useFetchStationDetatils';
 
 import StationDetailsWindow from '../StationDetailsWindow';
-import SummaryButtons from './SummaryButtons';
 
 export interface StationSummaryProps {
   selectedStationId: string;
@@ -49,70 +44,13 @@ const StationInfoWindow = ({ selectedStationId }: StationSummaryProps) => {
     );
   }
 
-  const {
-    address,
-    chargers,
-    companyName,
-    contact,
-    detailLocation,
-    isParkingFree,
-    isPrivate,
-    latitude,
-    longitude,
-    operatingTime,
-    privateReason,
-    reportCount,
-    stationId,
-    stationName,
-    stationState,
-  } = stationDetails;
-
   return (
-    <ListItem tag="div" key={stationId} css={padding}>
-      <Button width="100%" shadow css={foundStationButton} onClick={handleOpenStationDetail}>
-        <FlexBox alignItems="start" justifyContent="between" nowrap columnGap={2.8}>
-          <article>
-            <Text
-              tag="h3"
-              align="left"
-              variant="subtitle"
-              title={stationName}
-              lineClamp={1}
-              css={companyNameText}
-            >
-              {companyName}
-            </Text>
-            <Text tag="h3" align="left" variant="h5" title={stationName} lineClamp={1}>
-              {stationName}
-            </Text>
-            <Text variant="body" align="left" lineClamp={1} mb={1} color="#585858">
-              {address === 'null' || !address ? '주소 미확인' : address}
-            </Text>
-            <Text variant="caption" align="left" lineClamp={1} mb={1.8} color="#585858">
-              {operatingTime}
-            </Text>
-          </article>
-          {/*{quickChargerCount !== 0 && <ChargingSpeedIcon />}*/}
-        </FlexBox>
-      </Button>
-      <SummaryButtons
-        handleCloseStationSummary={handleCloseStationSummary}
-        handleOpenStationDetail={handleOpenStationDetail}
-      />
-    </ListItem>
+    <StationInfo
+      stationDetails={stationDetails}
+      handleOpenStationDetail={handleOpenStationDetail}
+      handleCloseStationSummary={handleCloseStationSummary}
+    />
   );
 };
-
-const padding = css`
-  padding: 2.4rem 2.4rem 1.8rem;
-`;
-
-const companyNameText = css`
-  font-size: 1.3rem;
-`;
-
-const foundStationButton = css`
-  box-shadow: none;
-`;
 
 export default StationInfoWindow;
