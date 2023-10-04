fb7507ce7b2636bacfbf1eb56c63c0e64429d288
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-10-04 11:56:17 +0900

    rename: 변수명 변경
    [#822]

--- frontend/src/components/ui/StationListWindow/StationList.tsx
+++ frontend/src/components/ui/StationListWindow/StationList.tsx
@@ -88,10 +88,10 @@ const StationList = () => {
     );
   };
 
-  return <List css={searchResultList}>{renderStationSummaryCards()}</List>;
+  return <List css={stationListCss}>{renderStationSummaryCards()}</List>;
 };
 
-const searchResultList = css`
+const stationListCss = css`
   width: 34rem;
   height: calc(100vh - 14.133rem);
   border-top: 1.2rem solid var(--lighter-color);
