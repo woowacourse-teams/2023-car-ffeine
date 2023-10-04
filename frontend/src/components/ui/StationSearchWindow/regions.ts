2624d5667636b2b9e28e9bb58698dacf452fdd45
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-10-05 01:44:36 +0900

    feat: regions 데이터 추가
    [#825]

---
+++ frontend/src/components/ui/StationSearchWindow/regions.ts
@@ -0,0 +1,87 @@
+export const regions = [
+  {
+    name: '서울특별시',
+    latitude: 37.5666103,
+    longitude: 126.9783882,
+  },
+  {
+    name: '강원도',
+    latitude: 37.8603672,
+    longitude: 128.3115261,
+  },
+  {
+    name: '경기도',
+    latitude: 37.4363177,
+    longitude: 127.550802,
+  },
+  {
+    name: '경상남도',
+    latitude: 35.56666,
+    longitude: 128.2417453,
+  },
+  {
+    name: '경상북도',
+    latitude: 36.6308397,
+    longitude: 128.962578,
+  },
+  {
+    name: '광주광역시',
+    latitude: 35.160032,
+    longitude: 126.851338,
+  },
+  {
+    name: '대구광역시',
+    latitude: 35.87139,
+    longitude: 128.601763,
+  },
+  {
+    name: '대전광역시',
+    latitude: 36.3504396,
+    longitude: 127.3849508,
+  },
+  {
+    name: '부산광역시',
+    latitude: 35.179816,
+    longitude: 129.0750223,
+  },
+  {
+    name: '세종특별자치시',
+    latitude: 36.4803512,
+    longitude: 127.2894325,
+  },
+  {
+    name: '울산광역시',
+    latitude: 35.5394773,
+    longitude: 129.3112994,
+  },
+  {
+    name: '전라남도',
+    latitude: 34.9007274,
+    longitude: 126.9571667,
+  },
+  {
+    name: '전라북도',
+    latitude: 35.6910153,
+    longitude: 127.2368291,
+  },
+  {
+    name: '제주특별자치도',
+    latitude: 33.4273366,
+    longitude: 126.5758344,
+  },
+  {
+    name: '충청남도',
+    latitude: 36.6173379,
+    longitude: 126.8453965,
+  },
+  {
+    name: '충청북도',
+    latitude: 36.7853718,
+    longitude: 127.6551404,
+  },
+  {
+    name: '인천광역시',
+    latitude: 37.4559418,
+    longitude: 126.7051505,
+  },
+];
