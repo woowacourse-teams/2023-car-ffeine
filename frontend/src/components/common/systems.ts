9fb8b005c19900dcdc7292db6599e4c9fca24a3a
Dain Lee <feb.dain@gmail.com>
2023-10-05 15:42:50 +0900

    refactor: spacing system 개선
    [#823]

--- frontend/src/components/common/systems.ts
+++ frontend/src/components/common/systems.ts
@@ -1,60 +1,100 @@
 import { css } from 'styled-components';
 
+import { addUnit } from './utils/addUnit';
+
 export interface SpacingProps {
-  p?: number;
-  px?: number;
-  py?: number;
-  pl?: number;
-  pr?: number;
-  pt?: number;
-  pb?: number;
-  m?: number;
-  mx?: number;
-  my?: number;
-  ml?: number;
-  mr?: number;
-  mt?: number;
-  mb?: number;
+  /** 상하좌우 padding,
+   * - [number] 숫자만 적을 경우 rem 단위로 자동 변환
+   * - [string] 단위까지 적어줘야 함 (ex. 0.8rem, 1rem 2rem)
+   */
+  p?: number | string;
+  /** 좌우 padding,
+   * - [number] 숫자만 적을 경우 rem 단위로 자동 변환
+   * - [string] 단위까지 적어줘야 함 (ex. 0.8rem, 1rem 2rem)
+   */
+  px?: number | string;
+  /** 상하 padding,
+   * - [number] 숫자만 적을 경우 rem 단위로 자동 변환
+   * - [string] 단위까지 적어줘야 함 (ex. 0.8rem, 1rem 2rem)
+   */
+  py?: number | string;
+  /** 왼쪽에 padding,
+   * - [number] 숫자만 적을 경우 rem 단위로 자동 변환
+   * - [string] 단위까지 적어줘야 함 (ex. 0.8rem, 1rem 2rem)
+   */
+  pl?: number | string;
+  /** 오른쪽에 padding,
+   * - [number] 숫자만 적을 경우 rem 단위로 자동 변환
+   * - [string] 단위까지 적어줘야 함 (ex. 0.8rem, 1rem 2rem)
+   */
+  pr?: number | string;
+  /** 위에 padding,
+   * - [number] 숫자만 적을 경우 rem 단위로 자동 변환
+   * - [string] 단위까지 적어줘야 함 (ex. 0.8rem, 1rem 2rem)
+   */
+  pt?: number | string;
+  /** 아래에 padding,
+   * - [number] 숫자만 적을 경우 rem 단위로 자동 변환
+   * - [string] 단위까지 적어줘야 함 (ex. 0.8rem, 1rem 2rem)
+   */
+  pb?: number | string;
+  /** 상하좌우 margin,
+   * - [number] 숫자만 적을 경우 rem 단위로 자동 변환
+   * - [string] 단위까지 적어줘야 함 (ex. 0.8rem, 1rem 2rem)
+   */
+  m?: number | string;
+  /** 좌우 margin,
+   * - [number] 숫자만 적을 경우 rem 단위로 자동 변환
+   * - [string] 단위까지 적어줘야 함 (ex. 0.8rem, 1rem 2rem)
+   */
+  mx?: number | string;
+  /** 상하 margin,
+   * - [number] 숫자만 적을 경우 rem 단위로 자동 변환
+   * - [string] 단위까지 적어줘야 함 (ex. 0.8rem, 1rem 2rem)
+   */
+  my?: number | string;
+  /** 왼쪽에 margin,
+   * - [number] 숫자만 적을 경우 rem 단위로 자동 변환
+   * - [string] 단위까지 적어줘야 함 (ex. 0.8rem, 1rem 2rem)
+   */
+  ml?: number | string;
+  /** 오른쪽에 margin,
+   * - [number] 숫자만 적을 경우 rem 단위로 자동 변환
+   * - [string] 단위까지 적어줘야 함 (ex. 0.8rem, 1rem 2rem)
+   */
+  mr?: number | string;
+  /** 위에 margin,
+   * - [number] 숫자만 적을 경우 rem 단위로 자동 변환
+   * - [string] 단위까지 적어줘야 함 (ex. 0.8rem, 1rem 2rem)
+   */
+  mt?: number | string;
+  /** 아래에 margin,
+   * - [number] 숫자만 적을 경우 rem 단위로 자동 변환
+   * - [string] 단위까지 적어줘야 함 (ex. 0.8rem, 1rem 2rem)
+   */
+  mb?: number | string;
 }
 
 export const spacing = css<SpacingProps>`
-  ${({ p }) => p && `padding: ${p * 0.4}rem`};
-
+  ${({ p }) => p !== undefined && `padding: ${addUnit(p, 0.4)}`};
   ${({ px }) =>
-    (px || px === 0) &&
-    `
-      padding-left: ${px * 0.4}rem;
-      padding-right: ${px * 0.4}rem;
-    `}
+    px !== undefined && `padding-left: ${addUnit(px, 0.4)}; padding-right: ${addUnit(px, 0.4)}`};
   ${({ py }) =>
-    (py || py === 0) &&
-    `
-      padding-top: ${py * 0.4}rem;
-      padding-bottom: ${py * 0.4}rem;
-    `}
+    py !== undefined && `padding-top: ${addUnit(py, 0.4)}; padding-bottom: ${addUnit(py, 0.4)}`};
 
-  ${({ pl }) => pl && `padding-left: ${pl * 0.4}rem`};
-  ${({ pr }) => pr && `padding-right: ${pr * 0.4}rem`};
-  ${({ pt }) => pt && `padding-top: ${pt * 0.4}rem`};
-  ${({ pb }) => (pb || pb === 0) && `padding-bottom: ${pb * 0.4}rem`};
+  ${({ pt }) => pt !== undefined && `padding-top: ${addUnit(pt, 0.4)}`};
+  ${({ pr }) => pr !== undefined && `padding-right: ${addUnit(pr, 0.4)}`};
+  ${({ pb }) => pb !== undefined && `padding-bottom: ${addUnit(pb, 0.4)}`};
+  ${({ pl }) => pl !== undefined && `padding-left: ${addUnit(pl, 0.4)}`};
 
-  ${({ m }) => m && `margin: ${m * 0.4}rem`};
-
+  ${({ m }) => m !== undefined && `margin: ${addUnit(m, 0.4)}`};
   ${({ mx }) =>
-    mx &&
-    `
-      margin-left: ${mx * 0.4}rem;
-      margin-right: ${mx * 0.4}rem;
-    `}
+    mx !== undefined && `margin-left: ${addUnit(mx, 0.4)}; margin-right: ${addUnit(mx, 0.4)}`};
   ${({ my }) =>
-    my &&
-    `
-      margin-top: ${my * 0.4}rem;
-      margin-bottom: ${my * 0.4}rem;
-    `}
+    my !== undefined && `margin-top: ${addUnit(my, 0.4)}; margin-bottom: ${addUnit(my, 0.4)}`};
 
-  ${({ ml }) => ml && `margin-left: ${ml * 0.4}rem`};
-  ${({ mr }) => mr && `margin-right: ${mr * 0.4}rem`};
-  ${({ mt }) => mt && `margin-top: ${mt * 0.4}rem`};
-  ${({ mb }) => (mb || mb === 0) && `margin-bottom: ${mb * 0.4}rem`};
+  ${({ mt }) => mt !== undefined && `margin-top: ${addUnit(mt, 0.4)}`};
+  ${({ mr }) => mr !== undefined && `margin-right: ${addUnit(mr, 0.4)}`};
+  ${({ mb }) => mb !== undefined && `margin-bottom: ${addUnit(mb, 0.4)}`};
+  ${({ ml }) => ml !== undefined && `margin-left: ${addUnit(ml, 0.4)}`};
 `;
