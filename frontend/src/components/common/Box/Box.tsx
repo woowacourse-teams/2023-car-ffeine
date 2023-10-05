774998d4d6758bf26532839e0e8863941d9dca98
Dain Lee <feb.dain@gmail.com>
2023-10-05 14:09:48 +0900

    refactor: `Box` 컴포넌트에 border 관련 속성 추가
    [#823]

--- frontend/src/components/common/Box/Box.tsx
+++ frontend/src/components/common/Box/Box.tsx
@@ -1,14 +1,33 @@
 import type { CSSProp } from 'styled-components';
 
-import { type HTMLAttributes, type ReactNode } from 'react';
+import type { HTMLAttributes, ReactNode, ElementType } from 'react';
 
 import type { SpacingProps } from '@common/systems';
 
 import { StyledBox } from './Box.style';
 
+type FourSides = 'left' | 'right' | 'top' | 'bottom';
+
 export interface BoxProps extends HTMLAttributes<HTMLDivElement>, SpacingProps {
+  tag?: ElementType;
   children?: ReactNode;
-  border?: boolean;
+  /** 테두리에 둥글고(border-radius: 4px) 얇은 선(0.1px, #66666666)이 생김
+   * - 특정 방향(ex. 'left')을 넣으면 해당 부분만 얇은 선이 생김
+   * @default false
+   */
+  border?: boolean | FourSides;
+  /** border 색깔 변경 가능, **border가 false가 아닐 때 사용 가능** */
+  borderColor?: string;
+  /** border 두께 변경 가능, **border가 false가 아닐 때 사용 가능**
+   *- [string] 단위까지 적어줘야 함 (ex. 2px, 1%)
+   *- [number] 숫자만 적을 경우 px로 자동 변환
+   */
+  borderWidth?: number | string;
+  /** border 곡률 변경 가능
+   *- [string] 단위까지 적어줘야 함 (ex. 2px, 1%)
+   *- [number] 숫자만 적을 경우 px로 자동 변환
+   */
+  borderRadius?: number | string;
   height?: number | string;
   minHeight?: number | string;
   maxHeight?: number | string;
@@ -25,8 +44,12 @@ export interface BoxProps extends HTMLAttributes<HTMLDivElement>, SpacingProps {
   css?: CSSProp;
 }
 
-const Box = ({ children, ...props }: BoxProps) => {
-  return <StyledBox {...props}>{children}</StyledBox>;
+const Box = ({ tag = 'div', border = false, children, ...props }: BoxProps) => {
+  return (
+    <StyledBox as={tag} border={border} {...props}>
+      {children}
+    </StyledBox>
+  );
 };
 
 export default Box;
