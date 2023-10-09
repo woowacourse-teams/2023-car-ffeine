0038c77355dcc0d72409759e1fac4a9da23bb937
sosow0212 <sosow0212@naver.com>
2023-10-09 15:11:17 +0900

    refactor: 요청 보내는 기능 분리

--- backend/src/main/java/com/carffeine/carffeine/station/service/city/CityRequestService.java
+++ backend/src/main/java/com/carffeine/carffeine/station/service/city/CityRequestService.java
@@ -6,6 +6,7 @@ import com.carffeine.carffeine.station.infrastructure.api.RestTemplateCityReques
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
