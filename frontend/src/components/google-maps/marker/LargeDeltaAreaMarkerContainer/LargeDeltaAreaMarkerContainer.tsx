ae6b6b741841c68efdee1ff06e2769c59ae83116
kyw0716 <kyw0716@naver.com>
2023-10-13 14:52:46 +0900

    feat: 서버 클러스터링 마커 렌더링 기능 구현

--- frontend/src/components/google-maps/marker/LargeDeltaAreaMarkerContainer/LargeDeltaAreaMarkerContainer.tsx
+++ frontend/src/components/google-maps/marker/LargeDeltaAreaMarkerContainer/LargeDeltaAreaMarkerContainer.tsx
@@ -1,9 +1,46 @@
+import { useEffect } from 'react';
+
+import { markerInstanceStore } from '@stores/google-maps/markerInstanceStore';
+
 import { useClusterMarkers } from './hooks/useClusterMarkers';
+import { useRenderClusterMarkers } from './hooks/useRenderClusterMarker';
 
 const LargeDeltaAreaMarkerContainer = () => {
   const { data: clusterMarkers, isSuccess } = useClusterMarkers();
 
-  if (isSuccess) console.log('cluster', clusterMarkers);
+  const {
+    createNewMarkerInstances,
+    getRemainedMarkerInstances,
+    removeMarkersOutsideBounds,
+    removeAllMarkers,
+    renderFooMarkers,
+  } = useRenderClusterMarkers();
+
+  useEffect(() => {
+    return () => {
+      // MarkerContainers 컴포넌트에서 SmallMediumDeltaAreaMarkerContainer 컴포넌트가 unmount될 때 모든 마커를 지워준다.
+      removeAllMarkers(markerInstanceStore.getState());
+    };
+  }, []);
+
+  if (clusterMarkers === undefined || !isSuccess) {
+    return <></>;
+  }
+
+  const newMarkerInstances = createNewMarkerInstances(
+    markerInstanceStore.getState(),
+    clusterMarkers
+  );
+
+  const remainedMarkerInstances = getRemainedMarkerInstances(
+    markerInstanceStore.getState(),
+    clusterMarkers
+  );
+
+  removeMarkersOutsideBounds(markerInstanceStore.getState(), clusterMarkers);
+  renderFooMarkers(newMarkerInstances, clusterMarkers);
+
+  markerInstanceStore.setState([...remainedMarkerInstances, ...newMarkerInstances]);
 
   return <></>;
 };
