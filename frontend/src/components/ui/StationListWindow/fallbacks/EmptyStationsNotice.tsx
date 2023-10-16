2bbe149cea40c64dd2ef12dbca614c6fec6e8731
Dain Lee <108778921+feb-dain@users.noreply.github.com>
2023-10-16 17:00:26 +0900

    fix: 주변 충전소가 없습니다 컴포넌트 잘리는 문제 개선한다 (#866)
    * fix: 주변 충전소가 없습니다 컴포넌트 잘리는 문제 개선
    
    [#859]
    
    * refactor: 필요없는 코드 제거
    
    [#859]

--- frontend/src/components/ui/StationListWindow/fallbacks/EmptyStationsNotice.tsx
+++ frontend/src/components/ui/StationListWindow/fallbacks/EmptyStationsNotice.tsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 
-import Box from '@common/Box';
 import FlexBox from '@common/FlexBox';
 import Text from '@common/Text';
 
@@ -8,23 +7,21 @@ import Nothing from '@assets/nothing.svg';
 
 const EmptyStationsNotice = () => {
   return (
-    <FlexBox width="100%" height="100%" justifyContent="center">
-      <Box>
-        <Image />
-        <Text variant="h5" align="center" mt={-49} mb={4} color="#4b4b4b">
-          주변에 충전소가 없습니다.
-        </Text>
-        <Text align="center" color="#4b4b4b">
-          화면을 더 확대하거나 장소를 이동해 보세요.
-        </Text>
-      </Box>
+    <FlexBox width="100%" height="100%" nowrap direction="column" alignItems="center">
+      <Image />
+      <Text variant="h5" align="center" mt={-49} mb={4} color="#4b4b4b">
+        주변에 충전소가 없습니다.
+      </Text>
+      <Text align="center" color="#4b4b4b">
+        화면을 더 확대하거나 장소를 이동해 보세요.
+      </Text>
     </FlexBox>
   );
 };
 
 const Image = styled(Nothing)`
   display: block;
-  margin: 0.4rem auto 0;
+  margin-top: -2rem;
   transform: scale(0.1);
 `;
 
