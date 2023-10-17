64c1088e0ea8c42db4a9811f8c370c28aaa74d15
kyw0716 <kyw0716@naver.com>
2023-10-17 15:53:13 +0900

    fix: 서버 클러스터링 오류 수정
    - 큰 영역을 보고 있다가 작은 영역을 보려 할 때 서버 클러스터링 요청이 캐싱에 의해 다시 발생하지 않는 문제가 있었습니다.
    - 이를 해결하기 위해 줌 레벨을 활용했습니다.
    - 줌 레벨을 저장하는 수단으로 원래 마지막으로 접속했던 센터 좌표를 기억하기 위해 사용하던 로컬 스토리지의 공간을 활용했습니다.
    - 현재 줌 레벨이 로컬스토리지에 저장된 값과 비교해서 1 이상 차이가 나게 되면 그때 현재 줌 레벨을 로컬스토리지에 저장합니다.
    - 지도를 렌더링 하기 위해 가장 먼저 호출되는 메서드에 줌 레벨 초기화 코드를 추가해주었습니다 (로컬 스토리지에 있는 줌 레벨)
    
    [#860]

--- frontend/src/components/google-maps/map/CarFfeineListener.tsx
+++ frontend/src/components/google-maps/map/CarFfeineListener.tsx
@@ -5,14 +5,30 @@ import { useQueryClient } from '@tanstack/react-query';
 import { useExternalValue, useSetExternalState } from '@utils/external-state';
 import { getDisplayPosition } from '@utils/google-maps';
 import { isCachedRegion } from '@utils/google-maps/isCachedRegion';
-import { setLocalStorage } from '@utils/storage';
+import { getLocalStorage, setLocalStorage } from '@utils/storage';
 
 import { deltaAreaActions, deltaAreaStore } from '@stores/google-maps/deltaAreaStore';
 import { getGoogleMapStore } from '@stores/google-maps/googleMapStore';
 import { profileMenuOpenStore } from '@stores/profileMenuOpenStore';
 
+import { DEFAULT_CENTER, INITIAL_ZOOM_LEVEL } from '@constants/googleMaps';
 import { QUERY_KEY_CLUSTER_MARKERS, QUERY_KEY_STATION_MARKERS } from '@constants/queryKeys';
 import { LOCAL_KEY_LAST_POSITION } from '@constants/storageKeys';
+
+/**
+ * 지도의 center에 해당하는 위도, 경도 좌표는 googleMapStore에서 가져오고, 줌 레벨은 특정 상태에 따라 변경되어야 하므로 외부에서 받아온다.
+ *
+ * @param zoom 현재 지도 영역의 줌 레벨을 로컬 스토리지에 저장한다.
+ */
+const setDisplayPositionToLocalStorage = (zoom: number) => {
+  const googleMapCenter = getGoogleMapStore().getState().getCenter();
+
+  setLocalStorage<google.maps.LatLngLiteral & { zoom: number }>(LOCAL_KEY_LAST_POSITION, {
+    lat: googleMapCenter.lat(),
+    lng: googleMapCenter.lng(),
+    zoom: zoom,
+  });
+};
 
 const CarFfeineMapListener = () => {
   const googleMap = useExternalValue(getGoogleMapStore());
@@ -27,24 +43,35 @@ const CarFfeineMapListener = () => {
     }
 
     setIsProfileMenuOpen(false);
-
-    setLocalStorage<google.maps.LatLngLiteral>(LOCAL_KEY_LAST_POSITION, {
-      lat: googleMap.getCenter().lat(),
-      lng: googleMap.getCenter().lng(),
-    });
+    setDisplayPositionToLocalStorage(googleMap.getZoom());
   };
 
   const requestClusterMarkers = () => {
     const displayPosition = getDisplayPosition(googleMap);
+    const prevDisplayPosition = getLocalStorage<google.maps.LatLngLiteral & { zoom: number }>(
+      LOCAL_KEY_LAST_POSITION,
+      { ...DEFAULT_CENTER, zoom: INITIAL_ZOOM_LEVEL }
+    );
 
+    setIsProfileMenuOpen(false);
+
+    // 현재 사용자의 디바이스에 보여지는 지도 영역이 캐시된 영역 밖으로 나가지 않으면 서버 클러스터링 마커 재요청을 발생시키지 않는다.
     if (!isCachedRegion(displayPosition)) {
       queryClient.invalidateQueries({ queryKey: [QUERY_KEY_CLUSTER_MARKERS] });
+
+      setDisplayPositionToLocalStorage(googleMap.getZoom());
+      return;
     }
 
-    setLocalStorage<google.maps.LatLngLiteral>(LOCAL_KEY_LAST_POSITION, {
-      lat: googleMap.getCenter().lat(),
-      lng: googleMap.getCenter().lng(),
-    });
+    // 현재 사용자의 디바이스에 보여지는 지도 영역이 캐시된 영역 내에 있어도 줌 레벨이 이전보다 1이상 커졌을 시 서버 클러스터링 마커 재요청을 발생 시킨다.
+    if (Math.abs(googleMap.getZoom() - prevDisplayPosition.zoom) >= 1) {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY_CLUSTER_MARKERS] });
+
+      setDisplayPositionToLocalStorage(googleMap.getZoom());
+      return;
+    }
+
+    setDisplayPositionToLocalStorage(prevDisplayPosition.zoom);
   };
 
   useEffect(() => {
