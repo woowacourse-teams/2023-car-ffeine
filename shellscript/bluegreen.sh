211291a0ce1294cfb5c8f3e65845ca9e528afa73
sosow0212 <sosow0212@naver.com>
2023-10-17 16:33:09 +0900

    infra: 무중단배포 스크립트 작성

---
+++ shellscript/bluegreen.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+BEFORE_PORT="$BEFORE_PORT"
+NEW_PORT="$NEW_PORT"
+NEW_ACTUATOR_PORT="$NEW_ACTUATOR_PORT"
+
+echo "기존 포트 : $BEFORE_PORT"
+echo "새로운 포트: $NEW_PORT"
+echo "새로운 ACTUATOR_PORT: $NEW_ACTUATOR_PORT"
+
+count=0
+for count in {0..20}
+do
+  echo "서버 상태 확인(${count}/20)";
+
+  UP=$(curl -s http://127.0.0.1:${NEW_ACTUATOR_PORT}/actuator/health-check)
+
+  if [ "${UP}" != '{"status":"up"}' ]
+  	then
+  		sleep 10
+  		continue
+  	else
+  		break
+  fi
+done
+
+if [ $count -eq 20 ]
+then
+	echo "새로운 서버 배포를 실패했습니다."
+	exit 1
+fi
+
+export BACKEND_PORT=$NEW_PORT
+envsubst '${BACKEND_PORT}' < backend.template > backend.conf
+sudo mv backend.conf /etc/nginx/conf.d/
+sudo nginx -s reload
+
+if sudo docker ps | grep ${BEFORE_PORT}; then
+  sudo docker stop $(docker ps -q --filter "expose=${BEFORE_PORT}")
+fi
+
+sudo docker container prune -f
