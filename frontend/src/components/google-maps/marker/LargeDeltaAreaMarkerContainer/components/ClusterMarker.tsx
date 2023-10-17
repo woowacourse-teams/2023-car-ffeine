f72fc980f659836688ba40f9c3e24108b948f5eb
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-10-17 17:49:25 +0900

    refactor: p 감소
    [#872]

--- frontend/src/components/google-maps/marker/LargeDeltaAreaMarkerContainer/components/ClusterMarker.tsx
+++ frontend/src/components/google-maps/marker/LargeDeltaAreaMarkerContainer/components/ClusterMarker.tsx
@@ -8,16 +8,18 @@ export interface ClusterMarkerProps {
 const ClusterMarker = ({ count }: ClusterMarkerProps) => {
   return (
     <FlexBox
-      p={6}
+      p={4}
       borderRadius="50%"
       bgColor="#D9E5FF"
-      width="100px"
-      height="100px"
+      width="70px"
+      height="70px"
       justifyContent="center"
       alignItems="center"
       css={{ border: '1px solid #3366FF' }}
     >
-      <Text fontSize={2}>{count}</Text>
+      <Text tag="span" fontSize={2}>
+        {count}
+      </Text>
     </FlexBox>
   );
 };
