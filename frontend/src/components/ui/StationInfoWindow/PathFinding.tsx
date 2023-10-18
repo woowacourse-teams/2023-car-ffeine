feba61a2dd26df10db1909e7af535f2cb34e58e8
다인 <dain@dain-ui-MacBookAir.local>
2023-10-18 21:08:12 +0900

    refactor: 길찾기 컴포넌트에 속성 추가
    [#879]

--- frontend/src/components/ui/StationInfoWindow/PathFinding.tsx
+++ frontend/src/components/ui/StationInfoWindow/PathFinding.tsx
@@ -1,18 +1,28 @@
 import { BiSolidCar } from 'react-icons/bi';
 
 import FlexBox from '@common/FlexBox';
+import type { FlexBoxProps } from '@common/FlexBox/FlexBox';
 import Text from '@common/Text';
 
 import type { Station } from '@type';
 
-type PathFindingProps = Pick<Station, 'address' | 'latitude' | 'longitude'>;
+interface PathFindingProps
+  extends Pick<Station, 'address' | 'latitude' | 'longitude'>,
+    FlexBoxProps {}
 
-const PathFinding = ({ address, latitude, longitude }: PathFindingProps) => {
+const PathFinding = ({ address, latitude, longitude, ...props }: PathFindingProps) => {
   const KAKAO_MAP_LINK = `https://map.kakao.com/link/to/${address},${latitude},${longitude}`;
 
   return (
-    <FlexBox tag="a" alignItems="center" rel="noreferrer" href={KAKAO_MAP_LINK} target="_blank">
-      <Text variant="label">길찾기</Text>
+    <FlexBox
+      tag="a"
+      alignItems="center"
+      rel="noreferrer"
+      href={KAKAO_MAP_LINK}
+      target="_blank"
+      {...props}
+    >
+      <Text variant="label" weight='regular'>길찾기</Text>
       <BiSolidCar />
     </FlexBox>
   );
