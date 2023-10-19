00c4326b380736eee1321ff6aa42b840407f848d
drunkenhw <drunkenhw@gmail.com>
2023-10-19 18:51:27 +0900

    feat: 위도 경도 변경
    [#819]

--- backend/src/main/java/com/carffeine/carffeine/station/domain/station/Region.java
+++ backend/src/main/java/com/carffeine/carffeine/station/domain/station/Region.java
@@ -7,13 +7,13 @@ import java.util.List;
 public enum Region {
 
     SEOUL("서울특별시", BigDecimal.valueOf(37.540705), BigDecimal.valueOf(126.956764)),
-    INCHEON("인천광역시", BigDecimal.valueOf(37.469221), BigDecimal.valueOf(126.573234)),
+    INCHEON("인천광역시", BigDecimal.valueOf(37.3865), BigDecimal.valueOf(126.647)),
     GWANGJU("광주광역시", BigDecimal.valueOf(35.126033), BigDecimal.valueOf(126.831302)),
     DAEGU("대구광역시", BigDecimal.valueOf(35.798838), BigDecimal.valueOf(128.583052)),
     ULSAN("울산광역시", BigDecimal.valueOf(35.519301), BigDecimal.valueOf(129.239078)),
     DAEJEON("대전광역시", BigDecimal.valueOf(36.321655), BigDecimal.valueOf(127.378953)),
     BUSAN("부산광역시", BigDecimal.valueOf(35.198362), BigDecimal.valueOf(129.053922)),
-    GYEONGGI("경기도", BigDecimal.valueOf(37.567167), BigDecimal.valueOf(127.190292)),
+    GYEONGGI("경기도", BigDecimal.valueOf(37.2895), BigDecimal.valueOf(127.0535)),
     GANGWON("강원특별자치도", BigDecimal.valueOf(37.555837), BigDecimal.valueOf(128.209315)),
     CHUNGNAM("충청남도", BigDecimal.valueOf(36.557229), BigDecimal.valueOf(126.779757)),
     CHUNGBUK("충청북도", BigDecimal.valueOf(36.628503), BigDecimal.valueOf(127.929344)),
