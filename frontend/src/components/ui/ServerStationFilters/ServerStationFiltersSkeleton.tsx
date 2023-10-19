a26731f0d4b91d289b9683cb59404bc573bc5e58
feb-dain <feb.dain@gmail.com>
2023-10-20 02:11:22 +0900

    design: 스켈레톤 개선 전까지 임시 로더 보여주도록 함
    [#892]

--- frontend/src/components/ui/ServerStationFilters/ServerStationFiltersSkeleton.tsx
+++ frontend/src/components/ui/ServerStationFilters/ServerStationFiltersSkeleton.tsx
@@ -1,28 +1,34 @@
 import FlexBox from '@common/FlexBox';
-import Skeleton from '@common/Skeleton';
+import Loader from '@common/Loader';
+import Text from '@common/Text';
 
-import FilterOptionSkeleton from '@ui/ServerStationFilters/FilterOptionSkeleton';
 import {
   borderCss,
   containerCss,
-  filterHeaderCss,
   overFlowCss,
 } from '@ui/ServerStationFilters/ServerStationFilters';
+
+import { getColor } from '@style';
 
 const ServerStationFiltersSkeleton = () => {
   return (
     <FlexBox
-      width={34}
       height={'100vh'}
+      justifyContent="center"
       alignItems={'center'}
       direction={'column'}
       background={'white'}
       css={[overFlowCss, borderCss, containerCss]}
       nowrap={true}
       noRadius={'all'}
+      px={6}
     >
-      <FlexBox
-        width={34}
+      <Text tag="h2" variant="h6" mb={6} weight="regular">
+        필터링을 불러오고 있습니다
+      </Text>
+      <Loader border={4} css={{ width: '60px', height: '60px', borderBottomColor: getColor() }} />
+      {/* <FlexBox
+        width={32}
         height={8}
         justifyContent="between"
         alignItems="center"
@@ -34,7 +40,7 @@ const ServerStationFiltersSkeleton = () => {
       </FlexBox>
       <FilterOptionSkeleton />
       <FilterOptionSkeleton />
-      <FilterOptionSkeleton />
+      <FilterOptionSkeleton /> */}
     </FlexBox>
   );
 };
