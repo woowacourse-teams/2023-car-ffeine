0ef2a762d3c84ea52ab27a1cae44181b21ecd2ea
kiarakim <kiarakim413@gmail.com>
2023-10-19 13:35:35 +0900

    refactor: final 추가 및 불필요한 어노테이션 제거

--- backend/src/main/java/com/carffeine/carffeine/station/domain/station/Point.java
+++ backend/src/main/java/com/carffeine/carffeine/station/domain/station/Point.java
@@ -1,25 +1,18 @@
 package com.carffeine.carffeine.station.domain.station;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Embeddable;
 import java.math.BigDecimal;
 
 @ToString
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@EqualsAndHashCode
-@Embeddable
 public class Point {
 
-    private Latitude latitude;
-    private Longitude longitude;
+    private final Latitude latitude;
+    private final Longitude longitude;
 
     public static Point of(long latitude, long longitude) {
         return new Point(Latitude.from(BigDecimal.valueOf(latitude)), Longitude.from(BigDecimal.valueOf(longitude)));
