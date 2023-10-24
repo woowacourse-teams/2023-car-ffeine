e66a1bbef14e97a4047006f5a9bce4908fa70519
feb-dain <feb.dain@gmail.com>
2023-10-24 18:10:30 +0900

    feat: 앱 설치 버튼 구현 및 버튼 누르면 나오는 설치 방법 모달 구현
    [#899]

--- frontend/src/components/ui/Navigator/NavigationBar/Menu.tsx
+++ frontend/src/components/ui/Navigator/NavigationBar/Menu.tsx
@@ -1,7 +1,7 @@
 import { AdjustmentsHorizontalIcon, Bars3Icon, UserCircleIcon } from '@heroicons/react/24/outline';
 import { css } from 'styled-components';
 
-import { HiArrowPath, HiOutlineChatBubbleOvalLeftEllipsis } from 'react-icons/hi2';
+import { HiOutlineArrowDownTray, HiOutlineChatBubbleOvalLeftEllipsis } from 'react-icons/hi2';
 
 import { useExternalValue } from '@utils/external-state';
 
@@ -16,6 +16,7 @@ import PersonalMenu from '@ui/Navigator/NavigationBar/PersonalMenu';
 import ServerStationFilters from '@ui/ServerStationFilters';
 import StationListWindow from '@ui/StationListWindow';
 import StationSearchWindow from '@ui/StationSearchWindow';
+import HowToAppInstallModal from '@ui/modal/HowToAppInstallModal';
 import LoginModal from '@ui/modal/LoginModal/LoginModal';
 
 import { displayNoneInMobile, displayNoneInWeb } from '@style/mediaQuery';
@@ -38,6 +39,10 @@ const Menu = () => {
     modalActions.openModal(<LoginModal />);
   };
 
+  const handleOpenHowToAppInstallModal = () => {
+    modalActions.openModal(<HowToAppInstallModal />);
+  };
+
   return (
     <FlexBox css={[fixedPositionCss, paddingCss, borderCss, flexCss]} noRadius="all" nowrap>
       <Button
@@ -52,13 +57,13 @@ const Menu = () => {
 
       <Button
         noRadius="all"
+        aria-label="카페인 앱 설치하기"
         css={displayNoneInWeb}
-        aria-label="새로 고침"
-        onClick={() => location.reload()}
+        onClick={handleOpenHowToAppInstallModal}
       >
-        <HiArrowPath size="2.8rem" fill="#555" />
+        <HiOutlineArrowDownTray size="2.8rem" stroke="#555" />
         <Text mt={0.5} variant="caption">
-          새로고침
+          앱설치
         </Text>
       </Button>
 
@@ -120,6 +125,18 @@ const Menu = () => {
         <HiOutlineChatBubbleOvalLeftEllipsis size="2.8rem" stroke="#555" />
         <Text mt={0.5} variant="caption">
           피드백
+        </Text>
+      </Button>
+
+      <Button
+        noRadius="all"
+        aria-label="카페인 앱 설치하기"
+        onClick={handleOpenHowToAppInstallModal}
+        css={displayNoneInMobile}
+      >
+        <HiOutlineArrowDownTray size="2.8rem" stroke="#555" />
+        <Text mt={0.5} variant="caption">
+          앱설치
         </Text>
       </Button>
     </FlexBox>
