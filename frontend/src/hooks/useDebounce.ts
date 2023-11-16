bca16388b8ee3309269b64f1f1fca02da490de49
feb-dain <feb.dain@gmail.com>
2023-11-16 15:55:14 +0900

    refactor: useDebounce 개선
    [#927]

--- frontend/src/hooks/useDebounce.ts
+++ frontend/src/hooks/useDebounce.ts
@@ -1,11 +1,11 @@
 import { useEffect, useCallback } from 'react';
 
 export const useDebounce = <T>(func: (param: T) => void, dependencies: string[], delay: number) => {
-  const callback = useCallback(func, dependencies);
+  const callback = useCallback(func, [delay]);
 
   useEffect(() => {
     const timeout = setTimeout(callback, delay);
 
     return () => clearTimeout(timeout);
-  }, [callback, delay]);
+  }, [callback, dependencies]);
 };
